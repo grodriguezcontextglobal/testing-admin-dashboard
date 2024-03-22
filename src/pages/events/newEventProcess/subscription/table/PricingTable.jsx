b317b8b9340db5215d8002708bd635151bab3163
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-03-22 14:07:13 -0400

    set value for annual option

--- src/pages/events/newEventProcess/subscription/table/PricingTable.jsx
+++ src/pages/events/newEventProcess/subscription/table/PricingTable.jsx
@@ -1,16 +1,37 @@
 import { Grid, Typography } from "@mui/material";
-import { Button, Tooltip } from "antd";
+import { Button, notification } from "antd";
+import { useEffect } from "react";
 import { useDispatch } from "react-redux";
-import { onAddSubscription } from "../../../../../store/slices/subscriptionSlice";
 import subscriptionsList from "../../../../../components/json/subscriptionList.json";
+import { onAddSubscription } from "../../../../../store/slices/subscriptionSlice";
 const PricingTable = ({
   handleSubmitEventPayment,
   value,
   setTotal,
+  eventsList
 }) => {
   const dispatch = useDispatch();
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = (msg, dsc) => {
+    api.open({
+      message: msg,
+      description: dsc,
+      duration: 0,
+    });
+  };
+  useEffect(() => {
+    const controller = new AbortController()
+  if(eventsList?.length > 1) {
+    return openNotification("Upgrade to Access More Events", "Your access to the free tier has been disabled due to reaching the event limit. Upgrade your subscription to unlock more events and enjoy enhanced features.")
+  }
+    return () => {
+      controller.abort()
+    }
+  }, [])
+  
   return (
     <Grid marginBottom={5} container>
+      {contextHolder}
       <Grid item xs={3}></Grid>
       <Grid
         display={"flex"}
@@ -65,8 +86,8 @@ const PricingTable = ({
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           </Typography>
           <Button
+            disabled={eventsList?.length > 1}
             style={{
-              // width: "fit-content",
               border: "1px solid var(--blue-dark-600, #155EEF)",
               borderRadius: "8px",
               background: "var(--blue-dark-600, #155EEF)",
@@ -156,55 +177,55 @@ const PricingTable = ({
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           </Typography>
-          <Tooltip title="Function still in progress.">
-            <Button
-              disabled={true}
+          {/* <Tooltip title="Function still in progress."> */}
+          <Button
+            // disabled={true}
+            style={{
+              border: "1px solid var(--blue-dark-600, #155EEF)",
+              borderRadius: "8px",
+              background: "var(--blue-dark-600, #155EEF)",
+              boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
+              margin: "5px 0px",
+            }}
+            onClick={() => {
+              handleSubmitEventPayment(`${value === 0 ? "price_1NzOBaJAluu3aB96XWZfQrBt" : "36000"}`); //*price_1O6xtmJAluu3aB96Rbzx7UGg annually
+              setTotal(`${value === 0 ? "30" : "360"}`);
+              dispatch(
+                onAddSubscription({
+                  ...subscriptionsList.subscriptionsList[1],
+                  total: `${value === 0 ? parseInt("30") : parseInt("360")}`,
+                })
+              );
+            }}
+          >
+            <Typography
+              textTransform={"none"}
               style={{
-                border: "1px solid var(--blue-dark-600, #155EEF)",
-                borderRadius: "8px",
-                background: "var(--blue-dark-600, #155EEF)",
-                boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
-                margin: "5px 0px",
-              }}
-              onClick={() => {
-                handleSubmitEventPayment(`${value === 0 ? "price_1NzOBaJAluu3aB96XWZfQrBt" : "price_1O6xtmJAluu3aB96Rbzx7UGg"}`);
-                setTotal(`${value === 0 ? "30" : "360"}`);
-                dispatch(
-                  onAddSubscription({
-                    ...subscriptionsList.subscriptionsList[1],
-                    total: `${value === 0 ? parseInt("30") : parseInt("360")}`,
-                  })
-                );
+                color: "var(--base-white, #FFF",
+                fontSize: "14px",
+                fontWeight: "600",
+                fontFamily: "Inter",
+                lineHeight: "20px",
               }}
             >
-              <Typography
-                textTransform={"none"}
-                style={{
-                  color: "var(--base-white, #FFF",
-                  fontSize: "14px",
-                  fontWeight: "600",
-                  fontFamily: "Inter",
-                  lineHeight: "20px",
-                }}
-              >
-                Get started
-              </Typography>
-            </Button>
-            <Button disabled={true}>
-              <Typography
-                textTransform={"none"}
-                style={{
-                  color: "#344054",
-                  fontSize: "14px",
-                  fontWeight: "600",
-                  fontFamily: "Inter",
-                  lineHeight: "20px",
-                }}
-              >
-                Contact sales
-              </Typography>
-            </Button>
-          </Tooltip>
+              Get started
+            </Typography>
+          </Button>
+          <Button disabled={true}>
+            <Typography
+              textTransform={"none"}
+              style={{
+                color: "#344054",
+                fontSize: "14px",
+                fontWeight: "600",
+                fontFamily: "Inter",
+                lineHeight: "20px",
+              }}
+            >
+              Contact sales
+            </Typography>
+          </Button>
+          {/* </Tooltip> */}
         </Grid>
         <Grid
           display={"flex"}
@@ -250,58 +271,57 @@ const PricingTable = ({
           >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit,
           </Typography>
-          <Tooltip title="Function still in progress.">
-            <Button
-              disabled={true}
+          {/* <Tooltip title="Function still in progress."> */}
+          <Button
+            // disabled={true}
+            style={{
+              // width: "fit-content",
+              border: "1px solid var(--blue-dark-600, #155EEF)",
+              borderRadius: "8px",
+              background: "var(--blue-dark-600, #155EEF)",
+              boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
+              margin: "5px 0px",
+            }}
+            onClick={() => {
+              handleSubmitEventPayment(`${value === 0 ? "price_1O6xsWJAluu3aB96sZsWjLGl" : "72000"}`); //*price_1O6xuIJAluu3aB96AcmN6vtH annually
+              setTotal(`${value === 0 ? "60" : "720"}`);
+              dispatch(
+                onAddSubscription({
+                  ...subscriptionsList.subscriptionsList[2],
+                  total: `${value === 0 ? parseInt("60") : parseInt("720")}`,
+                })
+              );
+            }}
+          >
+            <Typography
+              textTransform={"none"}
               style={{
-                // width: "fit-content",
-                border: "1px solid var(--blue-dark-600, #155EEF)",
-                borderRadius: "8px",
-                background: "var(--blue-dark-600, #155EEF)",
-                boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.05)",
-                margin: "5px 0px",
-              }}
-              onClick={() => {
-                handleSubmitEventPayment(`${value === 0 ? "price_1O6xsWJAluu3aB96sZsWjLGl" : "price_1O6xuIJAluu3aB96AcmN6vtH"}`);
-                setTotal(`${value === 0 ? "60" : "720"}`);
-                dispatch(
-                  onAddSubscription({
-                    ...subscriptionsList.subscriptionsList[2],
-                    total: `${value === 0 ? parseInt("60") : parseInt("720")}`,
-                  })
-                );
+                color: "var(--base-white, #FFF",
+                fontSize: "14px",
+                fontWeight: "600",
+                fontFamily: "Inter",
+                lineHeight: "20px",
               }}
             >
-              <Typography
-                textTransform={"none"}
-                style={{
-                  color: "var(--base-white, #FFF",
-                  fontSize: "14px",
-                  fontWeight: "600",
-                  fontFamily: "Inter",
-                  lineHeight: "20px",
-                }}
-              >
-                Get started
-              </Typography>
-            </Button>
-            <Button disabled={true}>
-              
-              <Typography
-                textTransform={"none"}
-                style={{
-                  color: "#344054",
-                  fontSize: "14px",
-                  fontWeight: "600",
-                  fontFamily: "Inter",
-                  lineHeight: "20px",
-                }}
-              >
-                Contact sales
-              </Typography>
-            </Button>
-          </Tooltip>
+              Get started
+            </Typography>
+          </Button>
+          <Button disabled={true}>
 
+            <Typography
+              textTransform={"none"}
+              style={{
+                color: "#344054",
+                fontSize: "14px",
+                fontWeight: "600",
+                fontFamily: "Inter",
+                lineHeight: "20px",
+              }}
+            >
+              Contact sales
+            </Typography>
+          </Button>
+          {/* </Tooltip> */}
         </Grid>
       </Grid>
     </Grid>
