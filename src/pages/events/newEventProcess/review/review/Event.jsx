35152610312444e52f3a3de17fdf7e1b74faddd6
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-03-29 08:16:21 -0400

    fix style to display all components as expected

--- src/pages/events/newEventProcess/review/review/Event.jsx
+++ src/pages/events/newEventProcess/review/review/Event.jsx
@@ -29,7 +29,7 @@ const Event = () => {
         alignItems={"center"}
         gap={2}
         item
-        xs={10}
+        xs={12}
       >
         <InputLabel style={{ marginBottom: "0.2rem", width: "100%" }}>
           <Typography
