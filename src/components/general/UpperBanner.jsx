39e171f05b930fa7cb95d582beff07025872b527
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-04-11 17:26:43 -0400

    layout updated

--- src/components/general/UpperBanner.jsx
+++ src/components/general/UpperBanner.jsx
@@ -6,17 +6,17 @@ const UpperBanner = () => {
     const { user } = useSelector((state) => state.admin)
     return (
         <Grid container>
-            <Grid item sx={CenteringGrid}>
+            <Grid item sx={{...CenteringGrid, margin: '0 auto'}}>
                 <AppBar style={{
                     ...CenteringGrid, padding: '0', backgroundColor: "var(--blue-dark--800)",
-                    height: '2.5dvh'
+                    height: '3dvh'
                 }} component="nav">
                     <Toolbar>
                         <Typography
                             textTransform={'capitalize'}
                             textAlign={'center'}
                             color={"var(--gray300)"}
-                            padding={'0.2rem auto'}
+                            padding={'0.8rem auto'}
                             >
                             {user.company}
                         </Typography>
