eec49fd1deac5146a2f5ae4dd2860410243c2cba
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-04-15 11:35:54 -0400

    component to generate spreadsheet with all data needed

--- src/pages/events/quickGlance/components/SpreadSheet.jsx
+++ src/pages/events/quickGlance/components/SpreadSheet.jsx
@@ -10,76 +10,218 @@ import GrayButtonText from '../../../../styles/global/GrayButtonText';
 import { message } from 'antd'
 const SpreadSheet = () => {
     const [fileName, setFileName] = useState('');
-    const [items, setItems] = useState([]);
+    const [itemsUsers, setItemsUsers] = useState([]);
+    const [defectedItems, setDefectedItems] = useState([]);
     const { event } = useSelector((state) => state.event)
     const transactionDeviceRecordInEvent = useQuery({
         queryKey: ['transactionAndDeviceRecord'],
-        queryFn: () => devitrakApi.post('/receiver/receiver-assigned-list', {
+        queryFn: () => devitrakApi.post('/receiver/receiver-pool-list', {
             eventSelected: event.eventInfoDetail.eventName,
-            provider: event.company
+            provider: event.company,
         }),
         enabled: false,
         refetchOnMount: false
     })
+    const transactionPlusUserInfo = useQuery({
+        queryKey: ['transactionPlusUserInfo'],
+        queryFn: () => devitrakApi.post('/receiver/receiver-assigned-users-list', {
+            eventSelected: event.eventInfoDetail.eventName,
+            provider: event.company,
+            'device.status': true
+        }),
+        enabled: false,
+        refetchOnMount: false
+    })
+
     useEffect(() => {
         const controller = new AbortController()
         transactionDeviceRecordInEvent.refetch()
+        transactionPlusUserInfo.refetch()
         return () => {
             controller.abort()
         }
     }, [])
+
     useEffect(() => {
         const controller = new AbortController()
-        if (transactionDeviceRecordInEvent.data) {
-            return setItems(transactionDeviceRecordInEvent.data.data.listOfReceivers)
+        if (transactionDeviceRecordInEvent.data && transactionPlusUserInfo.data) {
+            setItemsUsers(transactionPlusUserInfo.data.data.listOfReceivers)
+            setDefectedItems(transactionDeviceRecordInEvent.data.data.receiversInventory)
         }
         return () => {
             controller.abort()
         }
-    }, [transactionDeviceRecordInEvent.data])
+    }, [transactionPlusUserInfo.data, transactionDeviceRecordInEvent.data])
     const [messageApi, contextHolder] = message.useMessage();
     const success = () => {
         messageApi.open({
             type: 'success',
-            content: 'This is a success message',
+            content: 'xlsx file generated and downloading.',
         });
     };
-    const generateExcelFile = async () => {
-        // Your data array
-        const data = items;
 
-        // Define the header columns based on your expectation
+    const sortAndGroupData = () => {
+        const result = new Map()
+        for (let data of itemsUsers) {
+            if (!result.has(data.user)) {
+                result.set(data.user, [data])
+            } else {
+                result.set(data.user, [...result.get(data.user), data])
+            }
+        }
+
+        const sortedResult = new Set()
+        for (const [user, receivers] of result) {
+            sortedResult.add({
+                firstName: receivers[0].userInfo.name,
+                lastName: receivers[0].userInfo.lastName,
+                email: user,
+                phoneNumber: receivers[0].userInfo.phoneNumber,
+                pendingDevices: receivers.length
+            })
+        }
+        return Array.from(sortedResult)
+    }
+
+    const defectedDevicesInfo = () => {
+        const result = new Set()
+        for (const data of defectedItems) {
+            if (data.status !== "Operational") {
+                result.add(data)
+            }
+        }
+        return Array.from(result)
+    }
+
+    const generateExcelFile = async () => {
+        // Define the header columns for Sheet1
         const headers = [
-            'device - serial number',
-            'device - device type',
-            'paymentIntent',
-            'user',
-            'active',
-            'eventSelected',
-            'provider',
-            'timeStamp',
-            'id'
+            'User - First name',
+            'User - Last name',
+            'User - Email',
+            'User - Phone number',
+            'Pending devices to return',
         ];
 
-        // Convert data to worksheet format
-        const wsData = [headers, ...data.map(item => [
-            item.device.serialNumber,
-            item.device.deviceType,
-            item.paymentIntent,
-            item.user,
-            item.active ? "In-use" : "In-stock",
-            item.eventSelected.join(', '), // Convert array to comma-separated string
-            item.provider.join(', '), // Convert array to comma-separated string
-            item.timeStamp,
-            item.id
+        // Convert data to worksheet format for Sheet1
+        const wsData = [headers, ...sortAndGroupData().map(item => [
+            item.firstName,
+            item.lastName,
+            item.email,
+            item.phoneNumber,
+            item.pendingDevices,
         ])];
 
         // Create a new workbook
         const wb = utils.book_new();
 
-        // Add the worksheet data to the workbook
-        const ws = utils.aoa_to_sheet(wsData);
-        utils.book_append_sheet(wb, ws, 'Sheet1'); // You can change the sheet name if needed
+        // Add Sheet1 to the workbook
+        const wsSheet1 = utils.aoa_to_sheet(wsData);
+
+        // Set cell styles for Sheet1
+        wsSheet1['!cols'] = [{ width: 20 }, { width: 20 }, { width: 30 }, { width: 20 }, { width: 30 }];
+        wsSheet1['E1'].l = { Target: "#Details!A1" }
+        for (let colTitle of headers) {
+            const headerCellAddress = utils.encode_cell({ r: 0, c: headers.indexOf(`${colTitle}`) });
+            wsSheet1[headerCellAddress].s = {
+                fill: { patternType: 'solid', bgColor: { rgb: '#ee1515' } },
+                font: {
+                    name: 'Inter',
+                    sz: 16,
+                    color: { rgb: '#fff' },
+                    bold: true,
+                    italic: false,
+                    underline: true
+                }
+            };
+        }
+
+        // Set background color for "Pending devices" cell if value > 5
+        const pendingDevicesIndex = headers.indexOf('Pending devices to return');
+        if (pendingDevicesIndex !== -1) {
+            // Iterate through data rows to check and set background color
+            sortAndGroupData().forEach((item, rowIndex) => {
+                const cellValue = item.pendingDevices;
+                if (!isNaN(cellValue) && cellValue >= 5) {
+                    const cellAddress = utils.encode_cell({ r: rowIndex + 1, c: pendingDevicesIndex });
+                    wsSheet1[`${cellAddress}`].s = {
+                        fill: { patternType: 'solid', bgColor: { rgb: '#ee1515' } },
+                        font: {
+                            name: 'Inter',
+                            sz: 16,
+                            color: { rgb: '#fff' },
+                            bold: true,
+                            italic: false,
+                            underline: true
+                        }
+                    };
+                }
+            });
+        }
+
+        utils.book_append_sheet(wb, wsSheet1, 'Report');
+
+        // Your data array
+        const data = itemsUsers;
+
+        // Sheet2 config (Details)
+        const headers2 = [
+            'Device - Serial number',
+            'Device - Device type',
+            'User - First name',
+            'User - Last name',
+            'User - Email',
+            'User - Phone number',
+            'Status',
+            'Event',
+            'Date - Assigned device',
+        ];
+
+        // Convert data to worksheet format for Sheet2 (all data in detail)
+        const wsDataDetail = [headers2, ...data.map(item => [
+            item.device.serialNumber,
+            item.device.deviceType,
+            item.userInfo.name,
+            item.userInfo.lastName,
+            item.user,
+            item.userInfo.phoneNumber,
+            item.active ? "in-Use" : "in-Stock",
+            item.eventSelected.join(', '), // Convert array to comma-separated string
+            Date(item.timeStamp).toString()
+        ])];
+
+        // Add Sheet2 to the workbook
+        const wsSheet2 = utils.aoa_to_sheet(wsDataDetail);
+
+        // Set cell styles for Sheet1
+        wsSheet2['!cols'] = [{ width: 25 }, { width: 25 }, { width: 30 }, { width: 25 }, { width: 30 }, { width: 25 }, { width: 30 }, { width: 25 }, { width: 30 }];
+
+
+        utils.book_append_sheet(wb, wsSheet2, 'Details');
+
+        const headers3 = [
+            'Device',
+            'Device type',
+            'Device Status',
+            'Comment',
+        ];
+        const data2 = defectedDevicesInfo();
+
+        // Convert data to worksheet format for Sheet3 (all data in detail)
+        const wsDataDefected = [headers3, ...data2.map(item => [
+            item.device,
+            item.type,
+            item.status,
+            item.comment,
+        ])];
+
+        // Add Sheet3 to the workbook
+        const wsSheet3 = utils.aoa_to_sheet(wsDataDefected);
+
+        // Set cell styles for Sheet1
+        wsSheet3['!cols'] = [{ width: 25 }, { width: 25 }, { width: 30 }, { width: 25 }, { width: 30 }];
+
+        utils.book_append_sheet(wb, wsSheet3, 'Defected_and_Lost devices');
 
         // Generate a random file name (you can customize this logic)
         const newFileName = `excel_${Date.now()}.xlsx`;
@@ -89,7 +231,6 @@ const SpreadSheet = () => {
 
         // Set the generated file name to state
         setFileName(newFileName);
-        // await 
     };
 
     useEffect(() => {
