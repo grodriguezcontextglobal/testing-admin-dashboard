3f5852b719231a749a53a14e67ea1f4d81b6a8e9
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-04-16 23:24:01 -0400

    component to edit large amount of devices at once

--- src/pages/inventory/details/detailComponent/actions/EditItem.jsx
+++ src/pages/inventory/details/detailComponent/actions/EditItem.jsx
@@ -1,11 +1,13 @@
 import { Grid, Typography } from "@mui/material"
 import { Button, Card } from "antd"
-import { BlueButton } from "../../../../../styles/global/BlueButton"
-import { BlueButtonText } from "../../../../../styles/global/BlueButtonText"
 import { CardStyle } from "../../../../../styles/global/CardStyle"
 import { useWindowScroll } from "@uidotdev/usehooks"
 import EditItemModal from "../components/EditItemModal"
 import { useState } from "react"
+import LightBlueButtonText from "../../../../../styles/global/LightBlueButtonText"
+import { LightBlueButton } from "../../../../../styles/global/LightBlueButton"
+import { EditIcon } from "../../../../../components/icons/Icons"
+import CenteringGrid from "../../../../../styles/global/CenteringGrid"
 
 const EditItem = ({ dataFound }) => {
     const [{ x, y }, scrollTo] = useWindowScroll()
@@ -24,10 +26,11 @@ const EditItem = ({ dataFound }) => {
             md={12}
         >
             <Card
-                style={CardStyle}
+                style={{ ...CardStyle, padding: "0" }}
             >
                 <Grid
                     display={"flex"}
+                    padding={0}
                     justifyContent={"flex-end"}
                     alignItems={"center"}
                     container
@@ -40,7 +43,9 @@ const EditItem = ({ dataFound }) => {
                         item
                         xs={12}
                     >
-                        <Button onClick={() => { scrollTo({ left: 0, top: '50dv', behavior: 'smooth' }); setOpenEditItemModal(true) }} style={BlueButton}><Typography style={BlueButtonText}>Edit</Typography></Button>
+                        <Button onClick={() => { scrollTo({ left: 0, top: '50dv', behavior: 'smooth' }); setOpenEditItemModal(true) }} style={LightBlueButton}>
+                            <span style={{ CenteringGrid, alignSelf: 'stretch ' }}><EditIcon /></span>&nbsp;<Typography style={LightBlueButtonText}>Edit</Typography>
+                        </Button>
                     </Grid>
                 </Grid>
             </Card>
