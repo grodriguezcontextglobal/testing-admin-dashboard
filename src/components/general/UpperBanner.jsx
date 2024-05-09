7d5be00cb15d3afe4f7088c47691f7608cef39b4
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-05-09 19:05:26 -0400

    imported styles

--- src/components/general/UpperBanner.jsx
+++ src/components/general/UpperBanner.jsx
@@ -1,23 +1,22 @@
 import { Grid, Typography, AppBar, Toolbar } from "@mui/material"
 import { useSelector } from "react-redux"
 import CenteringGrid from "../../styles/global/CenteringGrid"
+import { Subtitle } from "../../styles/global/Subtitle"
 
 const UpperBanner = () => {
     const { user } = useSelector((state) => state.admin)
     return (
         <Grid container>
-            <Grid item sx={{...CenteringGrid, margin: '0 auto'}}>
+            <Grid item sx={{ ...CenteringGrid, margin: '0 auto' }}>
                 <AppBar style={{
                     ...CenteringGrid, padding: '0', backgroundColor: "var(--blue-dark--800)",
                     height: '3dvh'
                 }} component="nav">
                     <Toolbar>
                         <Typography
-                            textTransform={'capitalize'}
-                            textAlign={'center'}
-                            color={"var(--gray300)"}
+                            style={{...Subtitle, color:"var(--gray300)", textAlign:"center", textTransform:"capitalize"}}
                             padding={'0.8rem auto'}
-                            >
+                        >
                             {user.company}
                         </Typography>
                     </Toolbar>
