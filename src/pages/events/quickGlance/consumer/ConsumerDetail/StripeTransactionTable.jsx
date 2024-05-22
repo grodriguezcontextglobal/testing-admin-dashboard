5b98cf68113f8349591392da6486007e195d4730
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-05-21 22:42:53 -0400

    columns multiple option for search and filter removed

--- src/pages/events/quickGlance/consumer/ConsumerDetail/StripeTransactionTable.jsx
+++ src/pages/events/quickGlance/consumer/ConsumerDetail/StripeTransactionTable.jsx
@@ -154,7 +154,6 @@ const StripeTransactionTable = ({ searchValue }) => {
     }
     //*nested table starts here
     const renderDataPerRow = (rowRecord) => {
-        console.log("🚀 ~ renderDataPerRow ~ rowRecord:", rowRecord)
         const foundTransactionAndDevicesAssigned = () => {
             if (foundAllTransactionsAndDevicesAssigned()) {
                 const paymentIntentInRecord = foundAllTransactionsAndDevicesAssigned()[rowRecord.paymentIntent]
@@ -164,125 +163,6 @@ const StripeTransactionTable = ({ searchValue }) => {
             }
             return []
         }
-        const handleSearch = (selectedKeys, confirm, dataIndex) => {
-            confirm();
-            setSearchText(selectedKeys[0]);
-            setSearchedColumn(dataIndex);
-        };
-        const handleReset = (clearFilters) => {
-            clearFilters();
-            setSearchText("");
-        };
-
-        const getColumnSearchProps = (dataIndex) => ({
-            filterDropdown: ({
-                setSelectedKeys,
-                selectedKeys,
-                confirm,
-                clearFilters,
-                close,
-            }) => (
-                <div
-                    key={dataIndex}
-                    style={{
-                        padding: 8,
-                    }}
-                    onKeyDown={(e) => e.stopPropagation()}
-                >
-                    <Input
-                        ref={searchInput}
-                        placeholder={`Search ${dataIndex}`}
-                        value={selectedKeys[0]}
-                        onChange={(e) =>
-                            setSelectedKeys(e.target.value ? [e.target.value] : [])
-                        }
-                        onPressEnter={() =>
-                            handleSearch(selectedKeys, confirm, dataIndex)
-                        }
-                        style={{
-                            marginBottom: 8,
-                            display: "block",
-                        }}
-                    />
-                    <Space>
-                        <Button
-                            type="primary"
-                            onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-                            icon={<SearchOutlined />}
-                            size="small"
-                            style={{
-                                width: 90,
-                            }}
-                        >
-                            Search
-                        </Button>
-                        <Button
-                            onClick={() => clearFilters && handleReset(clearFilters)}
-                            size="small"
-                            style={{
-                                width: 90,
-                            }}
-                        >
-                            Reset
-                        </Button>
-                        <Button
-                            type="link"
-                            size="small"
-                            onClick={() => {
-                                confirm({
-                                    closeDropdown: false,
-                                });
-                                setSearchText(selectedKeys[0]);
-                                setSearchedColumn(dataIndex);
-                            }}
-                        >
-                            Filter
-                        </Button>
-                        <Button
-                            type="link"
-                            size="small"
-                            onClick={() => {
-                                close();
-                            }}
-                        >
-                            close
-                        </Button>
-                    </Space>
-                </div>
-            ),
-            filterIcon: (filtered) => (
-                <SearchOutlined
-                    style={{
-                        color: filtered ? "#1677ff" : undefined,
-                    }}
-                />
-            ),
-            onFilter: (value, record) =>
-                record[dataIndex]
-                    .toString()
-                    .toLowerCase()
-                    .includes(value.toLowerCase()),
-            onFilterDropdownOpenChange: (visible) => {
-                if (visible) {
-                    setTimeout(() => searchInput.current?.select(), 100);
-
-                }
-            },
-            render: (text) =>
-                searchedColumn === dataIndex ? (
-                    <Highlighter
-                        highlightStyle={{
-                            backgroundColor: "#ffc069",
-                            padding: 0,
-                        }}
-                        searchWords={[searchText]}
-                        autoEscape
-                        textToHighlight={text ? text.toString() : ""}
-                    />
-                ) : (
-                    text
-                ),
-        });
         const checkDevicesInTransaction = () => {
             const result = new Set()
             if (foundTransactionAndDevicesAssigned()) {
@@ -450,7 +330,7 @@ const StripeTransactionTable = ({ searchValue }) => {
                 title: "Device serial number",
                 dataIndex: "serialNumber",
                 key: "serialNumber",
-                ...getColumnSearchProps("serialNumber"),
+                // ...getColumnSearchProps("serialNumber"),
                 sorter: {
                     compare: (a, b) =>
                         ("" + a.serialNumber).localeCompare(b.serialNumber),
