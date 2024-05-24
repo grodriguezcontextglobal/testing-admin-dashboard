253b8cdc2b727a415172e9955038018b87a9923a
Gustavo <gar.santeliz@gmail.com>
2024-05-23 23:35:30 -0400

    nested row table in customer detail page

--- src/pages/consumers/tables/ExpandedRow.jsx
+++ src/pages/consumers/tables/ExpandedRow.jsx
@@ -1,7 +1,340 @@
-const ExpandedRow = () => {
-  return (
-    <div>ExpandedRow</div>
-  )
-}
+import { Chip } from "@mui/material";
+import { useQuery } from "@tanstack/react-query";
+import { Badge, Space, Table, message } from "antd";
+import { useEffect, useState } from "react";
+import { devitrakApi } from "../../../api/devitrakApi";
+import { BlueButton } from "../../../styles/global/BlueButton";
+import { BlueButtonText } from "../../../styles/global/BlueButtonText";
+import { GrayButton } from "../../../styles/global/GrayButton";
+import GrayButtonText from "../../../styles/global/GrayButtonText";
+import { useDispatch, useSelector } from "react-redux";
+import { onReceiverObjectToReplace } from "../../../store/slices/helperSlice";
+import {
+  onAddCustomer,
+  onAddDevicesAssignedInPaymentIntent,
+  onAddPaymentIntentDetailSelected,
+  onAddPaymentIntentSelected,
+} from "../../../store/slices/stripeSlice";
+import Choice from "../components/markedLostOption/Choice";
+import {
+  onAddEventData,
+  onAddEventInfoDetail,
+  onSelectCompany,
+  onSelectEvent,
+} from "../../../store/slices/eventSlice";
 
-export default ExpandedRow
+const ExpandedRow = ({ rowRecord }) => {
+  const [openModal, setOpenModal] = useState(false);
+  const { customer } = useSelector((state) => state.customer);
+  const { user } = useSelector((state) => state.admin);
+  const assignedDevicesQuery = useQuery({
+    queryKey: ["assignedDevicesByTransaction", rowRecord.key], // Include rowRecord.key in queryKey to differentiate queries
+    queryFn: () =>
+      devitrakApi.post("/receiver/receiver-assigned-users-list", {
+        paymentIntent: rowRecord.key,
+      }),
+    enabled: false,
+    refetchOnMount: false,
+  });
+
+  const eventsRelatedToTransactionQuery = useQuery({
+    queryKey: ["eventsInfoPerTransactionQuery", rowRecord.key], // Include rowRecord.key in queryKey to differentiate queries
+    queryFn: () =>
+      devitrakApi.post("/event/event-list", {
+        company: user.company,
+        "eventInfoDetail.eventName": rowRecord.eventSelected[0],
+      }),
+    enabled: false,
+    refetchOnMount: false,
+  });
+
+  useEffect(() => {
+    const controller = new AbortController();
+    assignedDevicesQuery.refetch();
+    console.log("assignedDevicesQuery", assignedDevicesQuery?.data?.data);
+    eventsRelatedToTransactionQuery.refetch();
+    return () => {
+      controller.abort();
+    };
+  }, [assignedDevicesQuery]);
+
+  const dispatch = useDispatch();
+  const [messageApi, contextHolder] = message.useMessage();
+  const success = () => {
+    messageApi.open({
+      type: "success",
+      content: "Device was returned",
+    });
+  };
+
+  const columns = [
+    {
+      title: "Date",
+      dataIndex: "timeStamp",
+      key: "timeStamp",
+      render: (timeStamp) => <p>{new Date(timeStamp).toUTCString()}</p>,
+    },
+    {
+      title: "Serial number",
+      dataIndex: "serial_number",
+      key: "serial_number",
+      render: (serial_number) => <p>{serial_number}</p>,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      key: "status",
+      render: (status) => (
+        <Badge
+          style={{
+            display: "flex",
+            padding: "2px 8px",
+            alignItems: "center",
+            borderRadius: "16px",
+            background: `${
+              status !== "Lost"
+                ? status
+                  ? "var(--Primary-50, #F9F5FF)"
+                  : "var(--Success-50, #ECFDF3)"
+                : "var(--Success-50, #ECFDF3)"
+            }`,
+            mixBlendMode: "multiply",
+          }}
+        >
+          <Chip
+            style={{
+              backgroundColor: `${
+                status !== "Lost"
+                  ? status
+                    ? "var(--Success-50, #ECFDF3)"
+                    : "var(--Primary-50, #F9F5FF)"
+                  : "var(--Primary-50, #F9F5FF)"
+              }`,
+            }}
+            label={
+              <p
+                style={{
+                  color: `${
+                    status !== "Lost"
+                      ? status
+                        ? "var(--success-700, #027A48)"
+                        : "var(--Primary-700, #6941C6)"
+                      : "var(--Primary-700, #6941C6)"
+                  }`,
+                  fontFamily: "Inter",
+                  fontSize: "12px",
+                  fontStyle: "normal",
+                  fontWeight: 500,
+                  lineHeight: "18px",
+                }}
+              >
+                {status === "Lost"
+                  ? "Returned"
+                  : status
+                  ? "Active"
+                  : "Returned"}
+              </p>
+            }
+          />
+        </Badge>
+      ),
+    },
+    {
+      title: "",
+      key: "operation",
+      render: (record) => (
+        <Space
+          size="middle"
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+          }}
+        >
+          <button
+            onClick={() => handleReturnSingleDevice(record)}
+            style={{
+              ...BlueButton,
+              display: `${record.status ? "flex" : "none"}`,
+            }}
+          >
+            <p style={BlueButtonText}>Mark as returned</p>
+          </button>
+          <button
+            onClick={() => handleLostSingleDevice(record)}
+            style={{
+              ...GrayButton,
+              display: `${record.status ? "flex" : "none"}`,
+            }}
+          >
+            <p
+              style={{
+                ...GrayButtonText,
+                color: `${
+                  record.status
+                    ? GrayButtonText.color
+                    : "var(--disabled0gray-button-text)"
+                }`,
+              }}
+            >
+              Mark as lost
+            </p>
+          </button>
+        </Space>
+      ),
+    },
+  ];
+  const dataRendering = () => {
+    if (assignedDevicesQuery.data) {
+      const query = assignedDevicesQuery.data.data.listOfReceivers;
+      const dataForTable = new Set();
+      for (let data of query) {
+        dataForTable.add({
+          key: data._id,
+          serial_number: data.device.serialNumber,
+          type: data.device.deviceType,
+          status: data.device.status,
+          timeStamp: data.timeStamp,
+          entireData: data,
+        });
+      }
+      return Array.from(dataForTable);
+    }
+    return [];
+  };
+  useEffect(() => {
+    dataRendering();
+  }, [assignedDevicesQuery.data]);
+
+  const handleReturnSingleDevice = async (props) => {
+    try {
+      const respUpdate = await devitrakApi.patch(
+        `/receiver/receiver-update/${props.entireData._id}`,
+        {
+          id: props.entireData._id,
+          device: {
+            serialNumber: props.serial_number,
+            deviceType: props.type,
+            status: false,
+          },
+        }
+      );
+      if (respUpdate.data) {
+        const deviceInPoolListQuery = await devitrakApi.post(
+          "/receiver/receiver-pool-list",
+          {
+            eventSelected: props.entireData.eventSelected[0],
+            provider: props.entireData.provider[0],
+            device: props.serial_number,
+            type: props.type,
+          }
+        );
+        if (deviceInPoolListQuery.data) {
+          const dateString = new Date().toString();
+          const dateRef = dateString.split(" ");
+          const deviceInPoolProfile = {
+            id: deviceInPoolListQuery.data.receiversInventory[0].id,
+            activity: "No",
+          };
+
+          const returningInPool = await devitrakApi.patch(
+            `/receiver/receivers-pool-update/${deviceInPoolListQuery.data.receiversInventory[0].id}`,
+            deviceInPoolProfile
+          );
+          if (returningInPool.data) {
+            await devitrakApi.post(
+              "/nodemailer/confirm-returned-device-notification",
+              {
+                consumer: {
+                  name: `${customer.name} ${customer.lastName}`,
+                  email: customer.email,
+                },
+                device: {
+                  serialNumber: props.serial_number,
+                  deviceType: props.type,
+                },
+                event: props.entireData.eventSelected[0],
+                company: props.entireData.provider[0],
+                transaction: props.entireData.paymentIntent,
+                date: String(dateRef.slice(0, 4)).replaceAll(",", " "),
+                time: dateRef[4],
+                link: `https://app.devitrak.net/authentication/${encodeURI(
+                  props.entireData.eventSelected[0]
+                )}/${encodeURI(props.entireData.provider[0])}/${customer.uid}`,
+              }
+            );
+            await assignedDevicesQuery.refetch();
+            return success();
+          }
+        }
+      }
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: StripeTransactionHistoryByUser.jsx:277 ~ handleReturnSingleDevice ~ error:",
+        error
+      );
+    }
+  };
+  const handleLostSingleDevice = (props) => {
+    try {
+      setOpenModal(true);
+      dispatch(
+        onAddEventData(eventsRelatedToTransactionQuery.data.data.list[0])
+      );
+      dispatch(
+        onAddEventInfoDetail(
+          eventsRelatedToTransactionQuery.data.data.list[0].eventInfoDetail
+        )
+      );
+      dispatch(
+        onSelectCompany(
+          eventsRelatedToTransactionQuery.data.data.list[0].company
+        )
+      );
+      dispatch(
+        onSelectEvent(
+          eventsRelatedToTransactionQuery.data.data.list[0].eventInfoDetail
+            .eventName
+        )
+      );
+      dispatch(
+        onReceiverObjectToReplace({
+          serialNumber: props.serial_number,
+          deviceType: props.type,
+          status: props.status,
+        })
+      );
+      dispatch(onAddDevicesAssignedInPaymentIntent(props.entireData));
+      dispatch(onAddPaymentIntentSelected(props.entireData.paymentIntent));
+      dispatch(
+        onAddPaymentIntentDetailSelected({
+          serialNumber: props.serial_number,
+          deviceType: props.type,
+          status: props.status,
+        })
+      );
+      dispatch(onAddCustomer(props.entireData.userInfo));
+    } catch (error) {
+      console.log(
+        "🚀 ~ file: StripeTransactionHistoryByUser.jsx:277 ~ handleReturnSingleDevice ~ error:",
+        error
+      );
+    }
+  };
+
+  return (
+    <>
+      {contextHolder}
+      <Table
+        columns={columns}
+        dataSource={dataRendering()}
+        pagination={false}
+      />
+      {openModal && (
+        <Choice openModal={openModal} setOpenModal={setOpenModal} />
+      )}
+    </>
+  );
+};
+
+export default ExpandedRow;
--- src/pages/consumers/tables/StripeTransactionPerConsumer.jsx
+++ src/pages/consumers/tables/StripeTransactionPerConsumer.jsx
@@ -1,58 +1,78 @@
 import { Chip } from "@mui/material";
 import { Avatar, Badge, Table } from "antd";
-import { useCallback, useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { devitrakApi } from "../../../api/devitrakApi";
 import { RightNarrowInCircle } from "../../../components/icons/Icons";
-import { onAddEventData, onSelectCompany, onSelectEvent } from "../../../store/slices/eventSlice";
+import {
+  onAddEventData,
+  onSelectCompany,
+  onSelectEvent,
+} from "../../../store/slices/eventSlice";
 import { onAddPaymentIntentSelected } from "../../../store/slices/stripeSlice";
 import { onAddSubscription } from "../../../store/slices/subscriptionSlice";
 import { Subtitle } from "../../../styles/global/Subtitle";
-import '../../../styles/global/ant-table.css';
+import "../../../styles/global/ant-table.css";
+import ExpandedRow from "./ExpandedRow";
 const StripeTransactionPerConsumer = ({ searchValue }) => {
   const { user } = useSelector((state) => state.admin);
-  const { eventsPerAdmin } = useSelector((state) => state.event)
+  const { eventsPerAdmin } = useSelector((state) => state.event);
   const { customer } = useSelector((state) => state.customer);
-  const [responsedData, setResponsedData] = useState([])
+  const [responsedData, setResponsedData] = useState([]);
   const navigate = useNavigate();
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   //refactoring -->>
   const avoidDuplicatedEventsPerAdmin = () => {
-    const result = new Set()
+    const result = new Set();
     if (eventsPerAdmin.active) {
       for (let data of eventsPerAdmin.active) {
-        const toString = JSON.stringify(data)
+        const toString = JSON.stringify(data);
         if (!result.has(toString)) {
-          result.add(toString)
+          result.add(toString);
         }
       }
     }
     if (eventsPerAdmin.completed) {
       for (let data of eventsPerAdmin.completed) {
-        const toString = JSON.stringify(data)
+        const toString = JSON.stringify(data);
         if (!result.has(toString)) {
-          result.add(toString)
+          result.add(toString);
         }
       }
     }
-    return Array.from(result)
-  }
-  const fetchingDataPerAllowed = useCallback(async () => {
+    return Array.from(result);
+  };
+  const fetchingDataPerAllowed = async () => {
     const result = new Map();
     if (avoidDuplicatedEventsPerAdmin().length > 0) {
       for (let data of avoidDuplicatedEventsPerAdmin()) {
-        const parsing = JSON.parse(data)
-        if (customer.data.eventSelected.some(element => element === parsing.eventInfoDetail.eventName) && customer.data.provider.some(element => element === parsing.company)) {
-          const respo = await devitrakApi.post("/receiver/receiver-assigned-list", { provider: parsing.company, user: customer.email, eventSelected: parsing.eventInfoDetail.eventName })
+        const parsing = JSON.parse(data);
+        if (
+          customer.data.eventSelected.some(
+            (element) => element === parsing.eventInfoDetail.eventName
+          ) &&
+          customer.data.provider.some((element) => element === parsing.company)
+        ) {
+          const respo = await devitrakApi.post(
+            "/receiver/receiver-assigned-list",
+            {
+              provider: parsing.company,
+              user: customer.email,
+              eventSelected: parsing.eventInfoDetail.eventName,
+            }
+          );
           if (respo.data) {
             if (respo.data.listOfReceivers.length > 0) {
               for (let data of respo.data.listOfReceivers) {
                 if (!result.has(data.paymentIntent)) {
-                  result.set(data.paymentIntent, [data])
+                  result.set(data.paymentIntent, [data]);
                 } else {
-                  result.set(data.paymentIntent, [...result.get(data.paymentIntent), data])
+                  result.set(data.paymentIntent, [
+                    ...result.get(data.paymentIntent),
+                    data,
+                  ]);
                 }
               }
             }
@@ -60,135 +80,128 @@ const StripeTransactionPerConsumer = ({ searchValue }) => {
         }
       }
     }
-    return setResponsedData(result)
-  }, [])
-  fetchingDataPerAllowed()
+    return setResponsedData(result);
+  };
+  useEffect(() => {
+    const controller = new AbortController();
+    fetchingDataPerAllowed();
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   const reformedSourceData = () => {
-    const result = new Set()
+    const result = new Set();
     responsedData.forEach((value) => {
       result.add({
-        // key: nanoid(Number(String(value[0].paymentIntent).length)),
         eventSelected: value[0].eventSelected,
         paymentIntent: value[0].paymentIntent,
         device: value.length,
-        status: value.reduce((acc, { device }) => acc + (device.status === false), 0),
-      })
-    })
-    return Array.from(result)
+        status: value.reduce(
+          (acc, { device }) => acc + (device.status === false),
+          0
+        ),
+      });
+    });
+    return Array.from(result);
   };
 
   const addingKeysToExpandRow = () => {
-    const result = new Set()
+    const result = new Set();
     for (let data of reformedSourceData()) {
       result.add({
-        // key: nanoid(String(data.paymentIntent).length),
-        ...data
-      })
+        key: data.paymentIntent,
+        ...data,
+      });
     }
-    return Array.from(result)
+    return Array.from(result);
   };
 
-
-  const finalDataToDiplayIncludeSearchFN = () => {
+  const finalDataToDisplayIncludeSearchFN = () => {
     if (searchValue?.length > 0 && addingKeysToExpandRow()?.length > 0) {
-      return addingKeysToExpandRow().filter(element =>
-        String(element.eventSelected).toLowerCase().includes(String(searchValue).toLowerCase()) ||
-        String(element.paymentIntent).toLowerCase().includes(String(searchValue).toLowerCase())
-      )
+      return addingKeysToExpandRow().filter(
+        (element) =>
+          String(element.eventSelected)
+            .toLowerCase()
+            .includes(String(searchValue).toLowerCase()) ||
+          String(element.paymentIntent)
+            .toLowerCase()
+            .includes(String(searchValue).toLowerCase())
+      );
     }
-    return addingKeysToExpandRow()
-  }
+    return addingKeysToExpandRow();
+  };
   const moreDetailFn = async (record) => {
-    const eventListQuery = await devitrakApi.post("/event/event-list", { company: user.company, 'eventInfoDetail.eventName': record.eventSelected[0] })
-    dispatch(onSelectEvent(eventListQuery.data.list[0].eventInfoDetail.eventName));
+    const eventListQuery = await devitrakApi.post("/event/event-list", {
+      company: user.company,
+      "eventInfoDetail.eventName": record.eventSelected[0],
+    });
+    dispatch(
+      onSelectEvent(eventListQuery.data.list[0].eventInfoDetail.eventName)
+    );
     dispatch(onSelectCompany(eventListQuery.data.list[0].company));
     dispatch(onAddEventData(eventListQuery.data.list[0]));
     dispatch(onAddSubscription(eventListQuery.data.list[0].subscription));
     dispatch(onAddPaymentIntentSelected(record.paymentIntent));
-    navigate(`/events/event-attendees/${customer.uid ?? customer.id}/transactions-details`);
+    navigate(
+      `/events/event-attendees/${
+        customer.uid ?? customer.id
+      }/transactions-details`
+    );
   };
 
-  //*expanded row
-  // const expandedRowRender = (rowRecord) => {
-  //   const columns = [
-  //     {
-  //       title: 'Date',
-  //       dataIndex: 'date',
-  //       key: 'date',
-  //     },
-  //     {
-  //       title: 'Name',
-  //       dataIndex: 'name',
-  //       key: 'name',
-  //     },
-  //     {
-  //       title: 'Status',
-  //       key: 'state',
-  //       render: () => <Badge status="success" text="Finished" />,
-  //     },
-  //     {
-  //       title: 'Upgrade Status',
-  //       dataIndex: 'upgradeNum',
-  //       key: 'upgradeNum',
-  //     },
-  //     {
-  //       title: 'Action',
-  //       key: 'operation',
-  //       render: () => (
-  //         <Space size="middle">
-  //           <a>Pause</a>
-  //           <a>Stop</a>
-
-  //         </Space>
-  //       ),
-  //     },
-  //   ];
-  //   const dataExpanded = [];
-  //   for (let i = 0; i < 3; ++i) {
-  //     dataExpanded.push({
-  //       key: i.toString(),
-  //       date: '2014-12-24 23:12:00',
-  //       name: 'This is production name',
-  //       upgradeNum: 'Upgraded: 56',
-  //     });
-  //   }
-  //   return <Table columns={columns} dataSource={dataExpanded} pagination={false} />;
-  // };
+  // *expanded row
   const columns = [
     {
       title: "Event",
       dataIndex: "eventSelected",
       key: "eventSelected",
       render: (eventSelected) => {
-        const initials = String(eventSelected).split(" ")
+        const initials = String(eventSelected).split(" ");
         return (
-          <div style={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}><Avatar>{initials.map(item => item[0])}</Avatar>&nbsp;
-            <p style={{
-              color: "var(--Blue-dark-600, #155EEF)",
-              fontFamily: "Inter",
-              fontSize: "14px",
-              fontStyle: "normal",
-              fontWeight: "400",
-              lineHeight: "20px", /* 142.857% */
-            }}>{eventSelected}</p ></div>
-        )
-      }
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+            }}
+          >
+            <Avatar>{initials.map((item) => item[0])}</Avatar>&nbsp;
+            <p
+              style={{
+                color: "var(--Blue-dark-600, #155EEF)",
+                fontFamily: "Inter",
+                fontSize: "14px",
+                fontStyle: "normal",
+                fontWeight: "400",
+                lineHeight: "20px" /* 142.857% */,
+              }}
+            >
+              {eventSelected}
+            </p>
+          </div>
+        );
+      },
     },
     {
       title: "Transaction ID",
       dataIndex: "paymentIntent",
       key: "paymentIntent",
       render: (paymentIntent) => (
-        <p style={{
-          color: "var(--Gray900)", //, #101828
-          fontFamily: "Inter",
-          fontSize: "14px",
-          fontStyle: "normal",
-          fontWeight: "400",
-          lineHeight: "20px", /* 142.857% */
-        }}> {paymentIntent}</p >
-      )
+        <p
+          style={{
+            color: "var(--Gray900)", //, #101828
+            fontFamily: "Inter",
+            fontSize: "14px",
+            fontStyle: "normal",
+            fontWeight: "400",
+            lineHeight: "20px" /* 142.857% */,
+          }}
+        >
+          {" "}
+          {paymentIntent}
+        </p>
+      ),
     },
     {
       title: "Devices",
@@ -196,8 +209,7 @@ const StripeTransactionPerConsumer = ({ searchValue }) => {
       key: "device",
       responsive: ["lg"],
       render: (_, record) => (
-        <p
-          style={Subtitle}        >
+        <p style={Subtitle}>
           {record.device} {record.device > 1 ? "devices" : "device"}&nbsp;
           <Badge
             style={{
@@ -205,22 +217,41 @@ const StripeTransactionPerConsumer = ({ searchValue }) => {
               padding: "2px 8px",
               alignItems: "center",
               borderRadius: "16px",
-              background: "var(--success-50, #ECFDF3)",
+              background: `${
+                record.status === record.device
+                  ? "var(--Primary-50, #F9F5FF)"
+                  : "var(--Success-50, #ECFDF3)"
+              }`,
               mixBlendMode: "multiply",
             }}
-          ><Chip style={{ backgroundColor: `${record.status === record.device ? "var(--Success-50, #ECFDF3)" : "var(--Primary-50, #F9F5FF)"}` }} label={<p
-            style={{
-              color: `${record.status === record.device ? "var(--success-700, #027A48)" : "var(--Primary-700, #6941C6)"}`,
-              fontFamily: "Inter",
-              fontSize: "12px",
-              fontStyle: "normal",
-              fontWeight: 500,
-              lineHeight: "18px",
-            }}
           >
-            {record.status === record.device ? "Returned" : "Active"}
-          </p>
-          } />
+            <Chip
+              style={{
+                backgroundColor: `${
+                  record.status === record.device
+                    ? "var(--Primary-50, #F9F5FF)"
+                    : "var(--Success-50, #ECFDF3)"
+                }`,
+              }}
+              label={
+                <p
+                  style={{
+                    color: `${
+                      record.status === record.device
+                        ? "var(--Primary-700, #6941C6)"
+                        : "var(--success-700, #027A48)"
+                    }`,
+                    fontFamily: "Inter",
+                    fontSize: "12px",
+                    fontStyle: "normal",
+                    fontWeight: 500,
+                    lineHeight: "18px",
+                  }}
+                >
+                  {record.status === record.device ? "Returned":"Active"}
+                </p>
+              }
+            />
           </Badge>
         </p>
       ),
@@ -232,7 +263,10 @@ const StripeTransactionPerConsumer = ({ searchValue }) => {
       responsive: ["md"],
       width: "3%",
       render: (_, record) => (
-        <button style={{ background: "transparent", outline: "none" }} onClick={() => moreDetailFn(record)}>
+        <button
+          style={{ background: "transparent", outline: "none" }}
+          onClick={() => moreDetailFn(record)}
+        >
           <RightNarrowInCircle />
         </button>
       ),
@@ -241,11 +275,12 @@ const StripeTransactionPerConsumer = ({ searchValue }) => {
   return (
     <Table
       columns={columns}
-      // expandable={{
-      //   expandedRowRender: (record) => (
-      //     expandedRowRender(record))
-      // }}
-      dataSource={finalDataToDiplayIncludeSearchFN()}
+      expandable={{
+        expandIcon: false,
+        expandRowByClick: true,
+        expandedRowRender: (record) => <ExpandedRow rowRecord={record} />,
+      }}
+      dataSource={finalDataToDisplayIncludeSearchFN()}
       className="table-ant-customized"
       pagination={{
         position: ["bottomCenter"],
