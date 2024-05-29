12efa7a129abb498e46f680838acbb5319a8a161
Gustavo <gar.santeliz@gmail.com>
2024-05-29 05:41:34 -0400

    adding nested row functionality to organize data based on status and then expand row for more information.

--- src/pages/events/quickGlance/components/ModalListOfDefectedDevices.jsx
+++ src/pages/events/quickGlance/components/ModalListOfDefectedDevices.jsx
@@ -1,37 +1,76 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { Modal, Table } from "antd";
 
 const ModalListOfDefectedDevices = ({
   defectedDeviceList,
   setDefectedDeviceList,
-  data
+  data,
 }) => {
-  console.log(data)
   const closeModal = () => {
     return setDefectedDeviceList(false);
   };
+
+  const dataToRender = () => {
+    const result = new Map();
+    for (let item of data) {
+      if (!result.has(item.status)) {
+        result.set(item.status, [item]);
+      } else {
+        result.set(item.status, [...result.get(item.status), item]);
+      }
+    }
+    const final = new Set();
+    for (let [status, value] of result) {
+      final.add({
+        key: status,
+        status: status,
+        qty: value.length,
+        report: value,
+      });
+    }
+    return Array.from(final);
+  };
+  const internalRow = (record) => {
+    const columns = [
+      {
+        title: "Device ID",
+        dataIndex: "device",
+        key: "device",
+      },
+      {
+        title: "Status",
+        dataIndex: "status",
+        key: "status",
+      },
+      {
+        title: "Device type",
+        dataIndex: "type",
+        key: "type",
+      },
+      {
+        title: "Comment",
+        dataIndex: "comment",
+        key: "comment",
+      },
+    ];
+
+    return (
+      <Table columns={columns} dataSource={record.report} rowKey={record.key} />
+    );
+  };
+
   const columns = [
-    {
-      title: "Device ID",
-      dataIndex: "device",
-      key: "device",
-    },
     {
       title: "Status",
       dataIndex: "status",
       key: "status",
     },
     {
-      title: "Device type",
-      dataIndex: "type",
-      key: "type",
-    },
-    {
-      title: "Comment",
-      dataIndex: "comment",
-      key: "comment",
+      title: "Qty",
+      dataIndex: "qty",
+      key: "qty",
     },
   ];
-
   return (
     <Modal
       open={defectedDeviceList}
@@ -41,7 +80,37 @@ const ModalListOfDefectedDevices = ({
       maskClosable={false}
       title="List of Defected Devices"
     >
-      <Table columns={columns} dataSource={data} rowKey="device" />
+      <Table
+        columns={columns}
+        dataSource={dataToRender()}
+        rowKey="device"
+        expandable={{
+          expandIcon: (record) => {
+            if (record.expanded) {
+              return (
+                <Icon
+                  icon="mdi:arrow-collapse"
+                  width={20}
+                  color="var(--gray300)"
+                />
+              );
+            } else {
+              return (
+                <Icon
+                  icon="mdi:arrow-expand"
+                  width={20}
+                  color="var(--gray300)"
+                />
+              );
+            }
+          },
+          expandRowByClick: true,
+          expandedRowRender: (record) => internalRow(record),
+        }}
+        style={{
+          cursor: "pointer",
+        }}
+      />
     </Modal>
   );
 };
