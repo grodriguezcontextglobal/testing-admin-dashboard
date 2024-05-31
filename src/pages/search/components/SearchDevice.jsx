a40cdf2e428f616eb1d5bc87b46dadcfd6cfeffb
Gustavo <gar.santeliz@gmail.com>
2024-05-31 12:35:29 -0400

    adding check condition to wait for having the data fetched to render component

--- src/pages/search/components/SearchDevice.jsx
+++ src/pages/search/components/SearchDevice.jsx
@@ -1,63 +1,73 @@
 import { Grid, Typography } from "@mui/material";
-import { useQuery } from '@tanstack/react-query';
-import _ from 'lodash';
+import { useQuery } from "@tanstack/react-query";
+import _ from "lodash";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { devitrakApi } from "../../../api/devitrakApi";
 import Loading from "../../../components/animation/Loading";
 import { onAddCustomerInfo } from "../../../store/slices/customerSlice";
 import { onOpenDeviceAssignmentModalFromSearchPage } from "../../../store/slices/devicesHandleSlice";
-import { onSelectCompany, onSelectEvent } from "../../../store/slices/eventSlice";
-import { onAddCustomer, onAddPaymentIntentDetailSelected, onAddPaymentIntentSelected } from "../../../store/slices/stripeSlice";
+import {
+  onSelectCompany,
+  onSelectEvent,
+} from "../../../store/slices/eventSlice";
+import {
+  onAddCustomer,
+  onAddPaymentIntentDetailSelected,
+  onAddPaymentIntentSelected,
+} from "../../../store/slices/stripeSlice";
 import CenteringGrid from "../../../styles/global/CenteringGrid";
 import { TextFontSize20LineHeight30 } from "../../../styles/global/TextFontSize20HeightLine30";
 import { TextFontSize30LineHeight38 } from "../../../styles/global/TextFontSize30LineHeight38";
 import CardDeviceFound from "../utils/CardDeviceFound";
 import NoDataFound from "../utils/NoDataFound";
 const SearchDevice = ({ searchParams }) => {
-  const { user } = useSelector((state) => state.admin)
+  const { user } = useSelector((state) => state.admin);
   const staffMembersQuery = useQuery({
     queryKey: ["listOfAssignedReceivers"],
-    queryFn: () => devitrakApi.post("/receiver/receiver-assigned-users-list", {
-      company: user.company,
-      'device.serialNumber': searchParams,
-      'device.status': true
-    }),
+    queryFn: () =>
+      devitrakApi.post("/receiver/receiver-assigned-users-list", {
+        company: user.company,
+        "device.serialNumber": searchParams,
+        "device.status": true,
+      }),
     enabled: false,
     refetchOnMount: false,
   });
   const deviceInUseStaffMemberQuery = useQuery({
     queryKey: ["deviceInUseStaffMember"],
-    queryFn: () => devitrakApi.post("/db_item/consulting-item", {
-      company: user.company,
-      warehouse: 0
-    }),
+    queryFn: () =>
+      devitrakApi.post("/db_item/consulting-item", {
+        company: user.company,
+        warehouse: 0,
+      }),
     enabled: false,
     refetchOnMount: false,
   });
 
   const imageDeviceQuery = useQuery({
     queryKey: ["imageDeviceList"],
-    queryFn: () => devitrakApi.post("/image/images", {
-      company: user.company,
-    }),
+    queryFn: () =>
+      devitrakApi.post("/image/images", {
+        company: user.company,
+      }),
     enabled: false,
     refetchOnMount: false,
   });
 
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const controller = new AbortController()
-    staffMembersQuery.refetch()
-    imageDeviceQuery.refetch()
-    deviceInUseStaffMemberQuery.refetch()
+    const controller = new AbortController();
+    staffMembersQuery.refetch();
+    imageDeviceQuery.refetch();
+    deviceInUseStaffMemberQuery.refetch();
     return () => {
-      controller.abort()
-    }
-  }, [searchParams])
+      controller.abort();
+    };
+  }, [searchParams]);
   // const sortDeviceAssignedStaff = () => {
   //   const reference = new Set()
   //   if (deviceInUseStaffMemberQuery.data) {
@@ -73,77 +83,156 @@ const SearchDevice = ({ searchParams }) => {
   // }
 
   const sortAndRenderFoundData = () => {
-    if (staffMembersQuery.data && deviceInUseStaffMemberQuery.data ) {
-      const foundData = [...staffMembersQuery.data.data.listOfReceivers]
-      const result = foundData?.filter(element => JSON.stringify(element).toLowerCase().includes(`${searchParams}`.toLowerCase()))
-      return result
+    if (staffMembersQuery.data && deviceInUseStaffMemberQuery.data) {
+      const foundData = [...staffMembersQuery.data.data.listOfReceivers];
+      const result = foundData?.filter((element) =>
+        JSON.stringify(element)
+          .toLowerCase()
+          .includes(`${searchParams}`.toLowerCase())
+      );
+      return result;
     }
-  }
+  };
   const imagesDeviceFoundData = () => {
     if (imageDeviceQuery.data) {
-      const foundData = imageDeviceQuery?.data?.data?.item
-      const grouping = _.groupBy(foundData, 'item_group')
-      return grouping
+      const foundData = imageDeviceQuery?.data?.data?.item;
+      const grouping = _.groupBy(foundData, "item_group");
+      return grouping;
     }
-  }
+  };
 
   useEffect(() => {
-    const controller = new AbortController()
-    sortAndRenderFoundData()
+    const controller = new AbortController();
+    sortAndRenderFoundData();
     return () => {
-      controller.abort()
-    }
-  }, [searchParams, staffMembersQuery.data])
+      controller.abort();
+    };
+  }, [searchParams, staffMembersQuery.data]);
 
   const handleDeviceSearch = async (record) => {
-    const respTransaction = await devitrakApi.post('/transaction/transaction', {
-      paymentIntent: record.data.paymentIntent
-    })
+    console.log(record);
+    const respTransaction = await devitrakApi.post("/transaction/transaction", {
+      paymentIntent: record.data.paymentIntent,
+    });
+    console.log(respTransaction.data);
     if (respTransaction.data.ok) {
       let userProfile = {
         ...respTransaction.data.list[0].consumerInfo,
-        uid: respTransaction.data.list[0].consumerInfo.uid ?? respTransaction.data.list[0].consumerInfo.id,
+        uid:
+          respTransaction.data.list[0].consumerInfo.uid ??
+          respTransaction.data.list[0].consumerInfo.id,
       };
       const paymentIntentDetailSelectedProfile = {
         ...respTransaction.data.list[0],
         user: userProfile,
-        device: respTransaction.data.list[0].device[0].deviceNeeded
+        device: respTransaction.data.list[0].device[0].deviceNeeded,
       };
-      dispatch(
-        onOpenDeviceAssignmentModalFromSearchPage(true)
-      );
+      dispatch(onOpenDeviceAssignmentModalFromSearchPage(true));
       dispatch(
         onAddPaymentIntentDetailSelected(paymentIntentDetailSelectedProfile)
       );
       dispatch(onSelectEvent(record.event));
       dispatch(onSelectCompany(record.data.provider[0]));
       dispatch(onAddCustomer(paymentIntentDetailSelectedProfile.consumerInfo));
-      dispatch(onAddCustomerInfo(paymentIntentDetailSelectedProfile.consumerInfo));
+      dispatch(
+        onAddCustomerInfo(paymentIntentDetailSelectedProfile.consumerInfo)
+      );
       dispatch(onAddPaymentIntentSelected(record.data.paymentIntent));
-      return navigate(`/events/event-attendees/${userProfile.uid}/transactions-details`);
+      return navigate(
+        `/events/event-attendees/${userProfile.uid}/transactions-details`
+      );
     }
-  }
-  if (staffMembersQuery.isLoading) return <div style={CenteringGrid}><Loading /></div>
-  if (staffMembersQuery.data) {
+  };
+  if (
+    staffMembersQuery.isLoading &&
+    imageDeviceQuery.isLoading &&
+    deviceInUseStaffMemberQuery.isLoading
+  )
     return (
-      <Grid container style={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
-        <Grid style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center", alignSelf: "flex-start" }} item xs={12} sm={12} md={4} lg={4}>
-
-          <Typography style={{ ...TextFontSize30LineHeight38, fontSize: "36px", lineHeight: "44px", fontWeight: 600, width: "100%", textAlign: "left" }}>Search Device </Typography><br />
-          <Typography style={{ ...TextFontSize20LineHeight30, width: "100%", textAlign: "left" }}>
+      <div style={CenteringGrid}>
+        <Loading />
+      </div>
+    );
+  if (
+    staffMembersQuery.data &&
+    imageDeviceQuery.data &&
+    deviceInUseStaffMemberQuery.data
+  ) {
+    return (
+      <Grid
+        container
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <Grid
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            alignSelf: "flex-start",
+          }}
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={4}
+        >
+          <Typography
+            style={{
+              ...TextFontSize30LineHeight38,
+              fontSize: "36px",
+              lineHeight: "44px",
+              fontWeight: 600,
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
+            Search Device{" "}
+          </Typography>
+          <br />
+          <Typography
+            style={{
+              ...TextFontSize20LineHeight30,
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
             All devices matching the search keywords.
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={12} md={8} lg={8}>
           <Grid container gap={1}>
-            {sortAndRenderFoundData()?.length > 0 ?
-              sortAndRenderFoundData()?.map(item => <Grid key={item.id} item xs={12} sm={12} md={3} lg={3}> <CardDeviceFound props={{ serialNumber: item?.device?.serialNumber, type: item?.device?.deviceType, event: item?.eventSelected[0], image: imagesDeviceFoundData()[item?.device?.deviceType]?.at(-1)?.source, data: item ?? [] }} fn={handleDeviceSearch} /></Grid>)
-              : <NoDataFound />}
+            {sortAndRenderFoundData()?.length > 0 ? (
+              sortAndRenderFoundData()?.map((item) => (
+                <Grid key={item.id} item xs={12} sm={12} md={3} lg={3}>
+                  {" "}
+                  <CardDeviceFound
+                    props={{
+                      serialNumber: item?.device?.serialNumber,
+                      type: item?.device?.deviceType,
+                      event: item?.eventSelected[0],
+                      image:
+                        imagesDeviceFoundData()[item?.device?.deviceType]?.at(
+                          -1
+                        )?.source,
+                      data: item ?? [],
+                    }}
+                    fn={handleDeviceSearch}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <NoDataFound />
+            )}
           </Grid>
         </Grid>
       </Grid>
-    )
+    );
   }
-}
-export default SearchDevice
+};
+export default SearchDevice;
--- src/pages/search/components/SearchStaff.jsx
+++ src/pages/search/components/SearchStaff.jsx
@@ -1,8 +1,8 @@
 import { Grid, Typography } from "@mui/material";
-import { useQuery } from '@tanstack/react-query';
-import _ from 'lodash';
+import { useQuery } from "@tanstack/react-query";
+import _ from "lodash";
 import { useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { devitrakApi } from "../../../api/devitrakApi";
 import Loading from "../../../components/animation/Loading";
@@ -14,104 +14,178 @@ import CardSearchStaffFound from "../utils/CardSearchStaffFound";
 import NoDataFound from "../utils/NoDataFound";
 import { notification } from "antd";
 const SearchStaff = ({ searchParams }) => {
-  const { user } = useSelector((state) => state.admin)
+  const { user } = useSelector((state) => state.admin);
   const staffMembersQuery = useQuery({
     queryKey: ["employeesPerCompany"],
-    queryFn: () => devitrakApi.post("/company/search-company", {
-      company_name: user.company
-    }),
+    queryFn: () =>
+      devitrakApi.post("/company/search-company", {
+        company_name: user.company,
+      }),
     enabled: false,
     refetchOnMount: false,
   });
   const staffMembersInfoQuery = useQuery({
     queryKey: ["listOfStaffMembersInfo"],
-    queryFn: () => devitrakApi.post("/staff/admin-users", {
-      company: user.company
-    }),
+    queryFn: () =>
+      devitrakApi.post("/staff/admin-users", {
+        company: user.company,
+      }),
     enabled: false,
     refetchOnMount: false,
   });
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const [api, contextHolder] = notification.useNotification();
   const openNotification = (title) => {
     api.open({
-      message: title
+      message: title,
     });
   };
 
   useEffect(() => {
-    const controller = new AbortController()
-    staffMembersQuery.refetch()
-    staffMembersInfoQuery.refetch()
+    const controller = new AbortController();
+    staffMembersQuery.refetch();
+    staffMembersInfoQuery.refetch();
     return () => {
-      controller.abort()
-    }
+      controller.abort();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchParams])
+  }, [searchParams]);
 
   const sortAndRenderFoundData = () => {
     if (staffMembersQuery.data) {
-      const foundData = staffMembersQuery.data.data.company.at(-1).employees
-      const result = foundData?.filter(element => JSON.stringify(element).toLowerCase().includes(`${searchParams}`.toLowerCase()))
-      return result
+      const foundData = staffMembersQuery.data.data.company.at(-1).employees;
+      const result = foundData?.filter((element) =>
+        JSON.stringify(element)
+          .toLowerCase()
+          .includes(`${searchParams}`.toLowerCase())
+      );
+      return result;
     }
-    return []
-  }
+    return [];
+  };
 
   const checkStaffInfo = () => {
     if (staffMembersInfoQuery.data) {
-      const foundData = staffMembersInfoQuery.data.data.adminUsers
-      const result = foundData?.filter(element => JSON.stringify(element).toLowerCase().includes(`${searchParams}`.toLowerCase()))
-      const grouping = _.groupBy(result, 'email')
-      return grouping
+      const foundData = staffMembersInfoQuery.data.data.adminUsers;
+      const result = foundData?.filter((element) =>
+        JSON.stringify(element)
+          .toLowerCase()
+          .includes(`${searchParams}`.toLowerCase())
+      );
+      const grouping = _.groupBy(result, "email");
+      return grouping;
     }
-    return []
-  }
+    return [];
+  };
   useEffect(() => {
-    const controller = new AbortController()
-    sortAndRenderFoundData()
+    const controller = new AbortController();
+    sortAndRenderFoundData();
     return () => {
-      controller.abort()
-    }
+      controller.abort();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [searchParams, staffMembersQuery.data])
+  }, [searchParams, staffMembersQuery.data]);
 
   const handleDetailStaff = async (record) => {
     const template = {
-      ...record.data, ...record.other,
+      ...record.data,
+      ...record.other,
       adminUserInfo: record.other,
-      companyData: staffMembersQuery.data.data.company.at(-1)
-    }
+      companyData: staffMembersQuery.data.data.company.at(-1),
+    };
     if (record.status === "Pending") {
-      return openNotification('Staff member has not confirmed invitation yet.')
+      return openNotification("Staff member has not confirmed invitation yet.");
     }
     dispatch(onAddStaffProfile(template));
-    return navigate(`/staff/${record.other.id}/main`)
+    return navigate(`/staff/${record.other.id}/main`);
   };
-  if (staffMembersQuery.isLoading) return <div style={CenteringGrid}><Loading /></div>
-  if (staffMembersQuery.data) {
+  if (staffMembersQuery.isLoading && staffMembersInfoQuery.isLoading)
     return (
-      <Grid key={'searching-staff-container'} container style={{ display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+      <div style={CenteringGrid}>
+        <Loading />
+      </div>
+    );
+  if (staffMembersQuery.data && staffMembersInfoQuery.data) {
+    return (
+      <Grid
+        key={"searching-staff-container"}
+        container
+        style={{
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
         {contextHolder}
-        <Grid style={{ display: "flex", flexDirection: "column", justifyContent: "flex-start", alignItems: "center", alignSelf: "flex-start" }} item xs={12} sm={12} md={4} lg={4}>
-          <Typography style={{ ...TextFontSize30LineHeight38, fontSize: "36px", lineHeight: "44px", fontWeight: 600, width: "100%", textAlign: "left" }}>Search staff </Typography><br />
-          <Typography style={{ ...TextFontSize20LineHeight30, width: "100%", textAlign: "left" }}>
+        <Grid
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            alignSelf: "flex-start",
+          }}
+          item
+          xs={12}
+          sm={12}
+          md={4}
+          lg={4}
+        >
+          <Typography
+            style={{
+              ...TextFontSize30LineHeight38,
+              fontSize: "36px",
+              lineHeight: "44px",
+              fontWeight: 600,
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
+            Search staff{" "}
+          </Typography>
+          <br />
+          <Typography
+            style={{
+              ...TextFontSize20LineHeight30,
+              width: "100%",
+              textAlign: "left",
+            }}
+          >
             All staff matching the search keywords.
           </Typography>
         </Grid>
 
         <Grid item xs={12} sm={12} md={8} lg={8}>
-          <Grid key={'searching-staff-page'} container gap={1}>
-            {sortAndRenderFoundData()?.length > 0 && staffMembersInfoQuery.data ?
-              sortAndRenderFoundData()?.map(item => <Grid key={item.id} item xs={12} sm={12} md={3} lg={3}> <CardSearchStaffFound props={{ name: item.firstName, lastName: item.lastName, email: item.user, phoneNumber: "", data: item, other: checkStaffInfo()[`${item?.user}`]?.at(-1), status: item.status }} fn={handleDetailStaff} /></Grid>)
-              : <NoDataFound />}
+          <Grid key={"searching-staff-page"} container gap={1}>
+            {sortAndRenderFoundData()?.length > 0 &&
+            staffMembersInfoQuery.data ? (
+              sortAndRenderFoundData()?.map((item) => (
+                <Grid key={item.id} item xs={12} sm={12} md={3} lg={3}>
+                  {" "}
+                  <CardSearchStaffFound
+                    props={{
+                      name: item.firstName,
+                      lastName: item.lastName,
+                      email: item.user,
+                      phoneNumber: "",
+                      data: item,
+                      other: checkStaffInfo()[`${item?.user}`]?.at(-1),
+                      status: item.status,
+                    }}
+                    fn={handleDetailStaff}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <NoDataFound />
+            )}
           </Grid>
         </Grid>
       </Grid>
-    )
+    );
   }
-}
+};
 
-export default SearchStaff
+export default SearchStaff;
