d365ba77646e06263c07980ef36813fb54e0901c
Gustavo <grodriguez@contextglobal.com>
2024-06-04 19:02:24 -0400

    redirection page to replace login and register route when pathname is not changed

---
+++ src/components/utils/RedirectionPage.jsx
@@ -0,0 +1,12 @@
+import CenteringGrid from "../../styles/global/CenteringGrid";
+import Loading from "../animation/Loading";
+
+const RedirectionPage = () => {
+  return (
+    <div style={CenteringGrid}>
+      <Loading />
+    </div>
+  );
+};
+
+export default RedirectionPage;
