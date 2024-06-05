dd573b833588c3ef8d7dc484a97430a93e8b0735
Gustavo <grodriguez@contextglobal.com>
2024-06-05 08:56:24 -0400

    adding condition and component to display message if inventory data is empty

---
+++ src/pages/home/utils/bannerMsg.jsx
@@ -0,0 +1,58 @@
+import { Grid } from "@mui/material";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { Title } from "../../../styles/global/Title";
+import { TextFontSize20LineHeight30 } from "../../../styles/global/TextFontSize20HeightLine30";
+import { PropTypes } from "prop-types";
+
+const BannerMsg = ({props}) => {
+  return (
+    <Grid
+      display={"flex"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      marginTop={5}
+      container
+    >
+      <Grid
+        margin={"auto"}
+        display={"flex"}
+        flexDirection={"column"}
+        alignSelf={"stretch"}
+        gap={"40px"}
+        alignItems={"center"}
+        item
+        xs={10}
+      >
+        <p style={Title}>{props.title}</p>
+        <p
+          style={{
+            ...TextFontSize20LineHeight30, //*global attribute
+            alignSelf: "stretch",
+            textAlign: "center",
+            padding: "0 5px",
+            textWrap: "balance",
+          }}
+        >
+          {props.message}{" "}
+        </p>
+        <Link to={`${props.link}`}>
+          {" "}
+          <Button style={props.button}>
+            <p style={props.paragraphStyle}>{props.paragraphText}</p>
+          </Button>
+        </Link>
+      </Grid>
+    </Grid>
+  );
+};
+
+BannerMsg.propTypes = {
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  button: PropTypes.string.isRequired,
+  paragraphStyle: PropTypes.string.isRequired,
+  paragraphText: PropTypes.string.isRequired,
+};
+export default BannerMsg;
--- src/pages/inventory/MainPage.jsx
+++ src/pages/inventory/MainPage.jsx
@@ -1,14 +1,13 @@
-import {
-  Grid,
-  InputAdornment,
-  OutlinedInput,
-  Typography
-} from "@mui/material";
+import { Grid, InputAdornment, OutlinedInput, Typography } from "@mui/material";
 import { Divider } from "antd";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { BluePlusIcon, MagnifyIcon, WhiteCirclePlusIcon } from "../../components/icons/Icons";
+import {
+  BluePlusIcon,
+  MagnifyIcon,
+  WhiteCirclePlusIcon,
+} from "../../components/icons/Icons";
 import { BlueButton } from "../../styles/global/BlueButton";
 import { BlueButtonText } from "../../styles/global/BlueButtonText";
 import { LightBlueButton } from "../../styles/global/LightBlueButton";
@@ -18,9 +17,29 @@ import { OutlinedInputStyle } from "../../styles/global/OutlinedInputStyle";
 import { Title } from "../../styles/global/Title";
 import "../../styles/global/ant-select.css";
 import ItemTable from "./table/ItemTable";
+import { useQuery } from "@tanstack/react-query";
+import { devitrakApi } from "../../api/devitrakApi";
+import { useEffect } from "react";
+import BannerMsg from "../../components/utils/BannerMsg";
 const MainPage = () => {
-  const { user } = useSelector((state) => state.admin)
+  const { user } = useSelector((state) => state.admin);
   const { register, watch } = useForm();
+  const inventoryQuery = useQuery({
+    queryKey: ["itemsList"],
+    queryFn: () =>
+      devitrakApi.post("/db_item/consulting-item", {
+        company: user.company,
+      }),
+    enabled: false,
+    refetchOnMount: false,
+  });
+  useEffect(() => {
+    const controller = new AbortController();
+    inventoryQuery.refetch();
+    return () => {
+      controller.abort();
+    };
+  }, []);
 
   return (
     <Grid
@@ -69,38 +88,26 @@ const MainPage = () => {
           lg={6}
         >
           <Link to="/inventory/edit-group">
-            <button
-              style={{ ...LightBlueButton, width: "fit-content" }}
-            >
-              <p
-                style={{...LightBlueButtonText, textTransform:"none"}}
-              >
+            <button style={{ ...LightBlueButton, width: "fit-content" }}>
+              <p style={{ ...LightBlueButtonText, textTransform: "none" }}>
                 Update a group of device
               </p>
             </button>
           </Link>
           <Link to="/inventory/new-bulk-items">
-            <button
-              style={{ ...BlueButton, width: 'fit-content' }}
-            >
+            <button style={{ ...BlueButton, width: "fit-content" }}>
               <WhiteCirclePlusIcon style={{ height: "21px", margin: "auto" }} />
               &nbsp;
-              <p
-                style={{...BlueButtonText, textTransform:"none"}}
-              >
+              <p style={{ ...BlueButtonText, textTransform: "none" }}>
                 Add a group of devices
               </p>
             </button>
           </Link>
           <Link to="/inventory/new-item">
-            <button
-              style={{ ...LightBlueButton, width: "fit-content" }}
-            >
+            <button style={{ ...LightBlueButton, width: "fit-content" }}>
               <BluePlusIcon />
               &nbsp;
-              <p
-                style={{...LightBlueButtonText, textTransform:"none"}}
-              >
+              <p style={{ ...LightBlueButtonText, textTransform: "none" }}>
                 Add one device
               </p>
             </button>
@@ -120,43 +127,80 @@ const MainPage = () => {
         <Grid textAlign={"right"} item xs={4}></Grid>
       </Grid>
       <Divider />
-      <Grid
-        display={'flex'}
-        justifyContent={'flex-start'}
-        alignItems={"center"}
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={12}
-      >
-        <p style={{ ...Title, fontSize: "28px", padding: 0, width: "fit-content" }}>Search inventory:&nbsp;</p>
-        <Grid item xs sm md lg>
-          <OutlinedInput
-            {...register("searchItem")}
-            style={OutlinedInputStyle}
-            fullWidth
-            placeholder="Search device here"
-            startAdornment={
-              <InputAdornment position="start">
-                <MagnifyIcon />
-              </InputAdornment>
-            }
-          />
-        </Grid>
-
-      </Grid>
-      <Grid container>
+      {inventoryQuery?.data?.data?.items.length > 0 ? (
+        <>
+          <Grid
+            display={"flex"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
+          >
+            <p
+              style={{
+                ...Title,
+                fontSize: "28px",
+                padding: 0,
+                width: "fit-content",
+              }}
+            >
+              Search inventory:&nbsp;
+            </p>
+            <Grid item xs sm md lg>
+              <OutlinedInput
+                {...register("searchItem")}
+                style={OutlinedInputStyle}
+                fullWidth
+                placeholder="Search device here"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <MagnifyIcon />
+                  </InputAdornment>
+                }
+              />
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
+              item
+              xs={12}
+            >
+              <ItemTable searchItem={watch("searchItem")} />
+            </Grid>
+          </Grid>
+        </>
+      ) : (
         <Grid
+          textAlign={"right"}
           display={"flex"}
           justifyContent={"center"}
           alignItems={"center"}
+          gap={1}
           item
           xs={12}
+          sm={12}
+          md={10}
+          lg={10}
         >
-          <ItemTable searchItem={watch("searchItem")} />
+          <BannerMsg
+            props={{
+              title: "Add to your inventory",
+              message:
+                "Creating an event will let you assign and manage devices, as well as staff to an event with a start and end date. You will also be able to assign devices to consumers, collect retain deposits, collect fees for damaged devices, and keep track of your full inventory.",
+              link: "/inventory/new-item",
+              button: BlueButton,
+              paragraphStyle: BlueButtonText,
+              paragraphText: "Add to inventory",
+            }}
+          />
         </Grid>
-      </Grid>
+      )}{" "}
     </Grid>
   );
 };
