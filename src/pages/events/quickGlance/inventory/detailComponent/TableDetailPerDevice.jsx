43db21b18b196ecbd6d29a688a2a786562d3f2cd
Gustavo <grodriguez@contextglobal.com>
2024-06-06 17:17:25 -0400

    refactoring component and adding new column in table to visualize the status of device in each consumer in it was assigned to.

--- src/pages/events/quickGlance/inventory/detailComponent/TableDetailPerDevice.jsx
+++ src/pages/events/quickGlance/inventory/detailComponent/TableDetailPerDevice.jsx
@@ -1,183 +1,230 @@
-import { Typography } from "@mui/material";
 import { Table } from "antd";
 import { useCallback, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { onAddCustomer } from "../../../../../store/slices/stripeSlice";
-import { onAddCustomerInfo } from "../../../../../store/slices/customerSlice";
+import { useNavigate } from "react-router-dom";
 import { devitrakApi } from "../../../../../api/devitrakApi";
-import '../../../../../styles/global/ant-table.css'
+import { onAddCustomerInfo } from "../../../../../store/slices/customerSlice";
+import { onAddCustomer } from "../../../../../store/slices/stripeSlice";
+import "../../../../../styles/global/ant-table.css";
 const TableDetailPerDevice = ({ searching }) => {
-    const { deviceInfoSelected } = useSelector((state) => state.devicesHandle);
-    const { event } = useSelector((state) => state.event);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const { deviceInfoSelected } = useSelector((state) => state.devicesHandle);
+  const { event } = useSelector((state) => state.event);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [assignedDeviceList, setAssignedDeviceList] = useState([])
-    const assignedDeviceTracking = useCallback(async () => {
-        const respo = await devitrakApi.post('/receiver/receiver-assigned-list', {
-            'device.serialNumber': deviceInfoSelected.serialNumber,
-            eventSelected: event.eventInfoDetail.eventName,
-            provider: event.company
-        })
-        if (respo.data.ok) {
-            const result = [...assignedDeviceList, ...respo.data.listOfReceivers]
-            setAssignedDeviceList(result)
-        }
-    }, [])
-
-    const [defectedDevicesList, setDefectedDevicesList] = useState([])
-    const defecedDeviceTracking = useCallback(async () => {
-        const respo = await devitrakApi.post('/receiver/list-receiver-returned-issue', {
-            'device.serialNumber': deviceInfoSelected.serialNumber,
-            eventSelected: event.eventInfoDetail.eventName,
-            provider: event.company
-        })
-        if (respo.data.ok) {
-            const result = [...defectedDevicesList, ...respo.data.record]
-            setDefectedDevicesList(result)
-        }
-    }, [])
-
-    useEffect(() => {
-        const controller = new AbortController()
-        assignedDeviceTracking()
-        defecedDeviceTracking()
-        return () => {
-            controller.abort()
-        }
-    }, [assignedDeviceList.length, defectedDevicesList.length])
-
-    const sortingAssignedDeviceTrack = useCallback(() => {
-        const addingKey = new Set()
-        for (let data of assignedDeviceList) {
-            addingKey.add({ key: data.id, ...data })
-        }
-        return Array.from(addingKey).reverse()
-    }, [assignedDeviceList.length])
-
-    const finalResult = () => {
-        if (!searching || String(searching).length < 1) {
-            return [...sortingAssignedDeviceTrack(), ...defectedDevicesList]
-        } else {
-            const data = [...sortingAssignedDeviceTrack(), ...defectedDevicesList]
-            return data.filter(element => String(element.user).toLowerCase().includes(String(searching).toLowerCase()) || String(element.eventSelected[0]).toLowerCase().includes(String(searching).toLowerCase()))
-        }
+  const [assignedDeviceList, setAssignedDeviceList] = useState([]);
+  const assignedDeviceTracking = useCallback(async () => {
+    const respo = await devitrakApi.post("/receiver/receiver-assigned-list", {
+      "device.serialNumber": deviceInfoSelected.serialNumber,
+      eventSelected: event.eventInfoDetail.eventName,
+      provider: event.company,
+    });
+    if (respo.data.ok) {
+      const result = [...assignedDeviceList, ...respo.data.listOfReceivers];
+      setAssignedDeviceList(result);
     }
+  }, []);
 
-    const handleConsumerNavigation = async (record) => {
-        const consumerData = await devitrakApi.post("/auth/users", {
-            email: record.user
-        })
-        if (consumerData.data) {
-            const consumer = consumerData.data.users.at(-1)
-            let userFormatData = {
-                uid: consumer.id ?? consumer.uid,
-                name: consumer.name,
-                lastName: consumer.lastName,
-                email: consumer.email,
-                phoneNumber: consumer.phoneNumber,
-            };
-            dispatch(onAddCustomerInfo(userFormatData));
-            dispatch(onAddCustomer(userFormatData));
-            await navigate(
-                `/events/event-attendees/${userFormatData.uid}/transactions-details`
-            );
-        }
-    };
-    const columns = [
-        {
-            title: "Event",
-            dataIndex: "eventSelected",
-            align: "left",
-            sorter: {
-                compare: (a, b) =>
-                    ("" + a.eventSelected).localeCompare(b.eventSelected),
-            },
-            render: (eventSelected) => (
-                <span
-                    onClick={() => navigate("/events/event-quickglance")}
-                    style={{ margin: "auto", cursor: "pointer" }}
-                >
-                    <Typography
-                        textTransform={"none"}
-                        fontSize={"14px"}
-                        fontFamily={"Inter"}
-                        fontStyle={"normal"}
-                        fontWeight={400}
-                        lineHeight={"20px"}
-                        color={"var(--blue-dark-600, #155EEF)"}
-                    >
-                        {eventSelected}
-                    </Typography>
-                </span>
-            ),
-        },
-        {
-            title: "Status",
-            dataIndex: "status",
-            width: "33%",
-            align: "left",
-            responsive: ["md", "lg"],
-            sorter: {
-                compare: (a, b) => ("" + a.status).localeCompare(b.status),
-            },
-            render: (status) => (
-                <span style={{ margin: "auto" }}>
-                    <Typography
-                        textTransform={"none"}
-                        fontSize={"14px"}
-                        fontFamily={"Inter"}
-                        fontStyle={"normal"}
-                        fontWeight={400}
-                        lineHeight={"20px"}
-                    >
-                        {typeof status === "string" ? status : "Operational"}
-                    </Typography>
-                </span>
-            ),
-        },
-        {
-            title: "Consumer",
-            dataIndex: "user",
-            width: "33%",
-            align: "left",
-            sorter: {
-                compare: (a, b) => ("" + a.user).localeCompare(b.user),
-            },
-            render: (_, record) => (
-                <span
-                    onClick={() => handleConsumerNavigation(record)}
-                    style={{ margin: "auto", cursor: "pointer" }}
-                >
-                    <Typography
-                        textTransform={"none"}
-                        fontSize={"14px"}
-                        fontFamily={"Inter"}
-                        fontStyle={"normal"}
-                        fontWeight={400}
-                        lineHeight={"20px"}
-                        color={"var(--blue-dark-600, #155EEF)"}
-                    >
-                        {record.user}
-                    </Typography>
-                </span>
-            ),
-        },
-    ];
-
-    return (
-        <Table
-            sticky
-            size="large"
-            columns={columns}
-            dataSource={finalResult()}
-            pagination={{
-                position: ["bottomCenter"],
-            }}
-            className="table-ant-customized"
-        />
+  const [defectedDevicesList, setDefectedDevicesList] = useState([]);
+  const defecedDeviceTracking = useCallback(async () => {
+    const respo = await devitrakApi.post(
+      "/receiver/list-receiver-returned-issue",
+      {
+        "device.serialNumber": deviceInfoSelected.serialNumber,
+        eventSelected: event.eventInfoDetail.eventName,
+        provider: event.company,
+      }
     );
+    if (respo.data.ok) {
+      const result = [...defectedDevicesList, ...respo.data.record];
+      setDefectedDevicesList(result);
+    }
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    assignedDeviceTracking();
+    defecedDeviceTracking();
+    return () => {
+      controller.abort();
+    };
+  }, [assignedDeviceList.length, defectedDevicesList.length]);
+
+  const sortingAssignedDeviceTrack = useCallback(() => {
+    const addingKey = new Set();
+    for (let data of assignedDeviceList) {
+      addingKey.add({ key: data.id, ...data });
+    }
+    return Array.from(addingKey).reverse();
+  }, [assignedDeviceList.length]);
+  const finalResult = () => {
+    if (!searching || String(searching).length < 1) {
+      return [...sortingAssignedDeviceTrack(), ...defectedDevicesList];
+    } else {
+      const data = [...sortingAssignedDeviceTrack(), ...defectedDevicesList];
+      return data.filter(
+        (element) =>
+          String(element.user)
+            .toLowerCase()
+            .includes(String(searching).toLowerCase()) ||
+          String(element.eventSelected[0])
+            .toLowerCase()
+            .includes(String(searching).toLowerCase())
+      );
+    }
+  };
+
+  const handleConsumerNavigation = async (record) => {
+    const consumerData = await devitrakApi.post("/auth/users", {
+      email: record.user,
+    });
+    if (consumerData.data) {
+      const consumer = consumerData.data.users.at(-1);
+      let userFormatData = {
+        uid: consumer.id ?? consumer.uid,
+        name: consumer.name,
+        lastName: consumer.lastName,
+        email: consumer.email,
+        phoneNumber: consumer.phoneNumber,
+      };
+      dispatch(onAddCustomerInfo(userFormatData));
+      dispatch(onAddCustomer(userFormatData));
+      await navigate(
+        `/events/event-attendees/${userFormatData.uid}/transactions-details`
+      );
+    }
+  };
+  const renderRowStyle = {
+    textTransform: "none",
+    fontSize: "14px",
+    fontFamily: "Inter",
+    fontStyle: "normal",
+    fontWeight: 400,
+    lineHeight: "20px",
+  };
+  const columns = [
+    {
+      title: "Event",
+      dataIndex: "eventSelected",
+      align: "left",
+      sorter: {
+        compare: (a, b) =>
+          ("" + a.eventSelected).localeCompare(b.eventSelected),
+      },
+      render: (eventSelected) => (
+        <button
+          onClick={() => navigate("/events/event-quickglance")}
+          style={{
+            margin: "auto",
+            cursor: "pointer",
+            outline: "none",
+            backgroundColor: "transparent",
+          }}
+        >
+          <p
+            style={{
+              ...renderRowStyle,
+              color: "var(--blue-dark-600, #155EEF)",
+            }}
+          >
+            {eventSelected}
+          </p>
+        </button>
+      ),
+    },
+    {
+      title: "Status",
+      dataIndex: "device.status",
+      align: "left",
+      responsive: ["md", "lg"],
+      sorter: {
+        compare: (a, b) => ("" + a.status).localeCompare(b.status),
+      },
+      render: (_, record) => (
+        <span
+          style={{
+            margin: "auto",
+          }}
+        >
+          <p
+            style={{
+              ...renderRowStyle,
+              backgroundColor: record.device.status
+                ? "var(--orange-dark-50, #FFF4ED)"
+                : "var(--success-50, #ECFDF3)",
+              width: "fit-content",
+              padding: "5px 8px",
+              borderRadius: "8px",
+              color: record.device.status
+                ? "var(--orange-700, #B93815)"
+                : "var(--success-700, #027A48)",
+            }}
+          >
+            {record.device.status ? "In-use" : "Returned"}
+          </p>
+        </span>
+      ),
+    },
+    {
+      title: "Condition",
+      dataIndex: "status",
+      align: "left",
+      responsive: ["md", "lg"],
+      sorter: {
+        compare: (a, b) => ("" + a.status).localeCompare(b.status),
+      },
+      render: (status) => (
+        <span style={{ margin: "auto" }}>
+          <p style={renderRowStyle}>
+            {typeof status === "string" ? status : "Operational"}
+          </p>
+        </span>
+      ),
+    },
+    {
+      title: "Consumer",
+      dataIndex: "user",
+      width: "33%",
+      align: "left",
+      sorter: {
+        compare: (a, b) => ("" + a.user).localeCompare(b.user),
+      },
+      render: (_, record) => (
+        <button
+          onClick={() => handleConsumerNavigation(record)}
+          style={{
+            margin: 0,
+            padding: 0,
+            cursor: "pointer",
+            backgroundColor: "transparent",
+            outline: "none",
+          }}
+        >
+          <p
+            style={{
+              ...renderRowStyle,
+              color: "var(--blue-dark-600, #155EEF)",
+            }}
+          >
+            {record.user}
+          </p>
+        </button>
+      ),
+    },
+  ];
+
+  return (
+    <Table
+      sticky
+      size="large"
+      columns={columns}
+      dataSource={finalResult()}
+      pagination={{
+        position: ["bottomCenter"],
+      }}
+      className="table-ant-customized"
+    />
+  );
 };
 
 export default TableDetailPerDevice;
