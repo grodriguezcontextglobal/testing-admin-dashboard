5907c53d4113a492fa9467bda2a2375d5c0eaf5d
Gustavo <grodriguez@contextglobal.com>
2024-06-12 08:29:36 -0400

    adding buttons to improve UX providing options to check information in detail or take action.

--- src/pages/search/utils/CardDeviceFound.jsx
+++ src/pages/search/utils/CardDeviceFound.jsx
@@ -1,56 +1,104 @@
-import { Card } from 'antd'
-const CardDeviceFound = ({ props, fn }) => {
-    return (
-        <Card onClick={() => fn(props)} style={{
-            borderRadius: '12px',
-            border: '1px solid #D0D5DD',
-            background: '#FFF',
-            boxShadow: '0px 2px 4px 0px rgba(0, 0, 0, 0.05)',
-            display: 'flex',
-            padding: '5px',
-            flexDirection: 'column',
-            alignItems: 'flex-start',
-            gap: '20px',
-            cursor: "pointer"
+import { Button, Card, Divider } from "antd";
+import { PropTypes } from "prop-types";
+import { BlueButtonText } from "../../../styles/global/BlueButtonText";
+import { BlueButton } from "../../../styles/global/BlueButton";
+import CenteringGrid from "../../../styles/global/CenteringGrid";
+const CardDeviceFound = ({ props, fn, returnFn, loadingStatus }) => {
+  return (
+    <Card
+      key={props.data?._id}
+      style={{
+        borderRadius: "12px",
+        border: "1px solid #D0D5DD",
+        background: "#FFF",
+        boxShadow: "0px 2px 4px 0px rgba(0, 0, 0, 0.05)",
+        display: "flex",
+        padding: "5px",
+        flexDirection: "column",
+        alignItems: "flex-start",
+        gap: "20px",
+      }}
+      styles={{
+        body: {
+          padding: "5px 20px 20px 20px",
+        },
+      }}
+    >
+      <div style={{ width: "100%", textAlign: "left" }}>
+        {props.image ? (
+          <img
+            src={props?.image}
+            alt={`${props?.image}`}
+            style={{ objectFit: "cover", height: "auto", width: "70%" }}
+          />
+        ) : (
+          ""
+        )}
+      </div>
+      <div
+        style={{
+          width: "100%",
+          textAlign: "left",
+          color: "var(--Gray-900, #101828)",
+          fontFamily: "Inter",
+          fontSize: "18px",
+          fontStyle: " normal",
+          fontWeight: 600,
+          lineHeight: "28px" /* 155.556% */,
+          textWrap: "pretty",
         }}
-            styles={{
-                body: {
-                    padding: '5px 20px 20px 20px',
-                }
-            }}>
-            <div style={{ width: "100%", textAlign: "left" }}>
-                    {
-                        props.image ?
-                            <img src={props?.image} alt={`${props?.image}`} style={{ objectFit: "cover",  height: "auto", width: "70%" }} /> :
-                            ''
-                    }
-            </div>
-            <div style={{
-                width: "100%",
-                textAlign: "left",
-                color: 'var(--Gray-900, #101828)',
-                fontFamily: 'Inter',
-                fontSize: '18px',
-                fontStyle: ' normal',
-                fontWeight: 600,
-                lineHeight: '28px', /* 155.556% */
-                textWrap: "pretty"
-            }}>
-                {props?.serialNumber}
-            </div>
-            <div style={{
-                width: "100%",
-                textAlign: "left",
-                color: 'var(--Gray-600, #475467)',
-                fontFamily: 'Inter',
-                fontSize: '16px',
-                fontStyle: 'normal',
-                fontWeight: 400,
-                lineHeight: '24px', /* 150% */
-                textWrap: "pretty"
-            }}>{props?.type}</div>
-        </Card >
-    )
-}
+      >
+        {props?.serialNumber}
+      </div>
+      <div
+        style={{
+          width: "100%",
+          textAlign: "left",
+          color: "var(--Gray-600, #475467)",
+          fontFamily: "Inter",
+          fontSize: "16px",
+          fontStyle: "normal",
+          fontWeight: 400,
+          lineHeight: "24px" /* 150% */,
+          textWrap: "pretty",
+        }}
+      >
+        {props?.type}
+      </div>
+      <Divider style={{margin:"5px 0 10px"}} />
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          gap: "5px",
+        }}
+      >
+        <Button
+          onClick={() => fn(props)}
+          style={{ ...BlueButton, ...CenteringGrid, width: "100%" }}
+        >
+          <p style={BlueButtonText}>Details</p>
+        </Button>
+        <Button
+          loading={loadingStatus}
+          onClick={() => returnFn(props)}
+          style={{ ...BlueButton, ...CenteringGrid, width: "100%" }}
+        >
+          <p style={BlueButtonText}>Return</p>
+        </Button>
+      </div>
+    </Card>
+  );
+};
 
-export default CardDeviceFound
+export default CardDeviceFound;
+
+CardDeviceFound.propTypes = {
+  props: PropTypes.object.isRequired,
+  fn: PropTypes.func,
+  image: PropTypes.string.isRequired,
+  serialNumber: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+};
