da1977646f7706888b912b54c1dfb80424a90c2a
Gustavo <grodriguez@contextglobal.com>
2024-06-13 17:39:37 -0400

    styles set for adjusting screen as needed.

--- src/components/general/FooterComponent.jsx
+++ src/components/general/FooterComponent.jsx
@@ -1,22 +1,23 @@
-import { Grid, Typography } from "@mui/material";
 
 const FooterComponent = () => {
   return (
-    <Grid display={'flex'} justifyContent={'center'} alignItems={'center'} container>
-      <Grid display={'flex'} justifyContent={'flex-start'} alignItems={'center'} style={{margin:"0 auto 3rem"}} item xs={12} sm={12} md={11} lg={11}>
-          <Typography
-            textAlign={"left"}
-            fontWeight={400}
-            fontFamily={"Inter"}
-            fontSize={"14px"}
-            fontStyle={"normal"}
-            lineHeight={"20px"}
-            color={"var(--gray-600, #475467)"}
-          >
-            © Devitrak {new Date().getFullYear()}
-          </Typography>
-      </Grid>
-    </Grid>
+    // <Grid display={'flex'} justifyContent={'center'} alignItems={'center'} container>
+    //   <Grid display={'flex'} justifyContent={'flex-start'} alignItems={'center'} style={{margin:"0 auto 3rem"}} item xs={12} sm={12} md={11} lg={11}>
+    <p
+      style={{
+        textAlign: "left",
+        fontWeight: 400,
+        fontFamily: "Inter",
+        fontSize: "14px",
+        fontStyle: "normal",
+        lineHeight: "20px",
+        color: "var(--gray-600, #475467)",
+      }}
+    >
+      © Devitrak {new Date().getFullYear()}
+    </p>
+    //   </Grid>
+    // </Grid>
   );
 };
 
