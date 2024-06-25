1a18fcb937991df21965395338bb4f01d4a985c0
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-06-25 14:44:16 -0400

    adding new condition for redirecting page for coming path when new company is set up

--- src/components/utils/RedirectionPage.jsx
+++ src/components/utils/RedirectionPage.jsx
@@ -1,10 +1,17 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import CenteringGrid from "../../styles/global/CenteringGrid";
 import Loading from "../animation/Loading";
 
 const RedirectionPage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
   return (
     <div style={CenteringGrid}>
       <Loading />
+      {location.pathname === "/register/company-setup" &&
+        setTimeout(() => {
+          navigate("/");
+        }, 2000)}
     </div>
   );
 };
