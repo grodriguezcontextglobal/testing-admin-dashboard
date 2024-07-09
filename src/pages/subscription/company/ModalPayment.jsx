e038b91faeb65d251454e4d96c42472572cdd2dd
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-07-09 04:52:09 -0400

    modal to be displayed after tier selection to collect payment cc information for subscription.

---
+++ src/pages/subscription/company/ModalPayment.jsx
@@ -0,0 +1,48 @@
+import { Modal } from "antd";
+import { useState } from "react";
+import { PropTypes } from "prop-types";
+import { CheckoutElementCompanySubscription } from "../../../components/stripe/elements/CheckoutElementCompanySubscription";
+const ModalPayment = ({
+  clientSecret,
+  setClientSecret,
+  total,
+  title,
+  type,
+}) => {
+  const [openModal, setOpenModal] = useState(`${clientSecret.length}`);
+  const closeModal = () => {
+    setOpenModal(false);
+    setClientSecret(null);
+  };
+  return (
+    <Modal
+      title={title}
+      centered
+      open={openModal}
+      onOk={() => closeModal()}
+      onCancel={() => closeModal()}
+      footer={[]}
+    >
+      {/* <StripeCheckoutElementAdmin
+        clientSecret={clientSecret}
+        total={total}
+        type={type}
+      /> */}
+      <CheckoutElementCompanySubscription
+        clientSecret={clientSecret}
+        total={total}
+        type={type}
+      />
+    </Modal>
+  );
+};
+
+export default ModalPayment;
+
+ModalPayment.propTypes = {
+  clientSecret: PropTypes.string,
+  setClientSecret: PropTypes.func,
+  total: PropTypes.string,
+  title: PropTypes.string,
+  type: PropTypes.string,
+};
