9de54137a164239ad88b0996aacbc12b80cf329f
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-07-17 08:32:07 -0400

    adding new component for device assignment to customer as lease

--- src/pages/consumers/components/CardActionsButton.jsx
+++ src/pages/consumers/components/CardActionsButton.jsx
@@ -1,35 +1,76 @@
-import { Card } from "antd"
-import { AlarmIcon, WhiteCirclePlusIcon } from "../../../components/icons/Icons"
-import { BlueButton } from "../../../styles/global/BlueButton"
-import { BlueButtonText } from "../../../styles/global/BlueButtonText"
-import { GrayButton } from "../../../styles/global/GrayButton"
-import GrayButtonText from "../../../styles/global/GrayButtonText"
-import { CardStyle } from "../../../styles/global/CardStyle"
-import { useState } from "react"
-import SingleEmailNotification from "../../../components/notification/email/SingleEmail"
+import { Card } from "antd";
+import {
+  AlarmIcon,
+  WhiteCirclePlusIcon,
+} from "../../../components/icons/Icons";
+import { BlueButton } from "../../../styles/global/BlueButton";
+import { BlueButtonText } from "../../../styles/global/BlueButtonText";
+import { GrayButton } from "../../../styles/global/GrayButton";
+import GrayButtonText from "../../../styles/global/GrayButtonText";
+import { CardStyle } from "../../../styles/global/CardStyle";
+import { useState } from "react";
+import SingleEmailNotification from "../../../components/notification/email/SingleEmail";
+import ModalAssignDeviceToConsumer from "../action/ModalAssignDeviceToConsumer";
 
 const CardActionsButton = () => {
-    const [notificationActivation, setNotificationActivation] = useState(false)
-    return (
-        <>
-            <Card
-                style={{ ...CardStyle, alignSelf: "flex-start", width: "100%", }} styles={{
-                    body: {
-                        padding: "10px 0 10px 10px"
-                    }
-                }}>
-                <div style={{ width: "100%", alignSelf:"stretch" }}>
-                    <button style={{ ...BlueButton, width: "100%", display: "flex", justifyContent: "center", alignItems: "center", margin: "0 0 12.5px 0" }}><WhiteCirclePlusIcon />&nbsp;<p style={{ ...BlueButtonText, textAlign: "center" }}>Assign more devices</p></button>
-                    <button onClick={() => setNotificationActivation(true)} style={{ ...GrayButton, width: "100%", display: "flex", justifyContent: "center", alignItems: "center", margin: "0" }}><AlarmIcon />&nbsp;<p style={{ ...GrayButtonText, textAlign: "center" }}>Send notification to return</p></button>
-                </div>
-            </Card>
-            {notificationActivation && <SingleEmailNotification
-                customizedEmailNotificationModal={notificationActivation}
-                setCustomizedEmailNotificationModal={setNotificationActivation}
-            />}
+  const [notificationActivation, setNotificationActivation] = useState(false);
+  const [assignDevice, setAssignDevice] = useState(false);
+  return (
+    <>
+      <Card
+        style={{ ...CardStyle, alignSelf: "flex-start", width: "100%" }}
+        styles={{
+          body: {
+            padding: "10px 0 10px 10px",
+          },
+        }}
+      >
+        <div style={{ width: "100%", alignSelf: "stretch" }}>
+          <button
+            onClick={() => setAssignDevice(true)}
+            style={{
+              ...BlueButton,
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "0 0 12.5px 0",
+            }}
+          >
+            <WhiteCirclePlusIcon />
+            &nbsp;
+            <p style={{ ...BlueButtonText, textAlign: "center" }}>
+              Assign more devices
+            </p>
+          </button>
+          <button
+            onClick={() => setNotificationActivation(true)}
+            style={{
+              ...GrayButton,
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              margin: "0",
+            }}
+          >
+            <AlarmIcon />
+            &nbsp;
+            <p style={{ ...GrayButtonText, textAlign: "center" }}>
+              Send notification to return
+            </p>
+          </button>
+        </div>
+      </Card>
+      {notificationActivation && (
+        <SingleEmailNotification
+          customizedEmailNotificationModal={notificationActivation}
+          setCustomizedEmailNotificationModal={setNotificationActivation}
+        />
+      )}
+      {assignDevice && <ModalAssignDeviceToConsumer assignDevice={assignDevice} setAssignDevice={setAssignDevice}/>}
+    </>
+  );
+};
 
-        </>
-    )
-}
-
-export default CardActionsButton
+export default CardActionsButton;
