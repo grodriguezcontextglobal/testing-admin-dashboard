0ccdfb9a52bad342909f6db1fbb6024af1210fd3
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-08-01 17:59:10 -0400

    adding function to hash index.html for versioning.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import million from "million/compiler";
+import { createHash } from "crypto";
 
+function getHash(text) {
+  return createHash("sha256").update(text).digest("hex").substring(0, 8);
+}
+
+const htmlHashPlugin = {
+  name: "html-hash",
+  enforce: "post",
+  generateBundle(options, bundle) {
+    const indexHtml = bundle["index.html"];
+    indexHtml.fileName = `index.${getHash(indexHtml.source)}.html`;
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react()],
-})
+  plugins: [million.vite({ auto: true }), react(), htmlHashPlugin],
+});
