51912d1703ee1be378d7e694c0f4028aaa4f688f
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-08-09 17:10:07 -0400

    checking reset version

---
+++ src/components/utils/ChartsRenderer.jsx
@@ -0,0 +1,60 @@
+import { PropTypes } from "prop-types";
+import ReactECharts from "echarts-for-react";
+import { Subtitle } from "../../styles/global/Subtitle";
+
+const ChartsRenderer = ({
+  dataToRender,
+  title,
+  orient,
+  top,
+  right,
+  showLabel,
+  legendAlign,
+  colors
+}) => {
+  const option = {
+    color: colors, // Add your custom colors here
+    tooltip: {
+      trigger: "item",
+    },
+    legend: {
+      top: top,
+      right: right,
+      orient: orient,
+      align: legendAlign,
+      textStyle:{Subtitle}
+    },
+    series: [
+      {
+        name: title,
+        type: "pie",
+        radius: ["40%", "60%"],
+        avoidLabelOverlap: false,
+        label: {
+          show: false,
+          position: "center",
+        },
+        emphasis: {
+          label: {
+            show: showLabel,
+            fontSize: 14,
+            fontWeight: "500",
+          },
+        },
+        labelLine: {
+          show: false,
+        },
+        data: [...dataToRender],
+      },
+    ],
+  };
+
+  return <ReactECharts option={option} style={{width:"100%" }} />;
+};
+
+ChartsRenderer.propTypes = {
+  dataToRender: PropTypes.array,
+  title: PropTypes.string,
+};
+
+export default ChartsRenderer;
