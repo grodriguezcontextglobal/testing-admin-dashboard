d221f86070e06a26783c63debb7c15cbcdbd5f59
grodriguezcontextglobal <grodriguez@contextglobal.com>
2024-08-10 10:24:20 -0400

    remove function to hash index html

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import million from "million/compiler";
-import { createHash } from "crypto";
+// import { createHash } from "crypto";
 
-function getHash(text) {
-  return createHash("sha256").update(text).digest("hex").substring(0, 8);
-}
+// function getHash(text) {
+//   return createHash("sha256").update(text).digest("hex").substring(0, 8);
+// }
 
-const htmlHashPlugin = {
-  name: "html-hash",
-  enforce: "post",
-  generateBundle(options, bundle) {
-    const indexHtml = bundle["index.html"];
-    indexHtml.fileName = `index.${getHash(indexHtml.source)}.html`;
-  },
-};
+// const htmlHashPlugin = {
+//   name: "html-hash",
+//   enforce: "post",
+//   generateBundle(options, bundle) {
+//     const indexHtml = bundle["index.html"];
+//     indexHtml.fileName = `index.${getHash(indexHtml.source)}.html`;
+//   },
+// };
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [million.vite({ auto: true }), react(), htmlHashPlugin],
+  plugins: [million.vite({ auto: true }), react()],
 });
