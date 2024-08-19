0786f1b72be67efe86d16d35eb17c9e03a441a45
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-08-19 16:22:57 -0400

    remove unnecessary commented code.

--- src/pages/events/newEventProcess/subscription/components/ModalAnnualPayment.jsx
+++ src/pages/events/newEventProcess/subscription/components/ModalAnnualPayment.jsx
@@ -28,3 +28,43 @@ const ModalAnnualPayment = ({
 };
 
 export default ModalAnnualPayment;
+
+
+
+
+// import { Modal } from "antd";
+// import { useState } from "react";
+// // import { StripeCheckoutElementAdmin } from "../../../../../components/stripe/elements/StripeCheckoutElementAdmin";
+// import { CheckoutElementCompanySubscription } from "../../../../../components/stripe/elements/CheckoutElementCompanySubscription";
+
+// const ModalAnnualPayment = ({
+//   clientSecret,
+//   setClientSecret,
+//   total,
+//   title,
+//   type
+// }) => {
+//   const [openModal, setOpenModal] = useState(`${clientSecret.length}`);
+//   const closeModal = () => {
+//     setOpenModal(false);
+//     setClientSecret(null);
+//   };
+//   return (
+//     <Modal
+//       title={title}
+//       centered
+//       open={openModal}
+//       onOk={() => closeModal()}
+//       onCancel={() => closeModal()}
+//       footer={[]}
+//     >
+//       <CheckoutElementCompanySubscription
+//         clientSecret={clientSecret}
+//         total={total}
+//         type={type}
+//       />
+//     </Modal>
+//   );
+// };
+
+// export default ModalAnnualPayment;
--- src/pages/events/newEventProcess/subscription/components/ModalMonthlyPayment.jsx
+++ src/pages/events/newEventProcess/subscription/components/ModalMonthlyPayment.jsx
@@ -24,3 +24,34 @@ const ModalMonthlyPayment = ({ clientSecret, setClientSecret, total }) => {
   );
 };
 export default ModalMonthlyPayment;
+
+
+
+
+
+// import { Modal } from "antd";
+// import { useState } from "react";
+// import { StripeCheckoutElementAdmin } from "../../../../../components/stripe/elements/StripeCheckoutElementAdmin";
+
+// const ModalMonthlyPayment = ({ clientSecret, setClientSecret, total }) => {
+//   const [openModal, setOpenModal] = useState(
+//     `${clientSecret?.length > 0}`
+//   );
+//   const closeModal = () => {
+//     setOpenModal(false);
+//     setClientSecret(null)
+//   };
+//   return (
+//     <Modal
+//       title="Monthly payment method"
+//       centered
+//       open={openModal}
+//       onOk={() => closeModal()}
+//       onCancel={() => closeModal()}
+//       footer={[]}
+//     >
+//       <StripeCheckoutElementAdmin clientSecret={clientSecret} total={total} />
+//     </Modal>
+//   );
+// };
+// export default ModalMonthlyPayment;
