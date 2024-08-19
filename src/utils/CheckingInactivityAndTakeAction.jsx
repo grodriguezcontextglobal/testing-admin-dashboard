352c22b9d3f069bd606a5055ef3ffa34f3698b8b
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-08-19 16:17:58 -0400

    component to check inactivity and log out if reach out limit time.

---
+++ src/utils/CheckingInactivityAndTakeAction.jsx
@@ -0,0 +1,67 @@
+import { notification } from "antd";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom"; // Assuming you're using react-router
+import { onLogout } from "../store/slices/adminSlice";
+
+const InactivityLogout = ({ children }) => {
+  const [isActive, setIsActive] = useState(true);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [api, contextHolder] = notification.useNotification();
+  const openNotification = () => {
+    api.open({
+      message: "You have been logged out due to inactivity.",
+      duration: 0,
+    });
+  };
+  useEffect(() => {
+    let logoutTimer;
+
+    // Function to reset the timer
+    const resetTimer = () => {
+      setIsActive(true);
+      clearTimeout(logoutTimer);
+      logoutTimer = setTimeout(() => {
+        handleLogout();
+      }, 5 * 60 * 1000);
+    };
+
+    // Function to handle logout
+    const handleLogout = () => {
+      setIsActive(false);
+      // Perform logout actions here (e.g., clear user data, token, etc.)
+      openNotification();
+      dispatch(onLogout());
+      localStorage.removeItem("admin-token");
+      window.location.reload; // Redirect to the login page
+    };
+
+    // Add event listeners for user activity
+    window.addEventListener("mousemove", resetTimer);
+    window.addEventListener("keydown", resetTimer);
+    window.addEventListener("scroll", resetTimer);
+    window.addEventListener("click", resetTimer);
+
+    // Start the timer
+    resetTimer();
+
+    // Cleanup on component unmount
+    return () => {
+      clearTimeout(logoutTimer);
+      window.removeEventListener("mousemove", resetTimer);
+      window.removeEventListener("keydown", resetTimer);
+      window.removeEventListener("scroll", resetTimer);
+      window.removeEventListener("click", resetTimer);
+    };
+  }, [navigate]);
+
+  return (
+    <>
+      {contextHolder}
+      {isActive ? children : null}
+    </>
+  );
+};
+
+export default InactivityLogout;
