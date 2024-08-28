0ff0fa2b57758fc558b720e8fc77de85f6c68eeb
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-08-28 16:59:56 -0400

    create class for managing control and avoid to miss needed parameter

---
+++ src/classes/deviceAssigned.jsx
@@ -0,0 +1,34 @@
+export default class DeviceAssigned {
+  constructor(
+    paymentIntent,
+    device,
+    user,
+    active,
+    eventSelected,
+    provider,
+    timeStamp,
+    company
+  ) {
+    this.paymentIntent = paymentIntent;
+    this.device = device;
+    this.user = user;
+    this.active = active;
+    this.eventSelected = eventSelected;
+    this.provider = provider;
+    this.timeStamp = timeStamp;
+    this.company = company;
+  }
+
+  render() {
+    return {
+      paymentIntent: this.paymentIntent,
+      device: this.device,
+      user: this.user,
+      active: this.active,
+      eventSelected: this.eventSelected,
+      provider: this.provider,
+      timeStamp: this.timeStamp,
+      company: this.company,
+    };
+  }
+}
