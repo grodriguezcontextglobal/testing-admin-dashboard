7c8dbe68ab36aa75afdf008cb595ab3897023d2a
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-09-26 23:18:19 -0400

    commented manual chunks

--- vite.config.js
+++ vite.config.js
@@ -8,62 +8,62 @@ export default defineConfig({
     rollupOptions: {
       output: {
         // Manual chunk splitting to control how chunks are generated
-        manualChunks(id) {
-          // Split node_modules into separate chunks
-          if (id.includes("node_modules")) {
-            // Split large libraries into separate chunks
-            if (id.includes("react") || id.includes("react-dom")) {
-              return "react";
-            }
-            if (id.includes("@mui")) {
-              return "mui";
-            }
-            if (id.includes("antd")) {
-              return "antd";
-            }
-            if (id.includes("lodash")) {
-              return "lodash";
-            }
-            if (id.includes("echarts")) {
-              return "echarts";
-            }
-            if (id.includes("echarts-for-react")) {
-              return "echarts-for-react";
-            }
-            if (id.includes("recharts")) {
-              return "recharts";
-            }
-            if (id.includes("react-datepicker")) {
-              return "react-datepicker";
-            }
-            if (id.includes("react-phone-number-input")) {
-              return "react-phone-number-input";
-            }
-            if (id.includes("@uidotdev")) {
-              return "uidotdev";
-            }
-            if (id.includes("@stripe")) {
-              return "stripe";
-            }
-            if (id.includes("react-hook-form")) {
-              return "react-hook-form";
-            }
-            if (id.includes("yup")) {
-              return "yup";
-            }
-            if (id.includes("bcryptjs")) {
-              return "bcryptjs";
-            }
-            return "vendor";
-          }
-          // Split your app's pages or large components
-          if (id.includes("/src/pages/")) {
-            return `page-${id.split("/src/pages/")[1].split("/")[0]}`;
-          }
-          if (id.includes("/src/components/")) {
-            return `component-${id.split("/src/components/")[1].split("/")[0]}`;
-          }
-        },
+        // manualChunks(id) {
+        //   // Split node_modules into separate chunks
+        //   if (id.includes("node_modules")) {
+        //     // Split large libraries into separate chunks
+        //     if (id.includes("react") || id.includes("react-dom")) {
+        //       return "react";
+        //     }
+        //     if (id.includes("@mui")) {
+        //       return "mui";
+        //     }
+        //     if (id.includes("antd")) {
+        //       return "antd";
+        //     }
+        //     if (id.includes("lodash")) {
+        //       return "lodash";
+        //     }
+        //     if (id.includes("echarts")) {
+        //       return "echarts";
+        //     }
+        //     if (id.includes("echarts-for-react")) {
+        //       return "echarts-for-react";
+        //     }
+        //     if (id.includes("recharts")) {
+        //       return "recharts";
+        //     }
+        //     if (id.includes("react-datepicker")) {
+        //       return "react-datepicker";
+        //     }
+        //     if (id.includes("react-phone-number-input")) {
+        //       return "react-phone-number-input";
+        //     }
+        //     if (id.includes("@uidotdev")) {
+        //       return "uidotdev";
+        //     }
+        //     if (id.includes("@stripe")) {
+        //       return "stripe";
+        //     }
+        //     if (id.includes("react-hook-form")) {
+        //       return "react-hook-form";
+        //     }
+        //     if (id.includes("yup")) {
+        //       return "yup";
+        //     }
+        //     if (id.includes("bcryptjs")) {
+        //       return "bcryptjs";
+        //     }
+        //     return "vendor";
+        //   }
+        //   // Split your app's pages or large components
+        //   if (id.includes("/src/pages/")) {
+        //     return `page-${id.split("/src/pages/")[1].split("/")[0]}`;
+        //   }
+        //   if (id.includes("/src/components/")) {
+        //     return `component-${id.split("/src/components/")[1].split("/")[0]}`;
+        //   }
+        // },
       },
     },
     // Optimize for smaller bundles and multiple chunks
