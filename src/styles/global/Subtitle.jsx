3000c98c8a497eeb17b934af84d109194287279b
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-09-28 18:16:59 -0400

    redefine font weight value from string to number

--- src/styles/global/Subtitle.jsx
+++ src/styles/global/Subtitle.jsx
@@ -3,7 +3,7 @@ export const Subtitle = {
     letterSpacing: '0.00938em',
     textTransform: 'none',
     textAlign: 'left',
-    fontWeight: '400',
+    fontWeight: 400,
     fontSize: '14px',
     fontFamily: 'Inter',
     lineHeight: '20px',
