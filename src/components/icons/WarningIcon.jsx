2ac5c97b5c651bb1e3a61b3812f0c302efb72290
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-10-21 06:35:37 -0400

    add props to define  background color

--- src/components/icons/WarningIcon.jsx
+++ src/components/icons/WarningIcon.jsx
@@ -1,4 +1,6 @@
-export const WarningIcon = () => {
+export const WarningIcon = (props) => {
+  const { color } = props;
+  if (color) {
     return (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -7,10 +9,23 @@ export const WarningIcon = () => {
         viewBox="0 0 24 24"
       >
         <path
-          fill="currentColor"
+          fill={color}
           d="M12 2L1 21h22M12 6l7.53 13H4.47M11 10v4h2v-4m-2 6v2h2v-2"
         />
       </svg>
     );
-  };
-  
+  }
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="currentColor"
+        d="M12 2L1 21h22M12 6l7.53 13H4.47M11 10v4h2v-4m-2 6v2h2v-2"
+      />
+    </svg>
+  );
+};
