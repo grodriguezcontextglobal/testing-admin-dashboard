69a9de3d7ad5f62f79e226e376724375121f572f
Gustavo Santeliz <grodriguez@contextglobal.com>
2024-12-18 10:04:20 -0500

    refactor component by adding new endpoint and retrieve data. Also, update columns format by adding status and dic status and style.

--- src/pages/events/quickGlance/consumer/table/CustomerDatabase.jsx
+++ src/pages/events/quickGlance/consumer/table/CustomerDatabase.jsx
@@ -1,38 +1,49 @@
+import { Icon } from "@iconify/react/dist/iconify.js";
 import { Typography } from "@mui/material";
-import { Table } from "antd";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { Avatar, Table } from "antd";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
+import { devitrakApi } from "../../../../../api/devitrakApi";
 import {
   onAddCustomerInfo,
   onAddUsersOfEventList,
 } from "../../../../../store/slices/customerSlice";
 import { onAddCustomer } from "../../../../../store/slices/stripeSlice";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { devitrakApi } from "../../../../../api/devitrakApi";
 import "../../../../../styles/global/ant-table.css";
-import { useEffect } from "react";
+import { Subtitle } from "../../../../../styles/global/Subtitle";
 
 export const CustomerDatabase = ({ searchAttendees }) => {
   const { user } = useSelector((state) => state.admin);
-  const { choice, event } = useSelector((state) => state.event);
+  const { event } = useSelector((state) => state.event);
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const attendeesEventQuery = useQuery({
-    queryKey: ["consumersList"],
+  // const attendeesEventQuery = useQuery({
+  //   queryKey: ["consumersList"],
+  //   queryFn: () =>
+  //     devitrakApi.get(
+  //       `/auth/user-query?event_providers=${event.id}&company_providers=${user.companyData.id}`
+  //     ),
+  //   refetchOnMount: false,
+  // });
+  const attendeesAndTransactionsEventQuery = useQuery({
+    queryKey: ["checking_new_path_to"],
     queryFn: () =>
       devitrakApi.get(
-        `/auth/user-query?event_providers=${event.id}&company_providers=${user.companyData.id}`
+        `/event/all-users-and-transactions-per-event?event_providers=${event.id}&company_providers=${user.companyData.id}`
       ),
     refetchOnMount: false,
   });
+
   useEffect(() => {
     const controller = new AbortController();
-    attendeesEventQuery.refetch();
+    // attendeesEventQuery.refetch();
+    attendeesAndTransactionsEventQuery.refetch();
     return () => {
       controller.abort();
     };
   }, []);
-
   const queryClient = useQueryClient();
   const handleDataDetailUser = (record) => {
     let userFormatData = {
@@ -50,88 +61,143 @@ export const CustomerDatabase = ({ searchAttendees }) => {
       `/events/event-attendees/${record.entireData.id}/transactions-details`
     );
   };
-  const columns = [
-    {
-      title: "Company",
-      dataIndex: "company",
-      align: "left",
-      width: "25%",
-      responsive: ["lg"],
-      sorter: {
-        compare: (a, b) => ("" + a.company).localeCompare(b.company),
-      },
-      render: (company) => (
-        <span key={`${company}`}>
-          {company.map((detail, index) => {
-            return (
-              <div
-                key={`${detail}-${index}`}
-                style={{
-                  flexDirection: "column",
-                  color: `${
-                    index === 0
-                      ? "var(--gray-900, #101828)"
-                      : "var(--gray-600, #475467)"
-                  }`,
-                  fontSize: "14px",
-                  fontFamily: "Inter",
-                  lineHeight: "20px",
-                  fontWeight: `${index === 0 ? "500" : null}`,
-                }}
-              >
-                {detail}
-              </div>
-            );
-          })}
-        </span>
-      ),
+  //status = 0 means user has no transaction
+  //status = 1 means user has devices pending
+  //status = 2 means user has device in use
+  //status = 3 means user has devices returned
+
+  const styleDic = {
+    0: {
+      backgroundColor: "#dad7d7",
+      color: "#262424",
     },
+    1: {
+      backgroundColor: "#FFF4ED",
+      color: "#B93815",
+    },
+    2: {
+      backgroundColor: "#ECFDF3",
+      color: "#027A48",
+    },
+    3: {
+      backgroundColor: "#EFF8FF",
+      color: "#175CD3",
+    },
+  };
+  const response = attendeesAndTransactionsEventQuery?.data?.data?.data;
+  const columns = [
     {
       title: "Consumer",
       dataIndex: "user",
-      width: "30%",
+      width: "40%",
       responsive: ["md", "lg"],
       sorter: {
         compare: (a, b) => ("" + a.user).localeCompare(b.user),
       },
       render: (user) => (
-        <span key={`${user}`}>
-          {user.map((detail, index) => {
-            return (
-              <div
-                key={`${detail[0]}${detail[1]}`}
-                style={{
-                  flexDirection: "column",
-                  color: `${
-                    index === 0
-                      ? "var(--gray-900, #101828)"
-                      : "var(--gray-600, #475467)"
-                  }`,
-                  fontSize: "14px",
-                  fontFamily: "Inter",
-                  lineHeight: "20px",
-                  fontWeight: `${index === 0 ? "500" : null}`,
-                }}
-              >
-                <Typography textTransform={"capitalize"}>{detail}</Typography>
-              </div>
-            );
-          })}
+        <span style={{ display: "flex" }} key={`${user}`}>
+          <Avatar>
+            {user[0][0]}
+            {user[1][0]}
+          </Avatar>
+          &nbsp;
+          <div
+            key={`${user[0]}${user[1]}`}
+            style={{
+              flexDirection: "column",
+              color: "var(--gray-900, #101828)",
+              fontSize: "14px",
+              fontFamily: "Inter",
+              lineHeight: "20px",
+              fontWeight: "500",
+            }}
+          >
+            <Typography textTransform={"capitalize"}>
+              {user[0]}&nbsp;
+              {user[1]}
+            </Typography>
+          </div>
         </span>
       ),
     },
     {
+      title: "Status",
+      dataIndex: "status",
+      width: "20%",
+      responsive: ["md", "lg"],
+      sorter: {
+        compare: (a, b) => ("" + a.status).localeCompare(b.status),
+      },
+      render: (status) => {
+        return (
+          <span
+            style={{
+              borderRadius: "16px",
+              justifyContent: "center",
+              display: "flex",
+              padding: "2px 8px",
+              alignItems: "center",
+              background: `${styleDic[status]?.backgroundColor}`,
+              width: "fit-content",
+            }}
+          >
+            <Typography
+              color={`${styleDic[status]?.color}`}
+              textTransform={"capitalize"}
+              style={{
+                ...Subtitle,
+                fontWeight: 500,
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                color: `${styleDic[status]?.color}`,
+              }}
+            >
+              <Icon
+                icon="tabler:point-filled"
+                rotate={3}
+                color={`${styleDic[status]?.color}`}
+              />
+              {dicStatus[status]}
+            </Typography>
+          </span>
+        );
+      },
+    },
+    {
       title: "Email",
       dataIndex: "email",
-      width: "30%",
+      width: "auto",
       sorter: {
         compare: (a, b) => ("" + a.email).localeCompare(b.email),
       },
     },
+    {
+      title: "Phone",
+      dataIndex: "phone",
+      width: "15%",
+      sorter: {
+        compare: (a, b) => ("" + a.phone).localeCompare(b.phone),
+      },
+    },
+    {
+      title: "",
+      key: "action",
+      align: "right",
+      width: "5%",
+      render: () => (
+        <Icon
+          icon="fluent:arrow-circle-right-20-regular"
+          color="#475467"
+          width={25}
+          height={25}
+        />
+      ),
+    },
   ];
 
   const checkEventsPerCompany = () => {
-    const list = attendeesEventQuery?.data?.data?.users;
+    const list = typeof response === "string" ? JSON.parse(response) : response;
     if (list) {
       if (searchAttendees?.length > 0) {
         const check = list?.filter((item) =>
@@ -146,27 +212,29 @@ export const CustomerDatabase = ({ searchAttendees }) => {
     return [];
   };
 
+  const dicStatus = {
+    0: "No devices",
+    1: "Devices pending",
+    2: "Devices in use",
+    3: "Devices returned",
+  };
   const getInfoNeededToBeRenderedInTable = () => {
     let result = [];
-    // let index = checkEventsPerCompany()?.length - 1;
-    // const notElementToDelete = 0;
     let mapTemplate = {};
     for (let data of checkEventsPerCompany()) {
       mapTemplate = {
-        company: [choice, user.company],
-        user: [data.name, data.lastName],
-        email: data.email,
-        key: data.id,
-        entireData: data,
+        user: [data.user.name, data.user.lastName],
+        email: data.user.email,
+        status: data.transactions,
+        phone: data.user.phoneNumber,
+        key: data.user.id,
+        entireData: data.user,
       };
       result = [...result, mapTemplate];
-      // result.splice(index, notElementToDelete, mapTemplate);
-      // index--;
     }
     dispatch(onAddUsersOfEventList(result));
     return result;
   };
-
   return (
     <Table
       sticky
