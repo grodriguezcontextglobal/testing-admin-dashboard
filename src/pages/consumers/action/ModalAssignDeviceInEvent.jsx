e8c369ab8fdd2398f4b82f3101c8867210eb2120
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-02-04 15:00:18 -0500

    grouping fields to optimize space utilization and also, add function to check device availability before trigger auth deposit transaction function.

--- src/pages/consumers/action/ModalAssignDeviceInEvent.jsx
+++ src/pages/consumers/action/ModalAssignDeviceInEvent.jsx
@@ -1,12 +1,19 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
 import { Grid, InputAdornment, OutlinedInput } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
-import { Avatar, Button, Modal, Select, Spin } from "antd";
-import { useCallback, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { Avatar, Button, Modal, Select, Spin, Tooltip } from "antd";
+import {
+  useCallback,
+  useEffect,
+  useState
+} from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { devitrakApi } from "../../../api/devitrakApi";
 import Loading from "../../../components/animation/Loading";
 import { ProfileIcon } from "../../../components/icons/ProfileIcon";
+import { QuestionIcon } from "../../../components/icons/QuestionIcon";
+import { StripeCheckoutElementFromConsumerPage } from "../../../components/stripe/elements/StripeCheckoutElementFromConsumerPage";
+import { onAddTransactionInfo } from "../../../store/slices/customerSlice";
 import "../../../styles/global/ant-select.css";
 import { BlueButton } from "../../../styles/global/BlueButton";
 import { BlueButtonText } from "../../../styles/global/BlueButtonText";
@@ -17,7 +24,11 @@ import { TextFontSize14LineHeight20 } from "../../../styles/global/TextFontSize1
 import TextFontsize18LineHeight28 from "../../../styles/global/TextFontSize18LineHeight28";
 import CashDeposit from "./transaction/CashDeposit";
 import NoDepositTransaction from "./transaction/NoDeposit";
-const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
+const ModalAssignDeviceInEvent = ({
+  assignDevice,
+  setAssignDevice,
+  refetching,
+}) => {
   const { customer } = useSelector((state) => state.customer);
   const { user } = useSelector((state) => state.admin);
   const [typeOfTransaction, setTypeOfTransaction] = useState("");
@@ -28,6 +39,9 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
   const [qty, setQty] = useState("1");
   const [isLoadingState, setIsLoadingState] = useState(false);
   const [triggering, setTriggering] = useState(0);
+  const [clientSecret, setClientSecret] = useState("");
+  const [blockFields, setBlockFields] = useState(false);
+  const dispatch = useDispatch();
   const eventsWhereStaffIsAssigned = useQuery({
     queryKey: ["eventsWhereStaffIsAssigned"],
     queryFn: () =>
@@ -126,14 +140,67 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
     };
     await devitrakApi.patch(`/auth/${customer.uid}`, updateConsumerInfo);
   };
+
   const handleSubmitInformation = async (e) => {
     e.preventDefault();
     try {
       setIsLoadingState(true);
       await addEventToUserHistory();
       if (typeOfTransaction === "Authorized-Deposit") {
-        return setTriggering(1);
-      } else if (typeOfTransaction === "Cash-Deposit") {  
+        const checkAvailability = await devitrakApi.post(
+          "/receiver/receiver-pool-list",
+          {
+            eventSelected: JSON.parse(eventSelected).eventInfoDetail.eventName,
+            company: user.companyData.id,
+            device: serialNumber,
+            activity: false,
+          }
+        );
+        if (
+          checkAvailability.data.ok &&
+          checkAvailability.data.receiversInventory.length === 0
+        ) {
+          setIsLoadingState(false);
+          return alert("No device available in pool");
+        }
+        const transactionInfo = {
+          customerEmail: customer?.email,
+          total: authorizedAmount,
+          myUrl: `http://locahost:5522/consumers/${customer.uid}/payment-confirmation`, //`${window.location.origin}${location.pathname}/payment-confirmation`
+        };
+        const response = await devitrakApi.post(
+          "/stripe/create-payment-intent-customized",
+          transactionInfo
+        );
+        if (response) {
+          setIsLoadingState(false);
+          setBlockFields(true);
+          dispatch(
+            onAddTransactionInfo({
+              customer: customer,
+              quantity: qty,
+              staff: user,
+              event: eventSelected,
+              deviceInfo: {
+                group: deviceType,
+                deviceNeeded: qty,
+                value: authorizedAmount,
+              },
+              serialNumber: serialNumber,
+              loadingState: setIsLoadingState,
+              triggering: setTriggering,
+              closeModal: closeModal,
+              amount: authorizedAmount,
+              refetching: refetching,
+              clientSecret: response.data.paymentIntentCustomized.client_secret,
+              paymentInfo: response.data.paymentIntentCustomized,
+            })
+          );
+          return setClientSecret(
+            response.data.paymentIntentCustomized.client_secret
+          );
+        }
+      } else if (typeOfTransaction === "Cash-Deposit") {
         return setTriggering(2);
       } else {
         return setTriggering(3);
@@ -142,6 +209,23 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
       setIsLoadingState(false);
       alert(error.message ?? error.error);
     }
+  };
+
+  const rowStyle = {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  };
+
+  const subRowStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    width: "50%",
+    padding: "10px 5px",
   };
 
   return (
@@ -226,186 +310,211 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
             Select the event and type of transaction
           </p>
         </Grid>
-        <p
-          style={{
-            ...TextFontSize14LineHeight20,
-            fontWeight: 500,
-            textWrap: "pretty",
-            margin: "10px 0px 5px",
-          }}
-        >
-          Event where you want to assign the transaction:
-        </p>
-        <Select
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            height: "2.5rem",
-          }}
-          onChange={(event) => setEventSelected(event)}
-          options={[...renderingEventOptions()]}
-        />
-        <p
-          style={{
-            ...TextFontSize14LineHeight20,
-            fontWeight: 500,
-            textWrap: "pretty",
-            margin: "10px 0px 5px",
-          }}
-        >
-          Type of transaction you want to create:
-        </p>
+        <div style={rowStyle}>
+          <div style={subRowStyle}>
+            {" "}
+            <p
+              style={{
+                ...TextFontSize14LineHeight20,
+                fontWeight: 500,
+                textWrap: "pretty",
+                margin: "10px 0px 5px",
+              }}
+            >
+              Event where you want to assign the transaction:
+            </p>
+            <Select
+              disabled={blockFields}
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                height: "2.5rem",
+              }}
+              onChange={(event) => setEventSelected(event)}
+              options={[...renderingEventOptions()]}
+            />
+          </div>
+          <div style={subRowStyle}>
+            <p
+              style={{
+                ...TextFontSize14LineHeight20,
+                fontWeight: 500,
+                textWrap: "pretty",
+                margin: "10px 0px 5px",
+              }}
+            >
+              Type of transaction you want to create:
+            </p>
 
-        <Select
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            height: "2.5rem",
-          }}
-          onChange={(value) => setTypeOfTransaction(value)}
-          options={[
-            ...["No-Deposit", "Cash-Deposit"].map(
-              //, "Authorized-Deposit",
-              (item) => ({
-                value: item,
-                label: (
-                  <p
-                    style={{
-                      width: "100%",
-                      display: "flex",
-                      justifyContent: "flex-start",
-                      alignItems: "center",
-                      padding: "5px 5px 5px 0px",
-                    }}
-                  >
-                    {item}
-                  </p>
+            <Select
+              disabled={blockFields}
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                height: "2.5rem",
+              }}
+              onChange={(value) => setTypeOfTransaction(value)}
+              options={[
+                ...["No-Deposit", "Cash-Deposit", "Authorized-Deposit"].map(
+                  (item) => ({
+                    value: item,
+                    label: (
+                      <p
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          justifyContent: "flex-start",
+                          alignItems: "center",
+                          padding: "5px 5px 5px 0px",
+                        }}
+                      >
+                        {item}
+                      </p>
+                    ),
+                  })
                 ),
-              })
-            ),
-          ]}
-        />
-        <p
-          style={{
-            ...TextFontSize14LineHeight20,
-            fontWeight: 500,
-            textWrap: "pretty",
-            margin: "10px 0px 5px",
-          }}
-        >
-          Device type you want to assign:
-        </p>
-        <Select
-          style={{
-            width: "100%",
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
-            height: "2.5rem",
-          }}
-          onChange={(event) => setDeviceType(event)}
-          options={[
-            ...renderingInventoryOptionsBasedOnSelectedEvent().map((item) => ({
-              value: item.group,
-              label: (
-                <p
-                  style={{
-                    width: "100%",
-                    display: "flex",
-                    justifyContent: "flex-start",
-                    alignItems: "center",
-                    padding: "5px 5px 5px 0px",
-                  }}
-                >
-                  {item.group}
-                </p>
-              ),
-            })),
-          ]}
-        />
-        <p
-          style={{
-            ...TextFontSize14LineHeight20,
-            fontWeight: 500,
-            textWrap: "pretty",
-            margin: "10px 0px 5px",
-          }}
-        >
-          Serial number to assign
-          <span style={{ fontWeight: 400 }}>
-            &nbsp;(if transaction is for multiple devices, this will be the
-            first device of the range)
-          </span>
-          :
-        </p>
-        <OutlinedInput
-          style={OutlinedInputStyle}
-          type="text"
-          name="serialNumber"
-          onChange={(event) => setSerialNumber(event.target.value)}
-          fullWidth
-          placeholder="e.g. 0150235"
-        />
-        <p
-          style={{
-            ...TextFontSize14LineHeight20,
-            fontWeight: 500,
-            textWrap: "pretty",
-            margin: "10px 0px 5px",
-          }}
-        >
-          Quantity of devices to assign in transaction (1 unit as default):
-        </p>
-        <OutlinedInput
-          style={OutlinedInputStyle}
-          name="qty"
-          type="text"
-          onChange={(event) => setQty(event.target.value)}
-          fullWidth
-          placeholder="e.g. 20"
-        />
-        <p
-          style={{
-            ...TextFontSize14LineHeight20,
-            fontWeight: 500,
-            textWrap: "pretty",
-            margin: "10px 0px 5px",
-            display: typeOfTransaction === "No-Deposit" ? "none" : "flex",
-          }}
-        >
-          Authorized amount for transaction{" "}
-          <span style={{ textDecoration: "underline" }}>($)</span>:
-        </p>
-        <OutlinedInput
-          style={{
-            ...OutlinedInputStyle,
-            display: typeOfTransaction === "No-Deposit" ? "none" : "flex",
-          }}
-          onChange={(event) => setAuthorizedAmount(event.target.value)}
-          fullWidth
-          placeholder="e.g. 200"
-          name="authorizedAmount"
-          type="text"
-          startAdornment={
-            <InputAdornment position="start">
-              <Icon
-                icon="radix-icons:dollar-sign-line"
-                color="#344054"
-                width={20}
-                height={19}
-              />
-            </InputAdornment>
-          }
-        />
+              ]}
+            />
+          </div>
+        </div>
+        <div style={rowStyle}>
+          <div style={subRowStyle}>
+            {" "}
+            <p
+              style={{
+                ...TextFontSize14LineHeight20,
+                fontWeight: 500,
+                textWrap: "pretty",
+                margin: "10px 0px 5px",
+              }}
+            >
+              Device type you want to assign:
+            </p>
+            <Select
+              disabled={blockFields}
+              style={{
+                width: "100%",
+                display: "flex",
+                justifyContent: "flex-start",
+                alignItems: "center",
+                height: "2.5rem",
+              }}
+              onChange={(event) => setDeviceType(event)}
+              options={[
+                ...renderingInventoryOptionsBasedOnSelectedEvent().map(
+                  (item) => ({
+                    value: item.group,
+                    label: (
+                      <p
+                        style={{
+                          width: "100%",
+                          display: "flex",
+                          justifyContent: "flex-start",
+                          alignItems: "center",
+                          padding: "5px 5px 5px 0px",
+                        }}
+                      >
+                        {item.group}
+                      </p>
+                    ),
+                  })
+                ),
+              ]}
+            />
+          </div>
+          <div style={subRowStyle}>
+            {" "}
+            <Tooltip title="if transaction is for multiple devices, this will be the first device of the range">
+              <p
+                style={{
+                  ...TextFontSize14LineHeight20,
+                  textAlign: "left",
+                  fontWeight: 500,
+                  textWrap: "pretty",
+                  margin: "10px 0px 5px",
+                  width: "100%",
+                }}
+              >
+                Serial number to assign <QuestionIcon />:
+              </p>
+            </Tooltip>
+            <OutlinedInput
+              disabled={blockFields}
+              style={OutlinedInputStyle}
+              type="text"
+              name="serialNumber"
+              onChange={(event) => setSerialNumber(event.target.value)}
+              placeholder="e.g. 0150235"
+            />
+          </div>
+        </div>
+        <div style={rowStyle}>
+          <div style={subRowStyle}>
+            {" "}
+            <p
+              style={{
+                ...TextFontSize14LineHeight20,
+                fontWeight: 500,
+                textWrap: "pretty",
+                margin: "10px 0px 5px",
+              }}
+            >
+              Quantity of devices to assign in transaction:
+            </p>
+            <OutlinedInput
+              disabled={blockFields}
+              style={OutlinedInputStyle}
+              name="qty"
+              type="text"
+              onChange={(event) => setQty(event.target.value)}
+              fullWidth
+              placeholder="e.g. 20"
+            />
+          </div>
+          <div style={subRowStyle}>
+            <p
+              style={{
+                ...TextFontSize14LineHeight20,
+                fontWeight: 500,
+                textWrap: "pretty",
+                margin: "10px 0px 5px",
+                display: typeOfTransaction === "No-Deposit" ? "none" : "flex",
+              }}
+            >
+              Authorized amount for transaction ($):
+            </p>
+            <OutlinedInput
+              disabled={blockFields}
+              style={{
+                ...OutlinedInputStyle,
+                display: typeOfTransaction === "No-Deposit" ? "none" : "flex",
+              }}
+              onChange={(event) => setAuthorizedAmount(event.target.value)}
+              fullWidth
+              placeholder="e.g. 200"
+              name="authorizedAmount"
+              type="text"
+              startAdornment={
+                <InputAdornment position="start">
+                  <Icon
+                    icon="radix-icons:dollar-sign-line"
+                    color="#344054"
+                    width={20}
+                    height={19}
+                  />
+                </InputAdornment>
+              }
+            />
+          </div>
+        </div>
         <div
           style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "space-around",
+            ...rowStyle,
+            display: clientSecret !== "" ? "none" : "flex",
             width: "100%",
             gap: "10px",
           }}
@@ -431,8 +540,16 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
             </p>
           </Button>
         </div>
-
         {isLoadingState && <Spin indicator={<Loading />} fullscreen />}
+        {clientSecret !== "" && (
+          <div style={{ width: "100%", margin: "20px 0px" }}>
+            <StripeCheckoutElementFromConsumerPage
+              clientSecret={clientSecret}
+              total={authorizedAmount}
+              myUrl={window.location.href}
+            />
+          </div>
+        )}
         {triggering === 2 && (
           <CashDeposit
             customer={customer}
@@ -449,9 +566,9 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
             triggering={setTriggering}
             closeModal={closeModal}
             amount={authorizedAmount}
+            refetching={refetching}
           />
         )}
-
         {triggering === 3 && (
           <NoDepositTransaction
             customer={customer}
@@ -467,6 +584,7 @@ const ModalAssignDeviceInEvent = ({ assignDevice, setAssignDevice }) => {
             loadingState={setIsLoadingState}
             triggering={setTriggering}
             closeModal={closeModal}
+            refetching={refetching}
           />
         )}
       </Grid>
