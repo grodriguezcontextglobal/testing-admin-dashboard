f0ac8080a87b729a0b770b1ee23f4e2861122de9
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-02-19 06:15:48 -0500

    add props to set padding values based on screen size

--- src/pages/ParentRenderingChildrenPage.jsx
+++ src/pages/ParentRenderingChildrenPage.jsx
@@ -5,19 +5,23 @@ import { Outlet } from "react-router-dom";
 const ParentRenderingChildrenPage = () => {
   return (
     <Grid
+      id="parent-container"
       display={"flex"}
       justifyContent={"center"}
       alignItems={"center"}
-      maxWidth={'1228px'}
-      minWidth={'768px'}
+      maxWidth={"1228px"}
+      minWidth={"768px"}
       container
     >
       <Grid
         alignSelf={"flex-start"}
-        style={{ minHeight: "80dvh" }}
-        margin={"14dvh auto 1dvh"}
+        sx={{
+          minHeight: "80dvh",
+          padding: { xs: "0 24px", sm: "0 24px", md: 0, lg: 0 },
+        }}
+        margin={"14dvh 0 1dvh"}
         item
-        xs={12}
+        xs={11}
         sm={12}
         md={12}
         lg={12}
