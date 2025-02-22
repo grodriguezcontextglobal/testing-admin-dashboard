abac896e6c7371e2b7e69c9198b0cf9b70985371
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-02-21 19:48:46 -0500

    new reducer to store advance search result

--- src/store/slices/searchBarResultSlice.js
+++ src/store/slices/searchBarResultSlice.js
@@ -1,30 +1,40 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 // import pkg from '@reduxjs/toolkit';
 // const {createSlice} = pkg;
 const searchBarResultSlice = createSlice({
-    name: 'searchBarResult',
-    initialState: {
-        searchResult: [],
-        searchValue: undefined
+  name: "searchBarResult",
+  initialState: {
+    searchResult: [],
+    searchValue: undefined,
+    advanceSearch: null,
+  },
+  reducers: {
+    onAddNewResult: (state, { payload }) => {
+      state.searchResult = payload;
     },
-    reducers: {
-        onAddNewResult: (state, { payload }) => {
-            state.searchResult = payload
-        },
-        onResetResult: (state) => {
-            state.searchResult = []
-        },
-        onAddSearchValue: (state, { payload }) => {
-            state.searchValue = payload
-        },
-        onResetSearchValue: (state) => {
-            state.searchValue = undefined
-        },
+    onResetResult: (state) => {
+      state.searchResult = [];
     },
+    onAddSearchValue: (state, { payload }) => {
+      state.searchValue = payload;
+    },
+    onResetSearchValue: (state) => {
+      state.searchValue = undefined;
+    },
+    onAddAdvanceSearch: (state, { payload }) => {
+      state.advanceSearch = payload;
+    },
+  },
 });
 
 // action creators are generated for each case reducer function
 
-export const { onAddNewResult, onResetResult, onAddSearchValue, onResetSearchValue } = searchBarResultSlice.actions;
+export const {
+  onAddNewResult,
+  onResetResult,
+  onAddSearchValue,
+  onResetSearchValue,
+  onAddAdvanceSearch,
+} = searchBarResultSlice.actions;
 
 export default searchBarResultSlice.reducer;
