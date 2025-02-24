e05f3fac3cb974a212a3d77065c50c5b2b3e3441
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-02-24 11:57:34 -0500

    error boundary component to catch error within app

---
+++ src/components/utils/ErrorBoundaryComponent.jsx
@@ -0,0 +1,84 @@
+import { Card } from "antd";
+import { devitrakApi } from "../../api/devitrakApi";
+import { BlueButton } from "../../styles/global/BlueButton";
+import { BlueButtonText } from "../../styles/global/BlueButtonText";
+import { TextFontSize20LineHeight30 } from "../../styles/global/TextFontSize20HeightLine30";
+import { DevitrakLogo } from "../icons/DevitrakLogo";
+import { DevitrakName } from "../icons/DevitrakName";
+
+export const ErrorBoundaryComponent = ({ error, resetErrorBoundary }) => {
+  const titleRendering = () => {
+    return (
+      <h3
+        style={{
+          ...BlueButtonText,
+          fontWeight: 600,
+          fontSize: "20px",
+          lineHeight: "26px",
+          width: "100%",
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+      >
+        <DevitrakLogo />
+        <DevitrakName />
+      </h3>
+    );
+  };
+  return (
+    <Card
+      style={{
+        backgroundColor: "var(--blue700)",
+        width: "50%",
+        margin: "0 auto",
+        padding: "12px",
+        color: "var(--basewhite)",
+      }}
+      styles={{
+        body: {
+          margin: "15px 0 0",
+          padding: "12px 24px",
+          backgroundColor: "var(--basewhite)",
+          borderRadius: "12px",
+        },
+      }}
+      title={titleRendering()}
+    >
+      <div role="alert">
+        <p
+          style={{
+            ...TextFontSize20LineHeight30,
+            color: "var(--main-navbar-color)",
+          }}
+        >
+          Something went wrong:
+        </p>
+        <pre
+          style={{
+            ...TextFontSize20LineHeight30,
+            color: "var(--graphic-status-damage)",
+          }}
+        >
+          {error.message}
+        </pre>
+        <button
+          style={{ ...BlueButton, width: "100%" }}
+          onClick={resetErrorBoundary}
+        >
+          <p style={BlueButtonText}>Reset</p>
+        </button>
+      </div>
+    </Card>
+  );
+};
+
+export const ErrorLogFetch = async (error, info) => {
+  const fetchingError = await devitrakApi.post("/error_log/error_log", {
+    error: `${error}`,
+    componentStack: info.componentStack,
+  });
+  if (fetchingError.data.ok) {
+    return alert(`${fetchingError.data.msg}`);
+  }
+};
