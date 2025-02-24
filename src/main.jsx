161949e448e8cfead0c4537f19c7c02a4b8f0ed0
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-02-24 11:58:06 -0500

    error boundary component add to main component as HOC

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,27 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store, persistor } from "./store/Store.js";
-import { PersistGate } from "redux-persist/integration/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import './index.css'
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App.jsx";
+import "./index.css";
+import { persistor, store } from "./store/Store.js";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorBoundaryComponent, ErrorLogFetch } from "./components/utils/ErrorBoundaryComponent.jsx";
 const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <BrowserRouter basename="/">
         <PersistGate persistor={persistor}>
           <QueryClientProvider client={queryClient}>
-            <App />
+            <ErrorBoundary FallbackComponent={ErrorBoundaryComponent} onError={ErrorLogFetch}>
+              <App />
+            </ErrorBoundary>
           </QueryClientProvider>
         </PersistGate>
       </BrowserRouter>
     </React.StrictMode>
   </Provider>
-)
+);
