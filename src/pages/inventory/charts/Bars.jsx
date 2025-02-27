ab5f95fe98280be65eb7853e24599dfe20475a9a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-02-27 10:38:43 -0500

    refactor component to display animated bar s with background

--- src/pages/inventory/charts/Bars.jsx
+++ src/pages/inventory/charts/Bars.jsx
@@ -3,48 +3,99 @@ import ReactECharts from "echarts-for-react";
 import { PropTypes } from "prop-types";
 
 const Bars = ({ dataToRender, title }) => {
-  const formattingData = dataToRender.map((item) => {
-    return [item[0], item[1]];
-  });
-
-  const gettingMinAndMax = () => {
-    const result = [];
-    dataToRender.forEach(element => {
-        result.push(element[0]);
-    });
-    return Math.max(...result)
+  const labelOption = {
+    show: true,
+    position: "insideBottom",
+    distance: 15,
+    align: "left",
+    verticalAlign: "middle",
+    rotate: 80,
+    formatter: "{c}  {name|{a}}",
+    fontSize: 16,
+    fontFamily:"Inter",
+    color:"#000",
+    rich: {
+      name: {},
+    },
   };
 
   const option = {
-    legend: {},
-    tooltip: {},
-    dataset: {
-      source: [["amount", "item"], ...formattingData],
+    tooltip: {
+      trigger: "axis",
+      axisPointer: { type: "shadow" },
     },
-    grid: [{ bottom: '20%' }, { top: '55%' }],
-    // grid: { containLabel: true },
-    xAxis: { name: "Qty" },
-    yAxis: { type: "category" },
-    visualMap: {
+    legend: {
+      type: "scroll",
       orient: "horizontal",
+      top: 0,
       left: "center",
-      min: 0,
-      max: gettingMinAndMax(),
-      text: ["High Score", "Low Score"],
-      // Map the score column to color
-      dimension: 0,
-      inRange: {
-        color: ["#FD665F", "#FFCE34", "#65B581"],
+      itemGap: 10,
+      textStyle: {
+        fontSize: 12,
+      },
+      data: [...dataToRender.map((item) => item.group)],
+    },
+    grid: {
+      top: 60,
+      left: "10%",
+      right: "10%",
+      bottom: 40,
+      containLabel: true,
+    },
+    toolbox: {
+      show: true,
+      orient: "vertical",
+      left: "right",
+      top: "center",
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        magicType: { show: true, type: ["line", "bar", "stack"] },
+        restore: { show: true },
+        saveAsImage: { show: true },
       },
     },
+    xAxis: [
+      {
+        type: "category",
+        axisTick: { show: false },
+        data: [...dataToRender.map((item) => item.group)],
+      },
+    ],
+    yAxis: [
+      {
+        type: "value",
+      },
+    ],
     series: [
       {
+        name: "Available",
         type: "bar",
-        encode: {
-          // Map the "amount" column to X axis.
-          x: "amount",
-          // Map the "product" column to Y axis
-          y: "item",
+        barGap: 0.1,
+        label: labelOption,
+        showBackground: true,
+        emphasis: {
+          focus: "series",
+        },
+        data: [...dataToRender.map((item) => Number(item.available))],
+        itemStyle: {
+          color: "rgba(69, 104, 220, 1)", // Set Available items to blue
+          backgroundColor: "rgba(69, 104, 220, 1)",
+        },
+      },
+      {
+        name: "Not Available",
+        type: "bar",
+        barGap: 0.1,
+        label: labelOption,
+        showBackground: true,
+        emphasis: {
+          focus: "series",
+        },
+        data: [...dataToRender.map((item) => Number(item.notAvailable))],
+        itemStyle: {
+          color: "rgba(192, 43, 10, 1)", // Set Not Available items to red
+          backgroundColor: "rgba(192, 43, 10, 1)",
         },
       },
     ],
@@ -52,27 +103,26 @@ const Bars = ({ dataToRender, title }) => {
 
   return (
     <Card
-    title={title}
-    style={{
-      display: "flex",
-      flexDirection: "column",
-      gap: "24px",
-      alignSelf: "stretch",
-      borderRadius: "8px",
-      border: "1px solid var(--Gray-300, #D0D5DD)",
-      background: "var(--Gray-100, #F2F4F7)",
-      width: "100%",
-    }}
-    styles={{
-      body: {
-        padding: '0 15px',
-        height: '80%',
-        width: '95%',
-        margin: 'auto',
-      },
-    }}
-  >
-
+      title={title}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "24px",
+        alignSelf: "stretch",
+        borderRadius: "8px",
+        border: "1px solid var(--Gray-300, #D0D5DD)",
+        background: "var(--Gray-100, #F2F4F7)",
+        width: "100%",
+      }}
+      styles={{
+        body: {
+          padding: "0 15px",
+          height: "80%",
+          width: "90%",
+          margin: "auto",
+        },
+      }}
+    >
       <ReactECharts option={option} />
     </Card>
   );
