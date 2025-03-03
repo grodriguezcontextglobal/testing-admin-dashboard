b9b1bf9bfeb5b9f769a6e11641bae7b9ba632244
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-03 08:59:41 -0500

    add new function to collect staff member information and then redirect user to staff member info page

--- src/pages/events/quickGlance/staff/table/StaffTable.jsx
+++ src/pages/events/quickGlance/staff/table/StaffTable.jsx
@@ -3,15 +3,21 @@ import { Typography } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
 import { Avatar, Table } from "antd";
 import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { devitrakApi } from "../../../../../api/devitrakApi";
 import "../../../../../styles/global/ant-table.css";
 import { Subtitle } from "../../../../../styles/global/Subtitle";
 import checkTypeFetchResponse from "../../../../../components/utils/checkTypeFetchResponse";
+import { useNavigate } from "react-router-dom";
+import StaffMemberStructure from "../../../../../classes/staffMemberStructure";
+import { onAddStaffProfile } from "../../../../../store/slices/staffDetailSlide";
 
 const StaffTable = ({ searching }) => {
+  const { user } = useSelector((state) => state.admin);
   const { event } = useSelector((state) => state.event);
   const [staff, setStaff] = useState([]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const staffEventQuery = useQuery({
     queryKey: ["newEndpointQuery"],
     queryFn: () => devitrakApi.get(`/event/event-staff-detail/${event.id}`), //devitrakApi.get("/staff/admin-users"),
@@ -32,9 +38,11 @@ const StaffTable = ({ searching }) => {
     for (const data of employees) {
       if (data.admin_id === null) {
         result.add({
+          id: data.admin_id,
           name: `${data.staff.firstName} ${data.staff.lastName}`,
           online: false,
-          role: data.staff.role !== 'Administrator' ? "Assistant" : data.staff.role,
+          role:
+            data.staff.role !== "Administrator" ? "Assistant" : data.staff.role,
           email: data.staff.email,
           phone: "000-000-0000",
           photo: "",
@@ -44,9 +52,11 @@ const StaffTable = ({ searching }) => {
           `/admin/check-online-status/${data.staff.email}`
         );
         result.add({
+          id: data.admin_id,
           name: `${data.staff.firstName} ${data.staff.lastName}`,
           online: onlineStatus?.data?.online,
-          role: data.staff.role !== 'Administrator' ? "Assistant" : data.staff.role,
+          role:
+            data.staff.role !== "Administrator" ? "Assistant" : data.staff.role,
           email: data.staff.email,
           phone: data.phone ?? "000-000-0000",
           photo: data.photo ?? "",
@@ -165,8 +175,10 @@ const StaffTable = ({ searching }) => {
       key: "action",
       align: "right",
       width: "5%",
+      responsive: ["lg"],
       render: () => (
         <Icon
+          style={{ cursor: "pointer" }}
           icon="fluent:arrow-circle-right-20-regular"
           color="#475467"
           width={25}
@@ -175,6 +187,21 @@ const StaffTable = ({ searching }) => {
       ),
     },
   ];
+
+  const handleDataStaffMember = async (record) => {
+    const infoFound = await devitrakApi.post(`/staff/admin-users`, {
+      _id: record.id,
+    });
+    if (infoFound.data.ok) {
+      const staffMemberStructure = new StaffMemberStructure(
+        user.companyData,
+        infoFound.data.adminUsers[0],
+        record.role
+      );
+      dispatch(onAddStaffProfile(staffMemberStructure.fromStaffPage()));
+      return navigate(`/staff/${record.id}/main`);
+    }
+  };
   return (
     <Table
       pagination={{
@@ -183,6 +210,12 @@ const StaffTable = ({ searching }) => {
       className="table-ant-customized"
       columns={columns}
       dataSource={dataToRender()}
+      onRow={(record) => {
+        return {
+          onClick: () =>
+            record.id && user.role < 4 && handleDataStaffMember(record),
+        };
+      }}
     />
   );
 };
