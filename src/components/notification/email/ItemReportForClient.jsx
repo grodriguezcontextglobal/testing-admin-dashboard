cacf6dd825ffc5d90c1bbf5037daeaa930964ad5
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-05 17:40:09 -0500

    email item report per customer per event component

---
+++ src/components/notification/email/ItemReportForClient.jsx
@@ -0,0 +1,49 @@
+import { message } from "antd";
+import { devitrakApi } from "../../../api/devitrakApi";
+
+const itemReportForClient = async ({
+  customerInfo,
+  event,
+  paymentIntent,
+  user,
+  devicesInfo,
+  setLoadingState,
+}) => {
+  try {
+    setLoadingState(true);
+    const response = await devitrakApi.post(
+      "/nodemailer/device-report-per-transaction",
+      {
+        consumer: {
+          email: customerInfo.email,
+          firstName: customerInfo.name,
+          lastName: customerInfo.lastName,
+        },
+        devices: [
+          ...devicesInfo.map((item) => {
+            return {
+              device: { ...item },
+              paymentIntent: paymentIntent,
+            };
+          }),
+        ],
+        event: event.eventInfoDetail.eventName,
+        transaction: paymentIntent,
+        company: user.companyData.id,
+        link: `https://app.devitrak.net/?event=${event.id}&company=${user.companyData.id}`,
+        admin: user.email,
+      }
+    );
+    if (response.data.ok) {
+      setLoadingState(false);
+      return message.success(
+        `Device report was sent successfully to ${customerInfo.email}`
+      );
+    }
+  } catch (error) {
+    setLoadingState(false);
+    return message.error(`There was an error. ${error}`);
+  }
+};
+
+export default itemReportForClient;
