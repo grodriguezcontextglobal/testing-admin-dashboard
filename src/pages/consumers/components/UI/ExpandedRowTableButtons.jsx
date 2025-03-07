ed1f135b7255254edb66714ca7f98f8eb3d2e78b
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-07 17:23:42 -0500

    reusable component to display buttons when a condition is matched, improvement managing component

---
+++ src/pages/consumers/components/UI/ExpandedRowTableButtons.jsx
@@ -0,0 +1,78 @@
+import { Button } from "antd";
+import GrayButtonText from "../../../../styles/global/GrayButtonText";
+import { BlueButton } from "../../../../styles/global/BlueButton";
+import { BlueButtonText } from "../../../../styles/global/BlueButtonText";
+import { GrayButton } from "../../../../styles/global/GrayButton";
+
+const ExpandedRowTableButtons = ({
+  record,
+  handleReturnItemInTransaction,
+  handleLostSingleDevice,
+  handleReturnItemFromLeaseTransaction,
+  ReverseRightArrow,
+}) => {
+  return (
+    <div style={{ display: "flex", gap: "5px" }}>
+      <Button
+        disabled={!record.status || typeof record.status === "string"}
+        onClick={() =>
+          record.transactionData.type === "lease"
+            ? handleReturnItemFromLeaseTransaction(record)
+            : handleReturnItemInTransaction(record)
+        }
+        style={{
+          ...BlueButton,
+          backgroundColor: record.status
+            ? BlueButton.background
+            : "var(--disabled-blue-button)",
+          border: record.status
+            ? BlueButton.border
+            : "1px solid var(--disabled-blue-button)",
+        }}
+      >
+        {record.transactionData.type === "lease" ? (
+          <p
+            style={{
+              ...BlueButtonText,
+              color: record.status ? BlueButtonText.color : "var(--gray200)",
+            }}
+          >
+            Mark as ended lease
+          </p>
+        ) : (
+          <p
+            style={{
+              ...BlueButtonText,
+              color: record.status ? BlueButtonText.color : "var(--gray200)",
+            }}
+          >
+            <img src={ReverseRightArrow} alt="ReverseRightArrow" /> &nbsp;Mark
+            as returned
+          </p>
+        )}
+      </Button>
+      <Button
+        disabled={!record.status || typeof record.status === "string"}
+        onClick={() => handleLostSingleDevice(record)}
+        style={{
+          ...GrayButton,
+        }}
+      >
+        <p
+          style={{
+            ...GrayButtonText,
+            color: `${
+              record.status
+                ? GrayButtonText.color
+                : "var(--disabled0gray-button-text)"
+            }`,
+          }}
+        >
+          Mark as lost
+        </p>
+      </Button>
+    </div>
+  );
+};
+
+export default ExpandedRowTableButtons;
