6ba7601f313ba25dc23556605e26f4821ccbab9a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-10 08:07:13 -0400

    receiver props function to update item status marked as lost to active.

--- src/pages/consumers/components/UI/ExpandedLostButtons.jsx
+++ src/pages/consumers/components/UI/ExpandedLostButtons.jsx
@@ -3,45 +3,63 @@ import { DangerButton } from "../../../../styles/global/DangerButton";
 import { DangerButtonText } from "../../../../styles/global/DangerButtonText";
 import { GrayButton } from "../../../../styles/global/GrayButton";
 import GrayButtonText from "../../../../styles/global/GrayButtonText";
+import ChargeLostFee from "../../action/ChargeLostFee";
+import { useState } from "react";
 
-const ExpandedLostButton = ({ record, handleLostSingleDevice, Lost }) => {
+const ExpandedLostButton = ({
+  record,
+  handleFoundSingleDevice,
+  handleLostSingleDevice,
+  Lost,
+}) => {
+  const [chargeLostFeeModal, setChargeLostFeeModal] = useState(false);
+  const propsUpdateSingleDevice = {
+    ...record,
+    new_status: true,
+  }
   return (
-    <div style={{ display: "flex", gap: "5px" }}>
-      <Button
-        disabled
-        style={{ ...DangerButton, alignItems: "center" }}
-      >
-        <img src={Lost} alt="Lost" />
-        <p
+    <>
+      <div style={{ display: "flex", justifyContent: "flex-end", gap: "5px" }}>
+        <Button disabled onClick={() => handleLostSingleDevice(record)} style={{ ...DangerButton, alignItems: "center" }}>
+          <img src={Lost} alt="Lost" />
+          <p
+            style={{
+              ...DangerButtonText,
+              alignSelf: "center",
+            }}
+          >
+            Charge customer
+          </p>
+        </Button>
+        <Button
+          onClick={() => handleFoundSingleDevice(propsUpdateSingleDevice)}
           style={{
-            ...DangerButtonText,
-            alignSelf: "center",
+            ...GrayButton,
           }}
         >
-          Charge customer
-        </p>
-      </Button>
-      <Button
-        disabled={!record.status || typeof record.status === "string"}
-        onClick={() => handleLostSingleDevice(record)}
-        style={{
-          ...GrayButton,
-        }}
-      >
-        <p
-          style={{
-            ...GrayButtonText,
-            color: `${
-              record.status
-                ? GrayButtonText.color
-                : "var(--disabled0gray-button-text)"
-            }`,
-          }}
-        >
-          Mark as found
-        </p>
-      </Button>
-    </div>
+          <p
+            style={{
+              ...GrayButtonText,
+              color: `${
+                record.status
+                  ? GrayButtonText.color
+                  : "var(--disabled0gray-button-text)"
+              }`,
+            }}
+          >
+            Mark as found
+          </p>
+        </Button>
+      </div>
+      {chargeLostFeeModal && (
+        <ChargeLostFee
+          openModal={chargeLostFeeModal}
+          setOpenModal={setChargeLostFeeModal}
+          record={record}
+        />
+      )}
+
+    </>
   );
 };
 
