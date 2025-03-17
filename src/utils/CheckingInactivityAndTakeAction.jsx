db3add3ae2012d62d4040f5423069462ac81cbc6
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-17 06:35:58 -0400

    fix bug of automatically logout for inactivity.

--- src/utils/CheckingInactivityAndTakeAction.jsx
+++ src/utils/CheckingInactivityAndTakeAction.jsx
@@ -1,11 +1,22 @@
 import { notification } from "antd";
 import { useEffect, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, useNavigate } from "react-router-dom"; // Assuming you're using react-router
 import { onLogout } from "../store/slices/adminSlice";
+import { devitrakApi } from "../api/devitrakApi";
+import { onResetArticleEdited } from "../store/slices/articleSlide";
+import { onResetCustomer } from "../store/slices/customerSlice";
+import { onResetDeviceInQuickGlance, onResetDevicesHandle } from "../store/slices/devicesHandleSlice";
+import { onResetEventInfo } from "../store/slices/eventSlice";
+import { onResetStaffProfile } from "../store/slices/staffDetailSlide";
+import { onResetHelpers } from "../store/slices/helperSlice";
+import { onResetStripesInfo } from "../store/slices/stripeSlice";
+import { onResetSubscriptionInfo } from "../store/slices/subscriptionSlice";
+import { persistor } from "../store/Store";
 
 const InactivityLogout = ({ children }) => {
   const [isActive, setIsActive] = useState(true);
+  const { user } = useSelector((state) => state.admin);
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const [api, contextHolder] = notification.useNotification();
@@ -24,17 +35,30 @@ const InactivityLogout = ({ children }) => {
       clearTimeout(logoutTimer);
       logoutTimer = setTimeout(() => {
         handleLogout();
-      }, 2 * 60 * 60 * 1000);
+      }, 2 * 60 * 60 * 1000); 
     };
 
     // Function to handle logout
-    const handleLogout = () => {
+    const handleLogout = async () => {
       setIsActive(false);
       // Perform logout actions here (e.g., clear user data, token, etc.)
-      openNotification();
-      dispatch(onLogout());
-      localStorage.removeItem("admin-token");
-      // window.location.reload; // Redirect to the login page
+    await devitrakApi.patch(`/staff/edit-admin/${user.uid}`, {
+      online: false,
+    });
+    persistor.purge();
+    dispatch(onResetArticleEdited());
+    dispatch(onResetCustomer());
+    dispatch(onResetDevicesHandle());
+    dispatch(onResetDeviceInQuickGlance());
+    dispatch(onResetEventInfo());
+    dispatch(onResetStaffProfile());
+    dispatch(onResetHelpers());
+    dispatch(onResetStripesInfo());
+    dispatch(onResetSubscriptionInfo());
+    localStorage.removeItem("admin-token", "");
+    dispatch(onLogout());
+    openNotification();
+    return navigate("/login");
     };
 
     // Add event listeners for user activity
