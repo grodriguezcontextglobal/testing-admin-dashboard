3c657b30f1a38080db3df95f21be9a43a8273d8b
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-17 22:44:22 -0400

    handle check if device was lost fee collected already to display refund function if needed.

--- src/pages/consumers/components/UI/ExpandedLostButtons.jsx
+++ src/pages/consumers/components/UI/ExpandedLostButtons.jsx
@@ -3,22 +3,78 @@ import { DangerButton } from "../../../../styles/global/DangerButton";
 import { DangerButtonText } from "../../../../styles/global/DangerButtonText";
 import { GrayButton } from "../../../../styles/global/GrayButton";
 import GrayButtonText from "../../../../styles/global/GrayButtonText";
+import { useQuery } from "@tanstack/react-query";
+import { devitrakApi } from "../../../../api/devitrakApi";
+import { useEffect, useState } from "react";
+import { groupBy } from "lodash";
 
 const ExpandedLostButton = ({
   record,
   handleFoundSingleDevice,
   handleLostSingleDevice,
   Lost,
+  refetchingQueries,
 }) => {
-  // const [chargeLostFeeModal, setChargeLostFeeModal] = useState(false);
+  const [cashReportList, setCashReportList] = useState([]);
   const propsUpdateSingleDevice = {
     ...record,
     new_status: true,
-  }
+  };
+  const checkChargedLostFee = useQuery({
+    queryKey: [
+      "chargedLostFee",
+      `${record.key}-${record.serial_number}-${record.type}`,
+    ],
+    queryFn: () =>
+      devitrakApi.post(`/cash-report/cash-reports`, {
+        event: record.entireData.event_id,
+        company: record.entireData.company,
+        "deviceLost.label": record.serial_number,
+        "deviceLost.deviceType": record.type,
+        attendee: record.entireData.user,
+      }),
+    refetchOnMount: false,
+  });
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const groupingData = groupBy(
+      checkChargedLostFee?.data?.data?.report,
+      "deviceLost[0].label"
+    );
+    setCashReportList(groupingData);
+    return () => {
+      controller.abort();
+    };
+  }, [checkChargedLostFee.data]);
+
+  const handleRefund = async (record) => {
+    try {
+      for (let data of cashReportList[record.serial_number]) {
+        console.log(data)
+        if(data.paymentIntent_charge_transaction.length > 15 && !data.paymentIntent_charge_transaction.includes("cash")){
+        await devitrakApi.post(`/stripe/refund`, {
+          paymentIntent: data.paymentIntent_charge_transaction,
+        });
+      }
+      await devitrakApi.post(`/cash-report/remove-cash-report/${data.id}`);
+    }
+      return refetchingQueries();
+    } catch (error) {
+      return null;
+    }
+  };
+
   return (
-    <>
       <div style={{ display: "flex", justifyContent: "flex-end", gap: "5px" }}>
-        <Button onClick={() => handleLostSingleDevice(record)} style={{ ...DangerButton, alignItems: "center" }}>
+        <Button
+          disabled={cashReportList[record.serial_number]?.length > 0}
+          onClick={() => handleLostSingleDevice(record)}
+          style={{
+            ...DangerButton,
+            alignItems: "center",
+          }}
+        >
           <img src={Lost} alt="Lost" />
           <p
             style={{
@@ -26,11 +82,17 @@ const ExpandedLostButton = ({
               alignSelf: "center",
             }}
           >
-            Charge customer
+            {cashReportList[record.serial_number]?.length > 0
+              ? "Charged"
+              : "Charge customer"}
           </p>
         </Button>
         <Button
-          onClick={() => handleFoundSingleDevice(propsUpdateSingleDevice)}
+          onClick={() =>
+            cashReportList[record.serial_number]?.length > 0
+              ? handleRefund(record)
+              : handleFoundSingleDevice(propsUpdateSingleDevice)
+          }
           style={{
             ...GrayButton,
           }}
@@ -45,19 +107,12 @@ const ExpandedLostButton = ({
               }`,
             }}
           >
-            Mark as found
+            {cashReportList[record.serial_number]?.length > 0
+              ? "Refund"
+              : "Mark as found"}
           </p>
         </Button>
       </div>
-      {/* {chargeLostFeeModal && (
-        <ChargeLostFee
-          openModal={chargeLostFeeModal}
-          setOpenModal={setChargeLostFeeModal}
-          record={record}
-        />
-      )} */}
-
-    </>
   );
 };
 
