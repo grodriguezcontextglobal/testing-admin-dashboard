ea6641799f84b3da339754c24747b8851a506710
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-26 18:16:22 -0400

    icon props handle

--- src/components/icons/BluePlusIcon.jsx
+++ src/components/icons/BluePlusIcon.jsx
@@ -1,7 +1,7 @@
-export const BluePlusIcon = () => {
+export const BluePlusIcon = (props = null) => {
     return (
-      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none">
-        <path stroke="#0040C1" d="M10 4.167v11.666M4.167 10h11.666" />
+      <svg xmlns="http://www.w3.org/2000/svg" width={props.width ?? "20"} height={props.height ?? "20"} fill="none">
+        <path stroke={props.stroke ?? "#0040C1"} d="M10 4.167v11.666M4.167 10h11.666" />
       </svg>
     );
   };
