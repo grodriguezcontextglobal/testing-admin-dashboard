2b5bc735edb966138dcfc41119370d9e1ed573bc
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-26 18:17:10 -0400

    new reusable component for consumer detail header page

--- src/pages/consumers/MainPage.jsx
+++ src/pages/consumers/MainPage.jsx
@@ -3,17 +3,15 @@ import { Icon } from "@iconify/react";
 import {
   Button,
   Grid,
-  InputAdornment,
-  OutlinedInput,
-  Typography,
+  Typography
 } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
-import { Divider, Spin } from "antd";
+import { Spin } from "antd";
 import { useCallback, useEffect, useId, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
 import { devitrakApi } from "../../api/devitrakApi";
-import { MagnifyIcon } from "../../components/icons/MagnifyIcon";
+import Loading from "../../components/animation/Loading";
 import BannerMsg from "../../components/utils/BannerMsg";
 import { BlueButton } from "../../styles/global/BlueButton";
 import { BlueButtonText } from "../../styles/global/BlueButtonText";
@@ -21,15 +19,10 @@ import CenteringGrid from "../../styles/global/CenteringGrid";
 import { GrayButton } from "../../styles/global/GrayButton";
 import GrayButtonText from "../../styles/global/GrayButtonText";
 import "../../styles/global/OutlineInput.css";
-import { OutlinedInputStyle } from "../../styles/global/OutlinedInputStyle";
-import { Subtitle } from "../../styles/global/Subtitle";
 import TextFontsize18LineHeight28 from "../../styles/global/TextFontSize18LineHeight28";
-import { TextFontSize20LineHeight30 } from "../../styles/global/TextFontSize20HeightLine30";
-import { TextFontSize30LineHeight38 } from "../../styles/global/TextFontSize30LineHeight38";
-import RenderingConsumersChartsBehavior from "./components/RenderingConsumersChartsBehavior";
+import ConsumerHeader from "./components/ConsumerHeader";
 import TablesConsumers from "./tables/TablesConsumers";
 import { CreateNewConsumer } from "./utils/CreateNewUser";
-import Loading from "../../components/animation/Loading";
 const MainPage = () => {
   const [createUserButton, setCreateUserButton] = useState(false);
   const [counting, setCounting] = useState(null);
@@ -114,237 +107,14 @@ const MainPage = () => {
       }}
       container
     >
-      <Grid
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-        container
-      >
-        <Grid item xs={6}>
-          <Typography
-            textTransform={"none"}
-            style={{
-              ...TextFontSize30LineHeight38,
-              textAlign: "left",
-            }}
-          >
-            Consumers
-          </Typography>
-        </Grid>
-        <Grid
-          textAlign={"right"}
-          display={"flex"}
-          justifyContent={"flex-end"}
-          alignItems={"center"}
-          gap={1}
-          item
-          xs={6}
-        >
-          <Button onClick={() => setCreateUserButton(true)} style={BlueButton}>
-            <Icon
-              icon="ic:baseline-plus"
-              color="var(--base-white, #FFF"
-              width={20}
-              height={20}
-            />
-            &nbsp;
-            <Typography textTransform={"none"} style={BlueButtonText}>
-              Add new consumer
-            </Typography>
-          </Button>
-        </Grid>
-      </Grid>
-      <Divider />
-      <Grid
-        display={`${counting > 0 ? "flex" : "none"}`}
-        justifyContent={"flex-start"}
-        alignItems={"center"}
-        gap={1}
-        container
-      >
-        <Grid
-          textAlign={"right"}
-          flexDirection={"column"}
-          display={"flex"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
-          gap={1}
-          item
-          xs={12}
-          sm={12}
-          md={12}
-          lg={12}
-        >
-          <p
-            style={{
-              ...TextFontsize18LineHeight28,
-              textAlign: "left",
-              width: "100%",
-              color: " var(--Base-Black, #000)",
-            }}
-          >
-            Quick glance
-          </p>
-          <p style={{ ...Subtitle, textAlign: "left", width: "100%" }}>
-            Here are all the categories of devices within your inventory, and a
-            quick glance at devices locations.
-          </p>
-        </Grid>
-        <Grid
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          // overflow={"hidden"}
-          gap={2}
-          item
-          xs={12}
-          sm={12}
-          md={12}
-          lg={12}
-        >
-          <Grid
-            style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "flex-start",
-              alignSelf: "flex-start",
-            }}
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={6}
-          >
-            {" "}
-            <RenderingConsumersChartsBehavior
-              active={{
-                title: "Active",
-                number:
-                  allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                    ?.activeTransactions ?? 0,
-              }}
-              inactive={{
-                title: "Inactive",
-                number:
-                  allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                    ?.inactiveTransactions ?? 0,
-              }}
-              props={{
-                title: "General activity",
-                description:
-                  "Active consumers refers to those users currently holding one or more devices.", // from the database
-                total:
-                  allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                    ?.total ?? 0,
-              }}
-            />
-          </Grid>
-          <Grid
-            style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "flex-start",
-              alignSelf: "flex-start",
-            }}
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={6}
-          >
-            {" "}
-            <RenderingConsumersChartsBehavior
-              active={{
-                title: "Event",
-                number:
-                  allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                    ?.totalConsumersFromEvents ?? 0,
-              }}
-              inactive={{
-                title: "General",
-                number:
-                  allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                    ?.lease ?? 0,
-              }}
-              props={{
-                title: "Consumer origin",
-                description:
-                  "Consumers from an event typically spend a shorter time with your devices.",
-                total:
-                  Number(
-                    allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                      ?.totalConsumersFromEvents ?? 0
-                  ) +
-                  Number(
-                    allConsumersBasedOnEventsPerCompany?.data?.data?.result
-                      ?.lease ?? 0
-                  ),
-              }}
-            />
-          </Grid>
-        </Grid>
-        <Grid
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          item
-          xs={12}
-          sm={12}
-          md={12}
-          lg={12}
-        >
-          <p
-            style={{
-              ...TextFontsize18LineHeight28,
-              color: "var(--gray900)",
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              width: "fit-content",
-            }}
-          >
-            All consumers&nbsp;
-          </p>
-        </Grid>
-        <Divider style={{ margin: "20px 0 24px" }} />
-        <Grid
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          item
-          xs={12}
-          sm={12}
-          md={12}
-          lg={12}
-        >
-          <p
-            style={{
-              ...TextFontSize20LineHeight30,
-              display: "flex",
-              justifyContent: "flex-start",
-              alignItems: "center",
-              width: "fit-content",
-            }}
-          >
-            Search consumers:&nbsp;
-          </p>
-          <Grid item xs sm md lg>
-            <OutlinedInput
-              {...register("searchEvent")}
-              style={{ ...OutlinedInputStyle }}
-              fullWidth
-              placeholder="Search consumer here"
-              startAdornment={
-                <InputAdornment position="start">
-                  <MagnifyIcon />
-                </InputAdornment>
-              }
-            />
-          </Grid>
-        </Grid>
-      </Grid>
+      <ConsumerHeader
+        setCreateUserButton={setCreateUserButton}
+        counting={counting}
+        allConsumersBasedOnEventsPerCompany={
+          allConsumersBasedOnEventsPerCompany
+        }
+        register={register}
+      />
       <Grid
         marginY={3}
         display={`${counting > 0 ? "flex" : "none"}`}
