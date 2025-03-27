0639278529de1ed13bdf63664bed349b943935c9
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-03-27 13:32:23 -0400

    reusable functional component to send refund receipt email.

---
+++ src/pages/consumers/utils/sendRefundReceiptEmail.jsx
@@ -0,0 +1,32 @@
+import { devitrakApi } from "../../../api/devitrakApi";
+
+const sendRefundReceiptEmail = (props) => {
+  console.log(props);
+  const { customer, amount, paymentIntent, company, event } = props;
+  const sendEmail = async () => {
+    try {
+      const emailTemplate = {
+        email: customer.email,
+        amount: amount,
+        date: new Date().toString().slice(4, 15),
+        paymentIntent: paymentIntent,
+        customer: `${customer.name} ${customer.lastName}`,
+        company: company,
+        event: event
+      };
+      const response = await devitrakApi.post(
+        "/nodemailer/refund-notification",
+        emailTemplate
+      );
+      if (response.data) {
+        return response.data;
+      }
+    } catch (error) {
+      return error;
+    }
+  };
+  sendEmail();
+  return null;
+};
+
+export default sendRefundReceiptEmail;
