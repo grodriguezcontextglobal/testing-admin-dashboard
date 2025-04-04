0f11b4f65ef4cb102c78e93c1d50d2ef9f01da4a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-04 14:00:08 -0400

    set dynamic margin top to keep content close to top on every screen size.

--- src/pages/ParentRenderingChildrenPage.jsx
+++ src/pages/ParentRenderingChildrenPage.jsx
@@ -11,15 +11,16 @@ const ParentRenderingChildrenPage = () => {
       alignItems={"center"}
       maxWidth={"1228px"}
       minWidth={"768px"}
+      alignSelf={'flex-start'}
       container
     >
       <Grid
         alignSelf={"flex-start"}
         sx={{
           minHeight: "80dvh",
-          padding: { xs: "0 24px", sm: "0 24px", md: 0, lg: 0 },
+          padding: { xs: "0 12px", sm: "0 12px", md: 0, lg: 0 },
         }}
-        margin={"14dvh 0 1dvh"}
+        margin={"8rem 0 1rem"}
         item
         xs={11}
         sm={12}
