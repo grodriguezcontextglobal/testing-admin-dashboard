8dc67b2929f11d3c7d98a287a4d37c1205adb707
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-07 05:51:24 -0400

    remove suspense/lazy for no authenticated routes to avoid throw error.

--- src/routes/no-authorized/NoAuthRoutes.jsx
+++ src/routes/no-authorized/NoAuthRoutes.jsx
@@ -1,38 +1,30 @@
-import { Suspense, lazy } from "react";
+import { Route, Routes } from "react-router";
 import { Navigate } from "react-router-dom";
-import { Routes, Route } from "react-router";
-import Loading from "../../components/animation/Loading";
+// import Loading from "../../components/animation/Loading";
 import ResetPassword from "../../pages/authentication/ResetPassword";
-import CenteringGrid from "../../styles/global/CenteringGrid";
-import RegisterCompany from "../../pages/authentication/RegisterCompany";
+// import CenteringGrid from "../../styles/global/CenteringGrid";
 import InvitationLanding from "../../pages/authentication/InvitationLanding";
+import Login from "../../pages/authentication/Login";
+import RegisterCompany from "../../pages/authentication/RegisterCompany";
 import RegisterStripeConnectedAccount from "../../pages/authentication/RegisterStripeConnectedAccount";
+import Register from "../../pages/authentication/Registration";
 
 const NoAuthRoutes = () => {
-  const Register = lazy(() =>
-    import("../../pages/authentication/Registration")
-  );
-  const Login = lazy(() => import("../../pages/authentication/Login"));
   return (
     <div style={{ width: "100%", margin: "auto" }}>
-      <Suspense
-        fallback={
-          <div style={CenteringGrid}>
-            <Loading />
-          </div>
-        }
-      >
-        <Routes>
-          <Route path="/authenticate" element={""} />
-          <Route path="/reset-password" element={<ResetPassword />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/register/company-setup" element={<RegisterCompany />} />
-          <Route path="/register/connected-account" element={<RegisterStripeConnectedAccount />} />
-          <Route path="/invitation" element={<InvitationLanding />} />
-          <Route path="/*" element={<Navigate to="/login" replace />} />
-        </Routes>
-      </Suspense>
+      <Routes>
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/authenticate" element={""} />
+        <Route path="/register/company-setup" element={<RegisterCompany />} />
+        <Route
+          path="/register/connected-account"
+          element={<RegisterStripeConnectedAccount />}
+        />
+        <Route path="/invitation" element={<InvitationLanding />} />
+        <Route path="/*" element={<Navigate to="/login" replace />} />
+      </Routes>
     </div>
   );
 };
