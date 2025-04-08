3a82b6940efe4d8e94e588bc67d51079fbc7d884
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-08 17:22:39 -0400

    main file page to structure post dashboard.

---
+++ src/pages/posts/MainPage.jsx
@@ -0,0 +1,65 @@
+import { Grid } from "@mui/material";
+import Header from "./components/Header";
+import { useEffect, useState } from "react";
+import BannerMsg from "../../components/utils/BannerMsg";
+import { BlueButton } from "../../styles/global/BlueButton";
+import { BlueButtonText } from "../../styles/global/BlueButtonText";
+import MainGridPosts from "./components/MainGridPosts";
+import { useQuery } from "@tanstack/react-query";
+import { useSelector } from "react-redux";
+import { devitrakApi } from "../../api/devitrakApi";
+const MainPage = () => {
+  const { user } = useSelector((state) => state.admin);
+  const [posts, setPosts] = useState([]);
+  const postsCompanyData = useQuery({
+    queryKey: ["postsCompanyData"],
+    queryFn: () =>
+      devitrakApi.get(`/posts/posts-company?company=${user.companyData.id}`),
+    refetchOnMount: false,
+  });
+  useEffect(() => {
+    //fetch logic
+    const controller = new AbortController();
+    postsCompanyData.refetch();
+    return () => {
+      controller.abort();
+    };
+  }, []);
+
+  useEffect(() => {
+    const controller = new AbortController();
+    if (postsCompanyData.data) {
+      setPosts(postsCompanyData.data.data.list);
+    }
+    return () => {
+      controller.abort();
+    };
+  }, [postsCompanyData.data]);
+
+  return (
+    <Grid container>
+      <Header />
+      {posts.length > 0 ? (
+        <MainGridPosts
+          props={{
+            data: posts,
+          }}
+        />
+      ) : (
+        <BannerMsg
+          props={{
+            title: "Add your first post",
+            message:
+              "Posts in this section will show in the consumer side of the app. These posts can be tutorials for how to use devices, how to return them, or any other information you’d like to communicate to your consumer.",
+            link: "/posts/new-post",
+            button: BlueButton,
+            paragraphStyle: BlueButtonText,
+            paragraphText: "Add new post",
+          }}
+        />
+      )}
+    </Grid>
+  );
+};
+
+export default MainPage;
