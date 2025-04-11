2f387d8c5151a8a0c85954df8c71157cac6d5a1a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-11 08:20:05 -0400

    functional component (child component) to manage all articles created from company.

--- src/pages/posts/components/MainGridPosts.jsx
+++ src/pages/posts/components/MainGridPosts.jsx
@@ -1,108 +1,99 @@
-import { Grid, Pagination, PaginationItem } from "@mui/material";
-import { Card } from "antd";
+import { Grid } from "@mui/material";
+import { Card, message, Space, Switch } from "antd";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { devitrakApi } from "../../../api/devitrakApi";
 const { Meta } = Card;
 
-const MainGridPosts = () => {
-  const articles = [
-    {
-      id: 1,
-      title: "Worem ipsum dolor sit amet",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 2,
-      title: "Horem ipsum dolor sit amet",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 3,
-      title: "Korem ipsum dolor sit amet",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 4,
-      title: "Another article heading",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 5,
-      title: "Yet another heading",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 6,
-      title: "Sample article heading",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 7,
-      title: "Lorem ipsum title",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 8,
-      title: "Short title here",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-    {
-      id: 9,
-      title: "Last example article",
-      description: "Consectetur adipiscing elit.",
-      image:
-        "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
-    },
-  ];
+const MainGridPosts = ({ data, refetch }) => {
   const navigate = useNavigate();
-  const [page, setPage] = useState(1);
-  const handleChange = (event, value) => {
-    setPage(value);
+  // const [page, setPage] = useState(0);
+  const [loadingStatus, setLoadingStatus] = useState(null);
+  // const handleChange = (_, value) => {
+  //   setPage(value);
+  // };
+
+  const handleSwitchChange = async (props) => {
+    try {
+      setLoadingStatus(props.id);
+      const response = await devitrakApi.patch(
+        `/post/post-update/${props.id}`,
+        {
+          ...props.article,
+          published: props.published,
+        }
+      );
+      if (response.data.ok) {
+        refetch();
+        return setLoadingStatus(null);
+      }
+      return setLoadingStatus(null);
+    } catch (error) {
+      message.error(error.message);
+      return setLoadingStatus(null);
+    }
   };
 
   return (
-    <div style={{ padding: "1rem" }}>
-      <Grid container spacing={3}>
-        {articles.map((article) => (
-          <Grid item xs={12} sm={6} md={4} key={article.id}>
+    <div style={{ padding: "1rem 0" }}>
+      <Space size={[8, 16]} wrap>
+        {data.map((article) => (
+          <Grid item xs={12} key={article.id}>
             <Card
-              onClick={() => navigate(`/posts/post/${article.id}`)}
-              style={{ width: 300, padding: "24px", cursor: "pointer" }}
+              onClick={() =>
+                navigate(`/posts/post/${article.id}`, {
+                  state: { id: article.id },
+                })
+              }
+              style={{ padding: "24px", cursor: "pointer" }}
+              styles={{
+                actions: {
+                  border: "none",
+                  padding: "0",
+                },
+              }}
               cover={
                 <img
-                  src={article.image}
-                  alt={article.image}
+                  src={article.media.cover}
+                  alt={article.media.cover}
                   width={"100%"}
                   height={"100%"}
                 />
               }
-              actions={[]}
+              actions={[
+                <Switch
+                  key={`switch-${article.id}`}
+                  checkedChildren="Published"
+                  uncheckedChildren="No published"
+                  defaultChecked={article.published}
+                  loading={loadingStatus === article.id}
+                  onChange={(e) =>
+                    handleSwitchChange({
+                      id: article.id,
+                      published: e,
+                      article: article,
+                    })
+                  }
+                  style={{}}
+                />,
+              ]}
             >
-              <Meta title={article.title} description={article.description} />
+              <Meta title={article.title} description={article.subtitle} />
             </Card>{" "}
           </Grid>
         ))}
-      </Grid>
-      <Grid container>
-        <Grid item xs={12} sm={12} md={12} lg={12} key="posts-footer-pagination">
+      </Space>
+      {/* <Grid container>
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          md={12}
+          lg={12}
+          key="posts-footer-pagination"
+        >
           <Pagination
-            count={10}
+            count={data.length}
             page={page}
             onChange={handleChange}
             renderItem={(item) => <PaginationItem {...item} />}
@@ -116,9 +107,77 @@ const MainGridPosts = () => {
             }}
           />{" "}
         </Grid>
-      </Grid>
+      </Grid> */}
     </div>
   );
 };
 
 export default MainGridPosts;
+
+
+
+  // const articles = [
+  //   {
+  //     id: 1,
+  //     title: "Worem ipsum dolor sit amet",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Horem ipsum dolor sit amet",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Korem ipsum dolor sit amet",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Another article heading",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 5,
+  //     title: "Yet another heading",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 6,
+  //     title: "Sample article heading",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 7,
+  //     title: "Lorem ipsum title",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 8,
+  //     title: "Short title here",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  //   {
+  //     id: 9,
+  //     title: "Last example article",
+  //     description: "Consectetur adipiscing elit.",
+  //     image:
+  //       "https://fastly.picsum.photos/id/602/600/600.jpg?hmac=bzg_XbKsV7t3KThEGI94sZK2Xh0p8L6Qo7U7Ms1i4wQ",
+  //   },
+  // ];
