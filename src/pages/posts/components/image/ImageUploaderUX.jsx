f128913c404f778a038ff4d1a02dd61399abc591
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-11 08:21:34 -0400

    remove context and pass function as prop to collect new media file uploaded.

--- src/pages/posts/components/image/ImageUploaderUX.jsx
+++ src/pages/posts/components/image/ImageUploaderUX.jsx
@@ -1,13 +1,15 @@
 import { Grid, TextField, Typography } from "@mui/material";
 import { Avatar } from "antd";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { UploadIcon } from "../../../../components/icons/UploadIcon";
-import { useContext } from "react";
-import { ImageUploaderContext } from "../../action/NewPost";
 
-const ImageUploaderUX = () => {
-  const selectedImageValue = useContext(ImageUploaderContext);
-  const { register, watch } = useForm();
+const ImageUploaderUX = ({ CSS, setImageUploadedValue }) => {
+  const { register, watch } = useForm({
+    defaultValues:{
+      photo: null,
+    }
+  });
   const styling = {
     textTransform: "none",
     textAlign: "left",
@@ -19,77 +21,96 @@ const ImageUploaderUX = () => {
     color: "var(--gray-700, #344054)",
   };
 
-  console.log(watch("photo"));
-  console.log(selectedImageValue(watch("photo")));
+  const formStyle = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    borderRadius: "12px",
+    border: "1px solid var(--gray-200, #EAECF0)",
+    background: "var(--base-white, #FFF)",
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const media = watch("photo");
+    console.log(media)
+    if (media !== null && media.length > 0) {
+      setImageUploadedValue(media);
+    }
+    return () => {
+      controller.abort();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [watch("photo") !== null && watch("photo").length > 0]);
+
   return (
-    <form
+    <div
       style={{
-        display: "flex",
+        ...CSS,
         flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-        borderRadius: "12px",
-        border: "1px solid var(--gray-200, #EAECF0)",
-        background: "var(--base-white, #FFF)",
       }}
     >
-      <Grid
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        padding={0}
-        item
-        xs={12}
-      >
-        <Avatar
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            border: "6px solid var(--gray-50, #F9FAFB)",
-            background: "6px solid var(--gray-50, #F9FAFB)",
-            borderRadius: "28px",
-          }}
+      <h1 style={{ textAlign: "left", width: "100%" }}>Cover (image/video)</h1>
+      <form style={formStyle}>
+        <Grid
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          padding={0}
+          item
+          xs={12}
         >
-          {" "}
-          <UploadIcon />
-        </Avatar>
-      </Grid>
-      <Grid
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        item
-        xs={12}
-      >
-        <TextField
-          {...register("photo")}
-          type="file"
-          className="photo_input"
-          accept=".jpeg, .png, .jpg"
-          style={{
-            outline: "none",
-            border: "transparent",
-          }}
-        />
-      </Grid>
-      <Grid
-        display={"flex"}
-        justifyContent={"center"}
-        alignItems={"center"}
-        marginBottom={2}
-        item
-        xs={12}
-      >
-        <Typography
-          color={"var(--gray-600, #475467)"}
-          style={{ ...styling, fontWeight: 400 }}
+          <Avatar
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              border: "6px solid var(--gray-50, #F9FAFB)",
+              background: "6px solid var(--gray-50, #F9FAFB)",
+              borderRadius: "28px",
+            }}
+          >
+            {" "}
+            <UploadIcon />
+          </Avatar>
+        </Grid>
+        <Grid
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          item
+          xs={12}
         >
-          SVG, PNG, JPG or GIF (max. 1MB)
-        </Typography>
-      </Grid>
-    </form>
+          <TextField
+            {...register("photo")}
+            type="file"
+            className="photo_input"
+            accept=".jpeg, .png, .jpg"
+            style={{
+              outline: "none",
+              border: "transparent",
+            }}
+          />
+        </Grid>
+        <Grid
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          marginBottom={2}
+          item
+          xs={12}
+        >
+          <Typography
+            color={"var(--gray-600, #475467)"}
+            style={{ ...styling, fontWeight: 400 }}
+          >
+            SVG, PNG, JPG or GIF (max. 5MB)
+          </Typography>
+        </Grid>
+      </form>
+    </div>
   );
 };
 
