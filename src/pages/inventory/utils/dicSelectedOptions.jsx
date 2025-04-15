d03a9fac994b3d8289e29a0371544f9c20392613
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-15 11:20:09 -0400

    reusable component for display options.

---
+++ src/pages/inventory/utils/dicSelectedOptions.jsx
@@ -0,0 +1,14 @@
+export const dicSelectedOptions = {
+  0: "Brand",
+  1: "Model",
+  2: "Serial Number",
+  3: "Location",
+  4: "Ownership",
+  5: "Status",
+};
+
+export const dictionary = {
+  Permanent: "Owned",
+  Rent: "Leased",
+  Sale: "For sale",
+};
