dd253a23ac6a420ea7d64243f897789be91badb1
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-21 13:05:55 -0400

    new reusable component to collect extra info and sub location of an item and render them through an array.

---
+++ src/pages/inventory/details/detailComponent/components/ExtraInformationItemComponent.jsx
@@ -0,0 +1,60 @@
+import { Grid } from "@mui/material";
+import { checkValidJSON } from "../../../../../components/utils/checkValidJSON";
+import TextFontsize18LineHeight28 from "../../../../../styles/global/TextFontSize18LineHeight28";
+import CardRendered from "../../../utils/CardRendered";
+
+const ExtraInformationItemComponent = ({ dataFound }) => {
+  const sortAndMergeData = () => {
+    const result = new Set();
+    for (let [key, value] of Object.entries(dataFound[0])) {
+      if (key === "sub_location" && checkValidJSON(value).length > 0) {
+        const subLocations = checkValidJSON(value);
+        subLocations.map((item, index) => {
+          if (index === 0)
+            return result.add({ name: "sub_location", value: item });
+          else
+            return result.add({
+              name: `sub_location_${index + 1}`,
+              value: item,
+            });
+        });
+      }
+      if (key === "extra_serial_number" && checkValidJSON(value).length > 0) {
+        const extraSerialNumber = checkValidJSON(value);
+        extraSerialNumber.map((item) => {
+          return result.add({
+            name: item.keyObject,
+            value: item.valueObject,
+          });
+        });
+      }
+    }
+    return Array.from(result);
+  };
+
+  return (
+    <>
+      <h1 style={{ ...TextFontsize18LineHeight28, width: "100%", textAlign:"left", margin:".5rem 0" }}>Extra information</h1>
+      <Grid
+        display={"flex"}
+        justifyContent={"flex-start"}
+        container
+        spacing={2}
+      >
+        {sortAndMergeData().map((item) => {
+          return (
+            <Grid key={item.value} item xs={12} sm={12} md={4} lg={4}>
+              <CardRendered
+                props={item.value}
+                title={item.name}
+                optional={null}
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </>
+  );
+};
+
+export default ExtraInformationItemComponent;
