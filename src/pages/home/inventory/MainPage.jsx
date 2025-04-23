1795aa491a847d78f4eea4548a19e45d9e85052a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-23 16:17:36 -0400

    refactor layout to set responsive behavior.

--- src/pages/home/inventory/MainPage.jsx
+++ src/pages/home/inventory/MainPage.jsx
@@ -14,54 +14,92 @@ const TableLocations = lazy(() =>
 );
 const MainPage = () => {
   return (
-    <Suspense
-      fallback={
-        <div style={CenteringGrid}>
-          <Spin indicator={<Loading />} fullscreen />
-        </div>
-      }
-    >
-      <Grid
-        textAlign={"right"}
-        display={"flex"}
-        justifyContent={"flex-start"}
-        alignItems={"center"}
-        gap={1}
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={12}
+    <Grid container spacing={1}>
+      <Suspense
+        fallback={
+          <div style={CenteringGrid}>
+            <Spin indicator={<Loading />} fullscreen />
+          </div>
+        }
       >
-        <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={4} lg={4}>
-          <Graphic />
-        </Grid>
         <Grid
-          alignSelf={"flex-start"}
-          style={{ margin: "-10px 0 0 0", padding: 0 }}
-          item
-          xs={12}
-          sm={12}
-          md={4}
-          lg={4}
+          // textAlign={"right"}
+          // display={"flex"}
+          // justifyContent={"flex-start"}
+          // alignItems={"center"}
+          // gap={1}
+          // item
+          // xs={12}
+          // sm={12}
+          // md={12}
+          // lg={12}
+          container
+
+          gap={2}
         >
-          <TotalConsumer />
-          <TotalDevice />
-          <TableLocations />
+          <Grid alignSelf={"flex-start"} item xs={10} sm={10} md={3} lg={3}>
+            <Graphic />
+          </Grid>
+          <Grid
+            alignSelf={"flex-start"}
+            style={{ margin: "-10px 0 0 0", padding: 0 }}
+            item
+            xs={10}
+            sm={10}
+            md={4}
+            lg={4}
+          >
+            <Grid
+              alignSelf={"flex-start"}
+              style={{ padding: 0 }}
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+            >
+              {" "}
+              <TotalConsumer />
+            </Grid>
+            <Grid
+              alignSelf={"flex-start"}
+              style={{ padding: 0 }}
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+            >
+              {" "}
+              <TotalDevice />
+            </Grid>
+            <Grid
+              alignSelf={"flex-start"}
+              style={{ padding: 0 }}
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+            >
+              {" "}
+              <TableLocations />
+            </Grid>
+          </Grid>
+          <Grid
+            alignSelf={"flex-start"}
+            style={{ margin: "-10px 0 0 0", padding: 0 }}
+            item
+            xs={10}
+            sm={10}
+            md={4}
+            lg={4}
+          >
+            <CategoryInventory />
+          </Grid>
         </Grid>
-        <Grid
-          alignSelf={"flex-start"}
-          style={{ margin: "-10px 0 0 0", padding: 0 }}
-          item
-          xs={12}
-          sm={12}
-          md={4}
-          lg={4}
-        >
-          <CategoryInventory />
-        </Grid>
-      </Grid>
-    </Suspense>
+      </Suspense>{" "}
+    </Grid>
   );
 };
 
