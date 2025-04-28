9326a10be6e3ce5fe67b59f8e7d1dacb89f88d5d
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-27 21:58:01 -0400

    set up responsive behavior to be fit into multiple spaces by adding space html tag from ant design ui library.

--- src/pages/inventory/utils/FilterOptionsUX.jsx
+++ src/pages/inventory/utils/FilterOptionsUX.jsx
@@ -1,9 +1,9 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { Select } from "antd";
+import { Select, Space } from "antd";
 import TextFontsize18LineHeight28 from "../../../styles/global/TextFontSize18LineHeight28";
 import { dicSelectedOptions } from "./dicSelectedOptions";
 
-const FilterOptionsUX = ({filterOptions, chosen, setChosen}) => {
+const FilterOptionsUX = ({ filterOptions, chosen, setChosen }) => {
   return (
     <div
       style={{
@@ -16,46 +16,48 @@ const FilterOptionsUX = ({filterOptions, chosen, setChosen}) => {
         alignItems: "center",
       }}
     >
-      {new Array(6).fill(null).map((item, index) => {
-        return (
-          <Select
-            style={{
-              margin: "0 5px 0 0",
-              width: "fit-content",
-              overflowY: "hidden",
-            }}
-            key={index}
-            title={dicSelectedOptions[index]}
-            prefix={dicSelectedOptions[index]}
-            suffixIcon={
-              <Icon
-                icon="fluent:chevron-down-12-filled"
-                style={{ color: "var(--gray-600, #475467)" }}
-              />
-            }
-            value={chosen.category === index ? chosen.value : null}
-            options={[
-              ...filterOptions[index].map((item) => ({
-                value: item,
-                label: item,
-              })),
-            ]}
-            allowClear
-            onChange={(value) => {
-              if (value === undefined || value === null) {
-                return setChosen({
-                  category: null,
-                  value: null,
-                });
+      <Space size={[8, 16]} wrap>
+        {new Array(6).fill(null).map((_, index) => {
+          return (
+            <Select
+              style={{
+                margin: "0 5px 0 0",
+                width: "fit-content",
+                overflowY: "hidden",
+              }}
+              key={index}
+              title={dicSelectedOptions[index]}
+              prefix={dicSelectedOptions[index]}
+              suffixIcon={
+                <Icon
+                  icon="fluent:chevron-down-12-filled"
+                  style={{ color: "var(--gray-600, #475467)" }}
+                />
               }
-              return setChosen({
-                category: value === null ? null : index,
-                value: value,
-              });
-            }}
-          />
-        );
-      })}
+              value={chosen.category === index ? chosen.value : null}
+              options={[
+                ...filterOptions[index].map((item) => ({
+                  value: item,
+                  label: item,
+                })),
+              ]}
+              allowClear
+              onChange={(value) => {
+                if (value === undefined || value === null) {
+                  return setChosen({
+                    category: null,
+                    value: null,
+                  });
+                }
+                return setChosen({
+                  category: value === null ? null : index,
+                  value: value,
+                });
+              }}
+            />
+          );
+        })}
+      </Space>
     </div>
   );
 };
