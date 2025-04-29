db82209ed9fa544f3e8840eda5a16d28ba9bf50d
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-29 19:20:14 -0400

    add grip behavior for re locate elements when are displayed in multiple screen sizes.

--- src/pages/inventory/utils/TreeView.jsx
+++ src/pages/inventory/utils/TreeView.jsx
@@ -1,14 +1,17 @@
 // TreeView.jsx
 import TreeNode from "./TreeNode";
 import "../style/viewtree.css";
+import { Grid } from "@mui/material";
 
 const TreeView = ({ data }) => {
   return (
-    <div className="tree-container">
+    <Grid container gap={2}>
       {Object.entries(data).map(([location, details]) => (
-        <TreeNode key={location} nodeName={location} nodeData={details} />
+        <Grid key={location} margin={'auto'} item xs={10} sm={10} md={12} lg={12}>
+          <TreeNode key={location} nodeName={location} nodeData={details} />
+        </Grid>
       ))}
-    </div>
+    </Grid>
   );
 };
 
