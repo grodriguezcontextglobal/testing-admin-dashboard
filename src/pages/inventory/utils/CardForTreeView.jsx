bb5492a21b2a5dfaabab70a2ea4b1d8cf9a38c69
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-29 19:29:26 -0400

    set up grip variants for displaying children.

--- src/pages/inventory/utils/CardForTreeView.jsx
+++ src/pages/inventory/utils/CardForTreeView.jsx
@@ -1,58 +1,57 @@
 import { Grid } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useId } from "react";
 import CardLocations from "./CardLocations";
 import TreeView from "./TreeView";
-import { useId } from "react";
 
 const CardForTreeView = (props) => {
   const { item, dictionary, searchItem } = props;
   const elemId = useId();
   if (item.tree) {
     return (
-      <Grid
-        key={elemId}
-        id="grip-tree-view"
-        alignSelf={"flex-start"}
-        item
-        xs={12}
-        sm={12}
-        md={12}
-        lg={12}
-      >
-        <TreeView data={item.data} />
-      </Grid>
+      <div style={{ margin: ".5rem 0 0", width: "100%" }}>
+        <TreeView key={elemId} data={item.data} />
+      </div>
     );
   } else {
-    return item.data.map((opt) => {
-      return (
-        <Grid
-          key={`${elemId}-${opt.key}`}
-          alignSelf={"flex-start"}
-          item
-          xs={11}
-          sm={11}
-          md={4}
-          lg={4}
-        >
-          {" "}
-          <Link
-            to={`/inventory/${String(
-              item.routeTitle
-            ).toLowerCase()}?${decodeURI(opt.key)}&search=${
-              searchItem && searchItem
-            }`}
-          >
-            <CardLocations
-              title={dictionary[opt.key] ?? opt.key}
-              props={`${opt.value} total devices`}
-              optional={null}
-              style={{ width: "fit-content" }}
-              width="fit-content"
-            />
-          </Link>
-        </Grid>
-      );
-    });
+    return (
+      <Grid container spacing={1}>
+        {item.data.map((opt) => {
+          return (
+            <Grid
+              key={`${elemId}-${opt.key}`}
+              alignSelf={"stretch"}
+              sx={{
+                margin: {
+                  xs: "-0.3rem auto",
+                  sm: "-0.3rem auto 0",
+                  md: "0.3rem 0 0",
+                  lg: "0.3rem 0 0",
+                },
+                padding: 0,
+              }}
+              item
+              xs={10}
+              sm={10}
+              md={4}
+              lg={4}
+            >
+              <CardLocations
+                navigate={`/inventory/${String(
+                  item.routeTitle
+                ).toLowerCase()}?${decodeURI(opt.key)}&search=${
+                  searchItem && searchItem
+                }`}
+                title={dictionary[opt.key] ?? opt.key}
+                props={`${opt.value} total devices`}
+                optional={null}
+                style={{ width: "100%", padding: 0 }}
+                width="100%"
+              />
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
   }
 };
 
