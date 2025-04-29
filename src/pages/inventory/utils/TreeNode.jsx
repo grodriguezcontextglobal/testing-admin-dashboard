29b1ed8558676edd211cc34ef9d12fe67cbbed23
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-29 19:46:38 -0400

    add UI for navigation.

--- src/pages/inventory/utils/TreeNode.jsx
+++ src/pages/inventory/utils/TreeNode.jsx
@@ -1,6 +1,8 @@
 // TreeNode.jsx
 import { useState } from "react";
 import "../style/viewtree.css";
+import { Button } from "antd";
+import { RightNarrowInCircle } from "../../../components/icons/RightNarrowInCircle";
 
 const TreeNode = ({ nodeName, nodeData }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,11 +14,36 @@ const TreeNode = ({ nodeName, nodeData }) => {
   const toggleOpen = () => {
     if (children) setIsOpen(!isOpen);
   };
+  const style = {
+    backgroundColor: "transparent",
+    border: "none",
+    outline: "none",
+    margin: 0,
+    padding: 0,
+  };
 
   return (
     <div key={nodeName} className="tree-card">
-      <div onClick={toggleOpen} style={{ cursor: children ? "pointer" : "default" }}>
-        <div className="tree-title">{nodeName}</div>
+      <div
+        style={{
+          cursor: children ? "pointer" : "default",
+        }}
+      >
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Button className="tree-title" style={style} onClick={toggleOpen}>
+            {nodeName}
+          </Button>{" "}
+          <Button style={style} onClick={() => console.log(nodeName)}>
+            <RightNarrowInCircle />
+          </Button>
+        </div>
         <div className="tree-sub">
           Total: {total}, Available: {available}
         </div>
