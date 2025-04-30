8b8acc186d876077143f824eb05448d061083f80
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-04-30 06:37:41 -0400

    new css variants add and rendered based on screen size.

--- src/pages/ParentRenderingChildrenPage.jsx
+++ src/pages/ParentRenderingChildrenPage.jsx
@@ -24,13 +24,13 @@ const ParentRenderingChildrenPage = () => {
             md: "calc(100dvh - 80px)",
             lg: "calc(100dvh - 80px)",
           },
-          padding: { xs: 0, sm: 0, md: "0 20px", lg: 0 },
+          padding: { xs: "0 20px", sm: "0 20px", md:"0 20px", lg: 0 },
           margin: "1rem 0 1rem",
         }}
         // margin={"8rem 0 1rem"}
         item
-        xs={11}
-        sm={11}
+        xs={12}
+        sm={12}
         md={12}
         lg={12}
       >
