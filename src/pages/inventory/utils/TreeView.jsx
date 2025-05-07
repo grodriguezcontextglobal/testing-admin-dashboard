885b5e16a2f7a6e4e520195cf6c5f2a6bcf36829
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-05-07 17:18:18 -0400

    refactoring component to handle new route.

--- src/pages/inventory/utils/TreeView.jsx
+++ src/pages/inventory/utils/TreeView.jsx
@@ -5,12 +5,29 @@ import { Grid } from "@mui/material";
 
 const TreeView = ({ data }) => {
   return (
-    <Grid container gap={2}>
-      {Object.entries(data).map(([location, details]) => (
-        <Grid key={location} margin={'auto'} item xs={12} sm={12} md={12} lg={12}>
-          <TreeNode key={location} nodeName={location} nodeData={details} />
-        </Grid>
-      ))}
+    <Grid container gap={1}>
+      {Object.entries(data).map(
+        ([location, details]) => (
+          (
+            <Grid
+              key={location}
+              margin={"auto"}
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+            >
+              <TreeNode
+                key={location}
+                nodeName={location}
+                nodeData={details}
+                path={[location]} // start path
+              />
+            </Grid>
+          )
+        )
+      )}
     </Grid>
   );
 };
