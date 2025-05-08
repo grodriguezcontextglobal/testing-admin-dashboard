9516763090f9f24c99b2e75e3bb9a72459cb3e4a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-05-08 14:14:44 -0400

    refactoring component to use new component imported to handle data other than tree view.

--- src/pages/inventory/utils/CardForTreeView.jsx
+++ src/pages/inventory/utils/CardForTreeView.jsx
@@ -1,43 +1,19 @@
-import { Space } from "antd";
 import { useId } from "react";
-import CardLocations from "./CardLocations";
+import RenderingMoreThanTreeviewElements from "./RenderingMoreThanTreeviewElements";
 import TreeView from "./TreeView";
 
 const CardForTreeView = (props) => {
   const { item, dictionary, searchItem } = props;
   const elemId = useId();
   if (item.tree) {
-    return (
-      <div id={`${elemId}`} style={{ width: "100%" }}>
-        <TreeView id={`${elemId}`} key={elemId} data={item.data} />
-      </div>
-    );
+    return <TreeView id={`${elemId}`} key={elemId} data={item.data} />;
   } else {
     return (
-      <Space
-        style={{
-          margin: ".5rem 0 0",
-        }}
-        wrap
-        size={[8, 16]}
-      >
-        {item.data.map((opt) => {
-          return (
-            <CardLocations
-              id={`card-${elemId}-`}
-              key={`${elemId}-${opt?.key}`}
-              navigate={`/inventory/${String(
-                item.routeTitle
-              ).toLowerCase()}?${decodeURI(opt?.key)}&search=${
-                searchItem && searchItem
-              }`}
-              title={dictionary[opt?.key] ?? opt?.key}
-              props={`${opt?.value} total devices`}
-              optional={null}
-            />
-          );
-        })}
-      </Space>
+        <RenderingMoreThanTreeviewElements
+          item={item}
+          dictionary={dictionary}
+          searchItem={searchItem}
+        />
     );
   }
 };
