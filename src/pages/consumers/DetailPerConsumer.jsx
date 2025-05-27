6568921fcd711a0d1deb48d8bd4ae22ca9b848a7
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-05-27 14:55:21 -0400

    feat(consumers): add create new consumer modal functionality
    - Introduce state management for create user button
    - Implement CreateNewConsumer modal component
    - Replace link with button that triggers modal
    - Maintain existing layout and styling while adding new feature

--- src/pages/consumers/DetailPerConsumer.jsx
+++ src/pages/consumers/DetailPerConsumer.jsx
@@ -32,8 +32,10 @@ import { WhiteCirclePlusIcon } from "../../components/icons/WhiteCirclePlusIcon"
 import AssigmentAction from "./components/AssigmentAction";
 import NotesRendering from "./components/NotesCard";
 import StripeTransactionPerConsumer from "./tables/StripeTransactionPerConsumer";
+import { CreateNewConsumer } from "./utils/CreateNewUser";
 
 const DetailPerConsumer = () => {
+  const [createUserButton, setCreateUserButton] = useState(false);
   const { register, watch, setValue } = useForm();
   const { customer } = useSelector((state) => state.customer);
   const { user } = useSelector((state) => state.admin);
@@ -161,74 +163,76 @@ const DetailPerConsumer = () => {
     ];
 
     return (
-      <Grid
-        key={customer.id}
-        style={{
-          padding: "5px",
-          display: "flex",
-          justifyContent: "center",
-          alignSelf: "stretch",
-        }}
-        container
-        ref={rowRef}
-      >
+      <>
         <Grid
+          key={customer.id}
           style={{
+            padding: "5px",
             display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
+            justifyContent: "center",
+            alignSelf: "stretch",
           }}
           container
+          ref={rowRef}
         >
           <Grid
             style={{
               display: "flex",
               justifyContent: "space-between",
               alignItems: "center",
-              margin: "0 0 1.5dvh",
             }}
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={6}
+            container
           >
-            <Typography
-              textTransform={"none"}
-              style={TextFontSize30LineHeight38}
+            <Grid
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                margin: "0 0 1.5dvh",
+              }}
+              item
+              xs={12}
+              sm={12}
+              md={8}
+              lg={8}
             >
-              Consumer
-            </Typography>
-          </Grid>
-          {/* /event/new_subscription */}
-          <Grid
-            sx={{
-              display: "flex",
-              justifyContent: {
-                xs: "flex-start",
-                sm: "flex-start",
-                md: "flex-end",
-                lg: "flex-end",
-              },
-              alignItems: "center",
-              margin: {
-                xs: "0.5rem 0",
-                sm: "0.5rem 0",
-                md: "0.5rem 0",
-                lg: "0.5rem 0",
-              },
-            }}
-            item
-            xs={12}
-            sm={12}
-            md={6}
-            lg={6}
-          >
-            <Link to="/create-event-page/event-detail">
+              <Typography
+                textTransform={"none"}
+                style={TextFontSize30LineHeight38}
+              >
+                Consumer
+              </Typography>
+            </Grid>
+            {/* /event/new_subscription */}
+            <Grid
+              sx={{
+                display: "flex",
+                justifyContent: {
+                  xs: "flex-start",
+                  sm: "flex-start",
+                  md: "flex-end",
+                  lg: "flex-end",
+                },
+                alignItems: "center",
+                margin: {
+                  xs: "0.5rem 0",
+                  sm: "0.5rem 0",
+                  md: "0.5rem 0",
+                  lg: "0.5rem 0",
+                },
+              }}
+              item
+              xs={12}
+              sm={12}
+              md={4}
+              lg={4}
+            >
               <Button
+              onClick={() => setCreateUserButton(true)}
                 style={{
                   ...BlueButton,
                   ...CenteringGrid,
+                  margin:0
                 }}
               >
                 <Typography
@@ -241,190 +245,198 @@ const DetailPerConsumer = () => {
                   &nbsp;Add new consumer
                 </Typography>
               </Button>
-            </Link>
+            </Grid>
           </Grid>
-        </Grid>
-        <Grid
-          style={{
-            paddingTop: "0px",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-          container
-        >
           <Grid
-            display={"flex"}
-            justifyContent={"flex-start"}
-            alignItems={"center"}
-            item
-            xs={12}
+            style={{
+              paddingTop: "0px",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+            }}
+            container
           >
-            <Breadcrumb path={breadcrumbItems} />
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-start"}
+              alignItems={"center"}
+              item
+              xs={12}
+            >
+              <Breadcrumb path={breadcrumbItems} />
+            </Grid>
           </Grid>
-        </Grid>
-        <Divider />
-        <Grid
-          gap={"5px"}
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          alignSelf={"flex-start"}
-          container
-        >
+          <Divider />
           <Grid
+            gap={"5px"}
             display={"flex"}
-            justifyContent={"flex-start"}
-            alignItems={"center"}
-            item
-            xs={12}
-            sm={12}
-            md={3}
-            lg={3}
-          >
-            <ConsumerDetailInformation />
-          </Grid>
-          <Grid
-            display={"flex"}
-            justifyContent={"flex-start"}
+            justifyContent={"space-between"}
             alignItems={"center"}
             alignSelf={"flex-start"}
-            item
-            xs={12}
-            sm={12}
-            md={4}
-            lg={4}
+            container
           >
-            <ConsumerDetailInfoCntact />
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-start"}
+              alignItems={"center"}
+              item
+              xs={12}
+              sm={12}
+              md={3}
+              lg={3}
+            >
+              <ConsumerDetailInformation />
+            </Grid>
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-start"}
+              alignItems={"center"}
+              alignSelf={"flex-start"}
+              item
+              xs={12}
+              sm={12}
+              md={4}
+              lg={4}
+            >
+              <ConsumerDetailInfoCntact />
+            </Grid>
+            <Grid
+              display={"flex"}
+              flexDirection={"column"}
+              justifyContent={"flex-end"}
+              alignItems={"center"}
+              alignSelf={"flex-start"}
+              item
+              xs={12}
+              sm={12}
+              md={3}
+              lg={3}
+            >
+              <CardActionsButton />
+            </Grid>
           </Grid>
-          <Grid
-            display={"flex"}
-            flexDirection={"column"}
-            justifyContent={"flex-end"}
-            alignItems={"center"}
-            alignSelf={"flex-start"}
-            item
-            xs={12}
-            sm={12}
-            md={3}
-            lg={3}
-          >
-            <CardActionsButton />
-          </Grid>
-        </Grid>
-        <Divider />{" "}
-        <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={3} lg={3}>
-          <CardRendered
-            title={"Transactions"}
-            props={renderingTransactions()}
-            optional={null}
-          />
-        </Grid>{" "}
-        <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={3} lg={3}>
-          <CardRendered
-            title={"Events"}
-            props={`${eventsAttendedForCustomer}`}
-            optional={null}
-          />
-        </Grid>
-        <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={6} lg={6}>
-          <NotesRendering
-            title={"Notes"}
-            props={substractingNotesAddedForCompany()}
-          />
-        </Grid>
-        <Divider />{" "}
-        <p
-          style={{
-            ...TextFontsize18LineHeight28,
-            width: "100%",
-            textAlign: "left",
-            margin: "0 0 1.5dvh",
-          }}
-        >
-          Transactions
-        </p>
-        <Grid
-          display={"flex"}
-          justifyContent={"space-between"}
-          alignItems={"center"}
-          gap={1}
-          container
-        >
-          <Grid
-            display={"flex"}
-            justifyContent={"flex-start"}
-            alignItems={"center"}
-            alignSelf={"flex-start"}
-            item
-            xs={12}
-            md={8}
-            lg={8}
-          >
-            <OutlinedInput
-              {...register("searchEvent")}
-              style={OutlinedInputStyle}
-              fullWidth
-              placeholder="Search a transaction here"
-              startAdornment={
-                <InputAdornment position="start">
-                  <Icon
-                    icon="radix-icons:magnifying-glass"
-                    color="#344054"
-                    width={20}
-                    height={19}
-                  />
-                </InputAdornment>
-              }
-              endAdornment={
-                <InputAdornment position="end">
-                  <Icon
-                    cursor={"pointer"}
-                    icon="ic:baseline-delete-forever"
-                    color="#1e73be"
-                    width="25"
-                    height="25"
-                    opacity={`${watch("searchEvent")?.length > 0 ? 1 : 0}`}
-                    onClick={() => {
-                      setValue("searchEvent", "");
-                    }}
-                  />
-                </InputAdornment>
-              }
+          <Divider />{" "}
+          <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={3} lg={3}>
+            <CardRendered
+              title={"Transactions"}
+              props={renderingTransactions()}
+              optional={null}
+            />
+          </Grid>{" "}
+          <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={3} lg={3}>
+            <CardRendered
+              title={"Events"}
+              props={`${eventsAttendedForCustomer}`}
+              optional={null}
             />
           </Grid>
+          <Grid alignSelf={"flex-start"} item xs={12} sm={12} md={6} lg={6}>
+            <NotesRendering
+              title={"Notes"}
+              props={substractingNotesAddedForCompany()}
+            />
+          </Grid>
+          <Divider />{" "}
+          <p
+            style={{
+              ...TextFontsize18LineHeight28,
+              width: "100%",
+              textAlign: "left",
+              margin: "0 0 1.5dvh",
+            }}
+          >
+            Transactions
+          </p>
           <Grid
             display={"flex"}
-            justifyContent={"flex-end"}
+            justifyContent={"space-between"}
             alignItems={"center"}
-            alignSelf={"flex-start"}
-            item
-            xs={12}
-            sm={12}
-            md
-            lg
+            gap={1}
+            container
           >
-            <AssigmentAction
-              refetching={refetchingAfterReturnDeviceAssignedInTransaction}
-            />
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-start"}
+              alignItems={"center"}
+              alignSelf={"flex-start"}
+              item
+              xs={12}
+              md={8}
+              lg={8}
+            >
+              <OutlinedInput
+                {...register("searchEvent")}
+                style={OutlinedInputStyle}
+                fullWidth
+                placeholder="Search a transaction here"
+                startAdornment={
+                  <InputAdornment position="start">
+                    <Icon
+                      icon="radix-icons:magnifying-glass"
+                      color="#344054"
+                      width={20}
+                      height={19}
+                    />
+                  </InputAdornment>
+                }
+                endAdornment={
+                  <InputAdornment position="end">
+                    <Icon
+                      cursor={"pointer"}
+                      icon="ic:baseline-delete-forever"
+                      color="#1e73be"
+                      width="25"
+                      height="25"
+                      opacity={`${watch("searchEvent")?.length > 0 ? 1 : 0}`}
+                      onClick={() => {
+                        setValue("searchEvent", "");
+                      }}
+                    />
+                  </InputAdornment>
+                }
+              />
+            </Grid>
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-end"}
+              alignItems={"center"}
+              alignSelf={"flex-start"}
+              item
+              xs={12}
+              sm={12}
+              md
+              lg
+            >
+              <AssigmentAction
+                refetching={refetchingAfterReturnDeviceAssignedInTransaction}
+              />
+            </Grid>
+          </Grid>
+          <Grid
+            marginY={3}
+            display={"flex"}
+            justifyContent={"flex-start"}
+            alignItems={"center"}
+            gap={1}
+            container
+          >
+            <Grid item xs={12} sm={12} md={12} lg={12}>
+              <StripeTransactionPerConsumer
+                data={transactionsConsumerQuery?.data?.data?.list}
+                searchValue={watch("searchEvent")}
+              />
+            </Grid>
           </Grid>
         </Grid>
-        <Grid
-          marginY={3}
-          display={"flex"}
-          justifyContent={"flex-start"}
-          alignItems={"center"}
-          gap={1}
-          container
-        >
-          <Grid item xs={12} sm={12} md={12} lg={12}>
-            <StripeTransactionPerConsumer
-              data={transactionsConsumerQuery?.data?.data?.list}
-              searchValue={watch("searchEvent")}
+        {
+          createUserButton && (
+            <CreateNewConsumer
+              createUserButton={createUserButton}
+              setCreateUserButton={setCreateUserButton}
             />
-          </Grid>
-        </Grid>
-      </Grid>
+          )
+        }
+      </>
     );
   }
 };
