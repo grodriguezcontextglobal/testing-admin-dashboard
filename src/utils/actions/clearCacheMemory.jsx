6ae6f93a5e30dba55f2a2430024c5ac8b87f2528
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-06-13 11:15:00 -0400

    fix(clearCacheMemory): wrap key in object for API request
    The API endpoint expects the key to be passed as an object property rather than directly. This change ensures proper data format is sent to the server.

--- src/utils/actions/clearCacheMemory.jsx
+++ src/utils/actions/clearCacheMemory.jsx
@@ -1,7 +1,7 @@
 import { devitrakApi } from "../../api/devitrakApi";
 
 const clearCacheMemory = async (key) => {
-  return await devitrakApi.post("/cache_update/remove-cache", key);
+  return await devitrakApi.post("/cache_update/remove-cache", {key: key});
 };
 
 export default clearCacheMemory;
