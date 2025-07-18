c1395f772d508021ffc0205a69732a1c78efc70c
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-07-18 12:11:55 -0400

    perf(NewBulkItems): optimize subLocationsOptions with useMemo
    Prevent unnecessary recalculations of subLocationsOptions by memoizing the result and only recomputing when the watched location changes

--- src/pages/inventory/actions/NewBulkItems.jsx
+++ src/pages/inventory/actions/NewBulkItems.jsx
@@ -2,7 +2,7 @@ import { Grid } from "@mui/material";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Button, message, notification } from "antd";
 import { groupBy, orderBy } from "lodash";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
@@ -242,10 +242,9 @@ const AddNewBulkItems = () => {
     scannedSerialNumbers.length,
   ]);
   qtyDiff();
-
-  const subLocationsOptions = retrieveExistingSubLocationsForCompanyInventory(
-    itemsInInventoryQuery?.data?.data?.items
-  );
+  const subLocationsOptions = useMemo(() => retrieveExistingSubLocationsForCompanyInventory(
+    itemsInInventoryQuery?.data?.data?.items, watch("location")
+  ), [watch("location")]);
   const renderingOptionsForSubLocations = (item) => {
     const addSublocationButton = () => {
       return (
