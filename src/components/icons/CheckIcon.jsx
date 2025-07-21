cf2822de7e4d53b939f8f44ff3dcd12e1e9d43fd
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-07-21 09:40:46 -0400

    feat(icons): make stroke and fill colors customizable
    Add stroke prop to CheckIcon and fill prop to BorderedCloseIcon to allow customizing icon colors

--- src/components/icons/CheckIcon.jsx
+++ src/components/icons/CheckIcon.jsx
@@ -1,8 +1,8 @@
-export const CheckIcon = () => {
+export const CheckIcon = ({stroke = "#000"}) => {
     return (
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none">
         <path
-          stroke="#000"
+          stroke={stroke}
           d="m5 8 2 2 4-4m3.667 2A6.667 6.667 0 1 1 1.334 8a6.667 6.667 0 0 1 13.333 0Z"
         />
       </svg>
