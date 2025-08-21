a437d9cab118c269dd1ce87af6950b3cf0fdaa8a
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-08-21 13:48:11 -0400

    feat(inventory): add responsive layout to CardForTreeView component
    Implement responsive padding based on device breakpoints using useMediaQuery
    Replace commented Grid layout with Space component from antd

--- src/pages/inventory/utils/CardForTreeView.jsx
+++ src/pages/inventory/utils/CardForTreeView.jsx
@@ -1,16 +1,31 @@
 import { useId } from "react";
-// import RenderingMoreThanTreeviewElements from "./RenderingMoreThanTreeviewElements";
 import TreeView from "./TreeView";
-// import { Grid } from "@mui/material";
+import { Space } from "antd";
+import { useMediaQuery } from "@uidotdev/usehooks";
 
 const CardForTreeView = (props) => {
   const elemId = useId();
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+  const isMediumDevice = useMediaQuery(
+    "only screen and (min-width : 769px) and (max-width : 992px)"
+  );
+  const isLargeDevice = useMediaQuery(
+    "only screen and (min-width : 993px) and (max-width : 1200px)"
+  );
+  const isExtraLargeDevice = useMediaQuery(
+    "only screen and (min-width : 1201px)"
+  );
+  const renderingBreakPoints = () => {
+    if (isSmallDevice) return "0 0 0 1rem";
+    if (isMediumDevice) return "0 0 0 1rem";
+    if (isLargeDevice) return "0 0 0 2rem";
+    if (isExtraLargeDevice) return "0 0 0 2rem";
+    return "0 0 0 2rem";
+  }
   return (
-    // <Grid marginX={"auto"} display={"flex"} flexDirection={"column"} gap={1}>
-    //   <Grid width={"100%"} item xs={12} sm={12} md={12} lg={12}>
-    <TreeView id={`${elemId}`} key={elemId} data={props.data} />
-    //   </Grid>
-    // </Grid>
+    <Space align="start" size={[8, 16]} wrap style={{ maxWidth: "1400px", minWidth:"320px", width:"100%", padding: renderingBreakPoints() }}>
+      <TreeView id={`${elemId}`} key={elemId} data={props.data} />
+    </Space>
   );
 };
 
