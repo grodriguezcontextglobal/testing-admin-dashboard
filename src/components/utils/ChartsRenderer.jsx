626e4a688d50bc21363696fa056f491f4f8b80ad
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-08-27 18:18:42 -0400

    feat(ChartsRenderer): add click event handling and prop validation
    - Add onClick prop for custom click handling
    - Implement click event handler that checks location path
    - Add PropTypes validation for onClick prop

--- src/components/utils/ChartsRenderer.jsx
+++ src/components/utils/ChartsRenderer.jsx
@@ -1,6 +1,7 @@
-import { PropTypes } from "prop-types";
 import ReactECharts from "echarts-for-react";
+import { PropTypes } from "prop-types";
 import { Subtitle } from "../../styles/global/Subtitle";
+import { useLocation } from "react-router-dom";
 
 const ChartsRenderer = ({
   dataToRender,
@@ -12,7 +13,9 @@ const ChartsRenderer = ({
   legendAlign,
   colors,
   radiusProps,
+  onClick, // Add an onClick prop for custom click handling
 }) => {
+  const location = useLocation();
   const radiusP = radiusProps ?? ["40%", "60%"];
   const option = {
     color: colors, // Add your custom colors here
@@ -51,12 +54,32 @@ const ChartsRenderer = ({
     ],
   };
 
-  return <ReactECharts option={option} style={{ width: "100%" }} />;
+  // Handle click event
+  const onChartClick = (params) => {
+    // If you passed an onClick prop, call it with the clicked data
+    if (location.pathname === "/")
+      if (onClick) {
+        onClick(params.name);
+      }
+  };
+
+  return (
+    <>
+      <ReactECharts
+        option={option}
+        style={{ width: "100%" }}
+        onEvents={{
+          click: onChartClick, // Attach click event handler
+        }}
+      />
+    </>
+  );
 };
 
 ChartsRenderer.propTypes = {
   dataToRender: PropTypes.array,
   title: PropTypes.string,
+  onClick: PropTypes.func, // Add a prop type for onClick
 };
 
 export default ChartsRenderer;
