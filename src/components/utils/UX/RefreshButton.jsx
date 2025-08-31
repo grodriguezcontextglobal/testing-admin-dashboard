0949e0c5618316ab9b6593a87385e1507535f512
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-08-31 07:55:25 -0400

    refactor(UX): replace custom button with BlueButtonComponent for consistency
    Simplify RefreshButton implementation by using the shared BlueButtonComponent

--- src/components/utils/UX/RefreshButton.jsx
+++ src/components/utils/UX/RefreshButton.jsx
@@ -1,34 +1,9 @@
 import { Icon } from "@iconify/react/dist/iconify.js";
-import { Button } from "antd";
+import BlueButtonComponent from "../../UX/buttons/BlueButton";
 
 const RefreshButton = ({ propsFn = null }) => {
   return (
-    <Button
-      style={{
-        display: "flex",
-        alignItems: "center",
-        borderTop: "transparent",
-        borderLeft: "transparent",
-        borderBottom: "transparent",
-        borderRadius: "8px 8px 0 0",
-      }}
-      onClick={() => propsFn()}
-    >
-      <p
-        style={{
-          textTransform: "none",
-          textAlign: "left",
-          fontWeight: 500,
-          fontSize: "12px",
-          fontFamily: "Inter",
-          lineHeight: "28px",
-          color: "var(--blue-dark-700, #004EEB)",
-          padding: "0px",
-        }}
-      >
-        <Icon icon="jam:refresh" /> Refresh
-      </p>
-    </Button>
+    <BlueButtonComponent title={"Refresh"} func={() => propsFn()} icon={<Icon icon="jam:refresh" />} />
   );
 };
 
