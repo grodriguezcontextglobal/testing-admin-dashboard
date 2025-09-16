cdb0cb12c21329c811907f6b40fef5b373781878
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-09-16 16:20:18 -0400

    refactor(icons): make WhiteCirclePlusIcon props more configurable
    Add width, height and viewBox props to allow more flexible usage. Remove commented code and clean up formatting.

--- src/components/icons/WhiteCirclePlusIcon.jsx
+++ src/components/icons/WhiteCirclePlusIcon.jsx
@@ -1,19 +1,18 @@
-export const WhiteCirclePlusIcon = ({ stroke = "#fff" }) => {
+export const WhiteCirclePlusIcon = ({
+  stroke = "#fff",
+  viewBox = "0 0 24 24",
+  width = "20",
+  height = "18",
+}) => {
   return (
-    <svg width={24} height={24} viewBox="0 0 24 24" fill="none">
-    <path
+    <svg width={width} height={height} viewBox={viewBox} fill="none">
+      <path
         d="M12 8V16M8 12H16M22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12Z"
         stroke={stroke}
         strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-    />
-</svg>
-    // <svg xmlns="http://www.w3.org/2000/svg" width="21" height="20" fill="none">
-    //   <path
-    //     stroke="#fff"
-    //     d="M10 6.667v6.666M6.667 10h6.666m5 0a8.333 8.333 0 1 1-16.666 0 8.333 8.333 0 0 1 16.666 0Z"
-    //   />
-    // </svg>
+        // strokeLinecap="round"
+        // strokeLinejoin="round"
+      />
+    </svg>
   );
 };
