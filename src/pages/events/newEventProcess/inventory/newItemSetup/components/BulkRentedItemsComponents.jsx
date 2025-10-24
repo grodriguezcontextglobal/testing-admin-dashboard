164e3b1a3edaa1da08c5d0bd0c3a8ff9662ccb7c
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-10-24 06:54:29 -0400

    refactor(newItemSetup): remove unused renderTitle function from BulkRentedItemsComponents

--- src/pages/events/newEventProcess/inventory/newItemSetup/components/BulkRentedItemsComponents.jsx
+++ src/pages/events/newEventProcess/inventory/newItemSetup/components/BulkRentedItemsComponents.jsx
@@ -11,33 +11,33 @@ import { TextFontSize30LineHeight38 } from "../../../../../../styles/global/Text
 import ScanningMethod from "../../../../../inventory/actions/utils/ScanningMethod";
 import ScanningModal from "../../../../../inventory/actions/utils/ScanningModal";
 
-export const renderTitle = () => {
-  return (
-    <>
-      <InputLabel id="eventName" style={{ marginBottom: "6px", width: "100%" }}>
-        <Typography
-          textAlign={"left"}
-          style={TextFontSize30LineHeight38}
-          color={"var(--gray-600, #475467)"}
-        >
-          Add a group of items
-        </Typography>
-      </InputLabel>
-      <InputLabel id="eventName" style={{ marginBottom: "6px", width: "100%" }}>
-        <Typography
-          textAlign={"left"}
-          textTransform={"none"}
-          style={{ ...TextFontSize20LineHeight30, textWrap: "balance" }}
-          color={"var(--gray-600, #475467)"}
-        >
-          Devices serial numbers can be created by inputting a serial number
-          base to define the category of items, and then a range from one number
-          to another, depending on your inventory.
-        </Typography>
-      </InputLabel>
-    </>
-  );
-};
+// export const renderTitle = () => {
+//   return (
+//     <>
+//       <InputLabel id="eventName" style={{ marginBottom: "6px", width: "100%" }}>
+//         <Typography
+//           textAlign={"left"}
+//           style={TextFontSize30LineHeight38}
+//           color={"var(--gray-600, #475467)"}
+//         >
+//           Add a group of items
+//         </Typography>
+//       </InputLabel>
+//       <InputLabel id="eventName" style={{ marginBottom: "6px", width: "100%" }}>
+//         <Typography
+//           textAlign={"left"}
+//           textTransform={"none"}
+//           style={{ ...TextFontSize20LineHeight30, textWrap: "balance" }}
+//           color={"var(--gray-600, #475467)"}
+//         >
+//           Devices serial numbers can be created by inputting a serial number
+//           base to define the category of items, and then a range from one number
+//           to another, depending on your inventory.
+//         </Typography>
+//       </InputLabel>
+//     </>
+//   );
+// };
 
 export const renderingOptionsButtons = ({
   watch,
