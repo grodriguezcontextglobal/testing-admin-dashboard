0384f49ea7b3610ca244b62d1f67d378ea32b7f8
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-10-27 14:15:05 -0400

    feat(inventory): add main component for editing inventory UX options
    Implement the main component that provides three different methods for updating event inventory:
    1. Auto selection from available devices
    2. Starting from a provided serial number
    3. Adding serial numbers one-by-one

---
+++ src/pages/events/quickGlance/inventory/action/components/EditingInventoryUXOptions/Main.jsx
@@ -0,0 +1,120 @@
+import { Divider, Radio, Space, Tooltip, Typography } from "antd";
+import { useState } from "react";
+import { QuestionIcon } from "../../../../../../../components/icons/QuestionIcon";
+import { AppSelectingFromExistingAutomatically } from "./AppSelectingFromExistingAutoimatically";
+import { UpdateEventInventoryByAddingSerialNumberOneByOne } from "./UpdateEventInventoryByAddingSerialNumberOneByOne";
+import { UpdateEventInventorySubmittingStartingSerialNumber } from "./UpdateEventInventorySubmittingStartingSerialNumber";
+
+const Main = ({
+  assignAllDevices,
+  closeModal,
+  handleSubmit,
+  loadingStatus,
+  openNotification,
+  OutlinedInputStyle,
+  queryClient,
+  RefreshButton,
+  register,
+  setAssignAllDevices,
+  setLoadingStatus,
+  Subtitle,
+  valueItemSelected,
+  watch,
+}) => {
+  const [mode, setMode] = useState("OPTION_1"); // OPTION_1, OPTION_2, OPTION_3
+  return (
+    <Space
+      direction="vertical"
+      size="large"
+      style={{ width: "100%", margin: "0.5rem auto" }}
+    >
+      <Typography.Title level={5} style={{ margin: "1rem 0 0" }}>
+        Update Event Inventory
+      </Typography.Title>
+
+      <Radio.Group
+        value={mode}
+        onChange={(e) => setMode(e.target.value)}
+        optionType="button"
+        block
+        style={{ width: "100%", alignItems: "center" }}
+      >
+        <Radio.Button value="OPTION_1" style={{ alignItems: "center" }}>
+          <Tooltip title="System will add automatically available devices to event inventory.">
+            Auto&nbsp;
+            <QuestionIcon />
+          </Tooltip>
+        </Radio.Button>
+        <Radio.Button value="OPTION_2" style={{ alignItems: "center" }}>
+          <Tooltip title="System will add devices to event inventory starting from the provided serial number.">
+            Starting Serial&nbsp;
+            <QuestionIcon />
+          </Tooltip>
+        </Radio.Button>
+
+        <Radio.Button value="OPTION_3" style={{ alignItems: "center" }}>
+          <Tooltip title="System will add devices to event inventory one-by-one.">
+            Serials One-by-One&nbsp;
+            <QuestionIcon />
+          </Tooltip>
+        </Radio.Button>
+      </Radio.Group>
+
+      <Divider style={{ margin: "8px 0" }} />
+      <Space direction="vertical" size="middle" style={{ width: "100%" }}>
+        {mode === "OPTION_1" && (
+          <AppSelectingFromExistingAutomatically
+            handleSubmit={handleSubmit}
+            assignAllDevices={assignAllDevices}
+            closeModal={closeModal}
+            setAssignAllDevices={setAssignAllDevices}
+            loadingStatus={loadingStatus}
+            register={register}
+            OutlinedInputStyle={OutlinedInputStyle}
+            Subtitle={Subtitle}
+            valueItemSelected={valueItemSelected}
+            setLoadingStatus={setLoadingStatus}
+            watch={watch}
+            RefreshButton={RefreshButton}
+            queryClient={queryClient}
+            openNotification={openNotification}
+          />
+        )}
+
+        {mode === "OPTION_2" && (
+          <UpdateEventInventorySubmittingStartingSerialNumber
+            assignAllDevices={assignAllDevices}
+            closeModal={closeModal}
+            handleSubmit={handleSubmit}
+            loadingStatus={loadingStatus}
+            openNotification={openNotification}
+            OutlinedInputStyle={OutlinedInputStyle}
+            queryClient={queryClient}
+            register={register}
+            setAssignAllDevices={setAssignAllDevices}
+            setLoadingStatus={setLoadingStatus}
+            Subtitle={Subtitle}
+            watch={watch}
+          />
+        )}
+
+        {mode === "OPTION_3" && (
+          <UpdateEventInventoryByAddingSerialNumberOneByOne
+            closeModal={closeModal}
+            handleSubmit={handleSubmit}
+            loadingStatus={loadingStatus}
+            openNotification={openNotification}
+            OutlinedInputStyle={OutlinedInputStyle}
+            queryClient={queryClient}
+            register={register}
+            setLoadingStatus={setLoadingStatus}
+            Subtitle={Subtitle}
+            watch={watch}
+          />
+        )}
+      </Space>
+    </Space>
+  );
+};
+
+export default Main;
