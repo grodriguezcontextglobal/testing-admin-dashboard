0c9ce8e91ef193da72c18c70464d1c77e065b4e3
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-10-28 09:17:56 -0400

    feat(event-details): add phone number contact component for event creation
    Implement a new component to handle phone number input and management during event creation. Includes phone number validation, adding/removing functionality, and responsive design for both desktop and mobile views.

---
+++ src/pages/events/newEventProcess/eventDetails/ux/phoneNumberContact.jsx
@@ -0,0 +1,123 @@
+import { InputLabel, Typography } from "@mui/material";
+import { InputLabelStyle } from "../../style/InputLabelStyle";
+import { Space, Tag, Tooltip } from "antd";
+import PhoneInput from "react-phone-number-input";
+import CenteringGrid from "../../../../../styles/global/CenteringGrid";
+import BlueButtonComponent from "../../../../../components/UX/buttons/BlueButton";
+import { WhiteCirclePlusIcon } from "../../../../../components/icons/WhiteCirclePlusIcon";
+
+const PhoneNumberContact = ({
+    contactPhoneNumber,
+    setContactPhoneNumber,
+    isMobile,
+    addingPhoneNumber,
+    removePhoneNumber,
+    numberOfPhoneNumbersPerEvent,
+}) => {
+  return (
+    <>
+      <InputLabel style={{ marginBottom: "0.2rem", width: "100%" }}>
+        <Typography style={InputLabelStyle}>
+          Event contact Phone number
+        </Typography>
+      </InputLabel>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+          alignSelf: "stretch",
+          textAlign: "left",
+          gap: "10px",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            textAlign: "left",
+            width: "100%",
+            alignSelf: "flex-start",
+          }}
+        >
+          <Tooltip
+            title="Please click the 'Add phone number' button to include your phone number. Otherwise, it will not be added."
+            style={{ width: "100%" }}
+          >
+            <PhoneInput
+              className="container-phone input-phone"
+              id="phone_input_check"
+              style={{
+                boxShadow: "rgba(16, 24, 40, 0.05) 1px 1px 2px",
+                width: "100%",
+              }}
+              countrySelectProps={{ unicodeFlags: true }}
+              defaultCountry="US"
+              placeholder="(555) 000-0000"
+              value={contactPhoneNumber}
+              onChange={setContactPhoneNumber}
+            />
+          </Tooltip>
+        </div>
+        <div
+          style={{
+            ...CenteringGrid,
+            justifyContent: "flex-end",
+            height: "2.5rem",
+            textAlign: "left",
+            width: "50%",
+          }}
+        >
+          <BlueButtonComponent
+            title={isMobile ? "Add" : "Add phone number"}
+            buttonType="button"
+            func={() => addingPhoneNumber()}
+            disabled={contactPhoneNumber === ""}
+            styles={{ width: "fit-content" }}
+            icon={<WhiteCirclePlusIcon />}
+          />
+        </div>
+      </div>
+      <Space
+        style={{
+          width: "100%",
+          display: "flex",
+          justifyContent: "flex-start",
+          alignItems: "center",
+        }}
+        size={[0, "small"]}
+        wrap
+      >
+        {numberOfPhoneNumbersPerEvent?.map((item) => {
+          return (
+            <Tag
+              bordered={false}
+              closable
+              onClose={() => removePhoneNumber(item)}
+              key={`${item}`}
+              style={{
+                display: "flex",
+                padding: "2px 4px",
+                justifyContent: "space-between",
+                alignItems: "center",
+                gap: "3px",
+                borderRadius: "8px",
+                border: "1px solid var(--gray-300, #D0D5DD)",
+                background: "var(--base-white, #FFF)",
+                margin: "5px",
+              }}
+            >
+              &nbsp;
+              <Typography style={InputLabelStyle}>{item}</Typography>
+              &nbsp;
+            </Tag>
+          );
+        })}
+      </Space>
+    </>
+  );
+};
+
+export default PhoneNumberContact;
