8b3c5a5d0daa605b93cae586fde753c1089603d8
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-11-21 16:40:12 -0500

    refactor(consumers): simplify device assignment modal and improve query handling
    - Replace custom switch implementation with reusable CustomizedSwitch component
    - Remove unused imports and commented code
    - Optimize SQL consumer query with enabled flag and staleTime
    - Remove unnecessary useEffect for query refetching
    - Simplify inventory toggle UI and remove unused assignment option

--- src/pages/consumers/action/ModalAssignDeviceToConsumer.jsx
+++ src/pages/consumers/action/ModalAssignDeviceToConsumer.jsx
@@ -1,15 +1,14 @@
 import { Grid } from "@mui/material";
 import { useQuery } from "@tanstack/react-query";
-import { Divider, Switch } from "antd";
+import { Divider } from "antd";
 import { PropTypes } from "prop-types";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useSelector } from "react-redux";
 import { devitrakApi } from "../../../api/devitrakApi";
 import ModalUX from "../../../components/UX/modal/ModalUX";
 import "../../../styles/global/ant-select.css";
-import CenteringGrid from "../../../styles/global/CenteringGrid";
-import { TextFontSize20LineHeight30 } from "../../../styles/global/TextFontSize20HeightLine30";
-import AssignemntNewDeviceInInventory from "./assingmentComponents/AssignemntNewDeviceInInventory";
+import CustomizedSwitch from "../../staff/detail/components/equipment_components/assingmentComponents/components/CustomizedSwitch";
+// import AssignemntNewDeviceInInventory from "./assingmentComponents/AssignemntNewDeviceInInventory";
 import AssignmentFromExistingInventory from "./assingmentComponents/AssignmentFromExistingInventory";
 const ModalAssignDeviceToConsumer = ({ assignDevice, setAssignDevice }) => {
   const [existingOption, setExistingOption] = useState(true);
@@ -20,16 +19,9 @@ const ModalAssignDeviceToConsumer = ({ assignDevice, setAssignDevice }) => {
       devitrakApi.post("/db_consumer/consulting-consumer", {
         email: customer.email,
       }),
-    refetchOnMount: false,
+    enabled: !!customer.email,
+    staleTime: 5 * 60 * 100,
   });
-  useEffect(() => {
-    const controller = new AbortController();
-    checkConsumerInSqlDb.refetch();
-    return () => {
-      controller.abort();
-    };
-  }, []);
-
   const closeModal = () => {
     return setAssignDevice(false);
   };
@@ -44,44 +36,22 @@ const ModalAssignDeviceToConsumer = ({ assignDevice, setAssignDevice }) => {
         marginY={2}
         key={"settingUp-deviceList-event"}
       >
-        {" "}
         <Divider>
-          <div
-            style={{
-              width: "100%",
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "space-between",
-              alignItems: "center",
-              gap: "10px",
-            }}
-          >
-            <h2 style={{ ...CenteringGrid, ...TextFontSize20LineHeight30 }}>
-              New inventory
-            </h2>
-            &nbsp;
-            <Switch
-              value={existingOption}
-              onChange={() => setExistingOption(!existingOption)}
-              defaultChecked
-            />
-            &nbsp;
-            <h2 style={{ ...CenteringGrid, ...TextFontSize20LineHeight30 }}>
-              Existing inventory
-            </h2>
-          </div>
+          <CustomizedSwitch
+            state={existingOption}
+            handler={setExistingOption}
+          />
         </Divider>
-        {existingOption ? (
+        {/* {existingOption ? ( */}
           <AssignmentFromExistingInventory
-            consumerInfoSqlDb={checkConsumerInSqlDb?.data?.data?.consumer}
             closeModal={closeModal}
+            consumerInfoSqlDb={
+              checkConsumerInSqlDb?.data?.data?.consumer?.at(-1) ?? {}
+            }
           />
-        ) : (
-          <AssignemntNewDeviceInInventory
-            consumerInfoSqlDb={checkConsumerInSqlDb?.data?.data?.consumer}
-            closeModal={closeModal}
-          />
-        )}
+        {/* ) : (
+          <AssignemntNewDeviceInInventory />
+        )} */}
       </Grid>
     );
   };
@@ -92,13 +62,6 @@ const ModalAssignDeviceToConsumer = ({ assignDevice, setAssignDevice }) => {
       closeModal={closeModal}
       body={bodyModal()}
     />
-    // <Modal
-    //   open={assignDevice}
-    //   onCancel={() => closeModal()}
-    //   width={1000}
-    //   footer={[]}
-    //   style={{ zIndex: 30 }}
-    // ></Modal>
   );
 };
 
