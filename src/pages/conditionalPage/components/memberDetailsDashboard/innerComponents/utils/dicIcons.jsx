5b33d954bbbbd6dbfe1e999ccf0ec7293363952b
Gustavo Santeliz <grodriguez@contextglobal.com>
2025-12-09 20:55:41 -0500

    feat(memberDetailsDashboard): add dictionary for status icons
    Create a utility file to map status types to their corresponding icons for consistent rendering throughout the dashboard

---
+++ src/pages/conditionalPage/components/memberDetailsDashboard/innerComponents/utils/dicIcons.jsx
@@ -0,0 +1,9 @@
+import { CheckIcon } from "../../../../../../components/icons/CheckIcon";
+import { ErrorIcon } from "../../../../../../components/icons/ErrorIcon";
+import { WarningIcon } from "../../../../../../components/icons/WarningIcon";
+
+export const dicIcons = {
+  success: <CheckIcon stroke="green" />,
+  error: <ErrorIcon />,
+  warning: <WarningIcon color="orange" />,
+};
