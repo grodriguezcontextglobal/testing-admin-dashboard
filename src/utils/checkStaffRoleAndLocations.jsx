3b46b9aba1986cef4b2af2001f32f471c6b232b4
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-14 14:05:34 -0500

    feat(utils): add staff role and locations permission hook
    Create new utility hook to centralize staff role and location permissions logic

---
+++ src/utils/checkStaffRoleAndLocations.jsx
@@ -0,0 +1,61 @@
+import { useSelector } from "react-redux";
+
+export const useStaffRoleAndLocations = () => {
+  const { user } = useSelector((state) => state.admin);
+  const role =
+    user.companyData.employees.find((emp) => emp.user === user.email)?.role ||
+    "2";
+  const locationsViewPermission =
+    user.companyData.employees
+      .find((emp) => emp.user === user.email)
+      ?.preference?.managerLocation?.filter((loc) => !loc?.actions?.view)
+      ?.map((loc) => loc.location) || [];
+
+  const locationsAssignPermission =
+    user.companyData.employees
+      .find((emp) => emp.user === user.email)
+      ?.preference?.managerLocation?.filter((loc) => !loc?.actions?.assign)
+      ?.map((loc) => loc.location) || [];
+
+  const locationsManagePermission =
+    user.companyData.employees
+      .find((emp) => emp.user === user.email)
+      ?.preference?.managerLocation?.filter((loc) => !loc?.actions?.create)
+      ?.map((loc) => loc.location) || [];
+
+  const locationsDeletePermission =
+    user.companyData.employees
+      .find((emp) => emp.user === user.email)
+      ?.preference?.managerLocation?.filter((loc) => !loc?.actions?.delete)
+      ?.map((loc) => loc.location) || [];
+
+  const locationsUpdatePermission =
+    user.companyData.employees
+      .find((emp) => emp.user === user.email)
+      ?.preference?.managerLocation?.filter((loc) => !loc?.actions?.update)
+      ?.map((loc) => loc.location) || [];
+
+  const transferPermission =
+    user.companyData.employees
+      .find((emp) => emp.user === user.email)
+      ?.preference?.managerLocation?.filter((loc) => !loc?.actions?.transfer)
+      ?.map((loc) => loc.location) || [];
+  console.log({
+    role,
+    locationsViewPermission,
+    locationsAssignPermission,
+    locationsManagePermission,
+    locationsDeletePermission,
+    locationsUpdatePermission,
+    transferPermission,
+  });
+  return {
+    role,
+    locationsViewPermission,
+    locationsAssignPermission,
+    locationsManagePermission,
+    locationsDeletePermission,
+    locationsUpdatePermission,
+    transferPermission,
+  };
+};
