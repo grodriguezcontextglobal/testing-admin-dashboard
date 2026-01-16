201fa5cb0dabf74547772156fd4c19ee7cc2153d
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-16 12:36:47 -0500

    fix(inventory): add optional chaining to item data
    Use optional chaining when accessing industryData to prevent potential runtime errors if the itemsInInventoryQuery data structure is undefined or null.

--- src/pages/events/newEventProcess/inventory/newItemSetup/FormDeviceTrackingMethod.jsx
+++ src/pages/events/newEventProcess/inventory/newItemSetup/FormDeviceTrackingMethod.jsx
@@ -194,7 +194,7 @@ const FormDeviceTrackingMethod = ({
   const retrieveItemDataSelected = () => {
     const result = new Map();
     if (itemsInInventoryQuery.data) {
-      const industryData = itemsInInventoryQuery.data.data.items;
+      const industryData = itemsInInventoryQuery?.data?.data?.items;
       for (let data of industryData) {
         result.set(data.item_group, data);
       }
