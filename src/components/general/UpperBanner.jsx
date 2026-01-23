f09787aaad8af946a8d4e19493ad7292f6144d3b
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-23 12:55:01 -0500

    refactor(UpperBanner): replace MUI with Ant Design
    Replace the Material UI Grid and Typography implementation with the Ant Design Alert component. This simplifies the component structure and updates the styling to use the Inter font family while maintaining the dark blue background.

--- src/components/general/UpperBanner.jsx
+++ src/components/general/UpperBanner.jsx
@@ -1,32 +1,21 @@
-import { Grid, Typography } from "@mui/material";
+import { Alert } from "antd";
 import { forwardRef } from "react";
 import { useSelector } from "react-redux";
-import CenteringGrid from "../../styles/global/CenteringGrid";
-import { Subtitle } from "../../styles/global/Subtitle";
 
 const UpperBanner = forwardRef(function UpperBanner() {
   const { user } = useSelector((state) => state.admin);
   return (
-    <Grid
-      id="upper-nav-component"
+    <Alert
+      message={user.companyData.company_name}
+      banner
+      showIcon={false}
       style={{
-        ...CenteringGrid,
         backgroundColor: "var(--blue-dark--800)",
+        color:"#fff",
+        fontFamily:"Inter, serif-sans",
+        fontWeight:400
       }}
-      container
-    >
-      <Typography
-        style={{
-          ...Subtitle,
-          color: "var(--gray300)",
-          textAlign: "center",
-          textTransform: "none",
-        }}
-        padding={"0.8rem auto"}
-      >
-        {user.company}
-      </Typography>
-    </Grid>
+    />
   );
 });
 
