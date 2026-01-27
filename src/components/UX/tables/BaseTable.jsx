e554b670a70c13a66c92223a0f5de58fc1c8692b
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-27 14:03:43 -0500

    feat(BaseTable): add pagination and prop types
    Introduce pagination support with configurable page sizes and center alignment. Include PropTypes for better type safety and apply a custom CSS class for enhanced styling.

--- src/components/UX/tables/BaseTable.jsx
+++ src/components/UX/tables/BaseTable.jsx
@@ -1,22 +1,51 @@
 import { Table } from "antd";
 import { styled } from "@mui/material";
+import PropTypes from "prop-types";
+import "../../../styles/global/ant-table.css";
 
 const StyledTable = styled(Table)({
   width: "100%",
-  borderRadius: "12px",
-  border: "1px solid var(--gray-200)",
-  background: "var(--base-white)",
-  boxShadow: "0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10)"
+  boxShadow:
+    "0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10)",
 });
 
-const BaseTable = ({ columns, dataSource, ...props }) => {
+const BaseTable = ({
+  columns,
+  dataSource,
+  enablePagination,
+  pageSize,
+  ...props
+}) => {
+  const paginationConfig = enablePagination
+    ? {
+        pageSize: pageSize,
+        position: ["bottomCenter"],
+        showSizeChanger: false,
+        showQuickJumper: false,
+      }
+    : false;
+
   return (
-    <StyledTable 
+    <StyledTable
       columns={columns}
       dataSource={dataSource}
+      pagination={paginationConfig}
       {...props}
+      className="table-ant-customized"
     />
   );
+};
+
+BaseTable.propTypes = {
+  columns: PropTypes.array.isRequired,
+  dataSource: PropTypes.array.isRequired,
+  enablePagination: PropTypes.bool,
+  pageSize: PropTypes.number,
+};
+
+BaseTable.defaultProps = {
+  enablePagination: false,
+  pageSize: 10,
 };
 
 export default BaseTable;
