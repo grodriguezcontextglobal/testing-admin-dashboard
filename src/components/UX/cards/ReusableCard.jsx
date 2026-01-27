5f48d619c2495d887b333021e4a3da2236e114f6
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-26 23:21:22 -0500

    feat(cards): add ReusableCard component
    Introduce a new reusable card component using Ant Design and Material UI. This component supports responsive typography and custom click handlers to standardize card layouts across the application.

---
+++ src/components/UX/cards/ReusableCard.jsx
@@ -0,0 +1,70 @@
+import { Grid, Typography } from "@mui/material";
+import { Card } from "antd";
+import { useMediaQuery } from "@uidotdev/usehooks";
+import { Subtitle } from "../../../styles/global/Subtitle";
+import { TextFontSize30LineHeight38 } from "../../../styles/global/TextFontSize30LineHeight38";
+
+const ReusableCard = ({ title, props, onClick=null }) => {
+  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+  const isMediumDevice = useMediaQuery(
+    "only screen and (min-width : 769px) and (max-width : 992px)"
+  );
+  return (
+      <Grid
+        padding={'0 0 10px'}
+        item
+        xs={12}
+      >
+        <Card
+          style={{
+            borderRadius: "12px",
+            border: "1px solid var(--gray-200, #EAECF0)",
+            background: "var(--base-white, #FFF)",
+            boxShadow:
+              "0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10)",
+          }}
+          onClick={onClick}
+        >
+          <Grid
+            display={"flex"}
+            justifyContent={"space-around"}
+            alignItems={"center"}
+            container
+          >
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-start"}
+              alignItems={"center"}
+              item
+              xs={12}
+            >
+              <Typography
+                textAlign={`${(isSmallDevice || isMediumDevice) && "left"}`}
+                style={{ ...Subtitle, textWrap: 'nowrap' }}
+              >
+                {title}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid
+              display={"flex"}
+              justifyContent={"flex-start"}
+              alignItems={"center"}
+              item
+              xs={12}
+            >
+              <Typography
+                paddingTop={"8px"}
+                style={{ ...TextFontSize30LineHeight38}}
+              >
+                {props}
+              </Typography>
+            </Grid>
+          </Grid>
+        </Card>
+      </Grid>
+      )
+};
+
+export default ReusableCard;
