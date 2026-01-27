ae14dd2d6e3dbc89c29859e5a7a2a3791dbf4c68
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-27 14:03:22 -0500

    feat(ux): add TableHeader component
    Introduce a reusable TableHeader component using Material UI Grid and Typography. This component supports a centered title and optional left/right call-to-action slots for consistent table layouts.

---
+++ src/components/UX/TableHeader.jsx
@@ -0,0 +1,30 @@
+import { Grid, Typography } from "@mui/material";
+
+const TableHeader = ({ title, leftCta, rightCta }) => {
+  return (
+    <Grid
+      border={"1px solid var(--gray-200, #eaecf0)"}
+      borderRadius={"12px 12px 0 0"}
+      display={"flex"}
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      marginBottom={-1}
+      paddingBottom={-1}
+      // padding={"1rem"}
+      item
+      xs={12}
+    >
+      <Grid item xs={4} display={"flex"} justifyContent={"flex-start"}>
+        {leftCta}
+      </Grid>
+      <Grid item xs={4} display={"flex"} justifyContent={"center"}>
+        <Typography variant="h6">{title && title}</Typography>
+      </Grid>
+      <Grid item xs={4} display={"flex"} justifyContent={"flex-end"}>
+        {rightCta}
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TableHeader;
