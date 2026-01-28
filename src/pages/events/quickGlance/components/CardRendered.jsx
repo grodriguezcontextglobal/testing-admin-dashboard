de7bfc9d7e4e03f51bc3b2174b7159962302b6f1
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-01-28 08:40:13 -0500

    refactor(quickGlance): replace custom card with ReusableCard
    Replace the inline Grid and Card implementation in CardRendered with
    the ReusableCard component to improve maintainability and reduce
    code duplication.

--- src/pages/events/quickGlance/components/CardRendered.jsx
+++ src/pages/events/quickGlance/components/CardRendered.jsx
@@ -1,69 +1,18 @@
-import { Grid, Typography } from "@mui/material";
-import { useMediaQuery } from "@uidotdev/usehooks";
-import { Card } from "antd";
+import ReusableCard from "../../../../components/UX/cards/ReusableCard";
 import { Subtitle } from "../../../../styles/global/Subtitle";
 import { TextFontSize30LineHeight38 } from "../../../../styles/global/TextFontSize30LineHeight38";
 
 const CardRendered = ({ props, title }) => {
-  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
-  const isMediumDevice = useMediaQuery(
-    "only screen and (min-width : 769px) and (max-width : 992px)"
-  );
   return (
-      <Grid
-        padding={'0 0 10px'}
-        item
-        xs={12}
-      >
-        <Card
-          style={{
-            borderRadius: "12px",
-            border: "1px solid var(--gray-200, #EAECF0)",
-            background: "var(--base-white, #FFF)",
-            boxShadow:
-              "0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10)",
-          }}
-        >
-          <Grid
-            display={"flex"}
-            justifyContent={"space-around"}
-            alignItems={"center"}
-            container
-          >
-            <Grid
-              display={"flex"}
-              justifyContent={"flex-start"}
-              alignItems={"center"}
-              item
-              xs={12}
-            >
-              <Typography
-                textAlign={`${(isSmallDevice || isMediumDevice) && "left"}`}
-                style={{ ...Subtitle, textWrap: 'nowrap' }}
-              >
-                {title}
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid
-              display={"flex"}
-              justifyContent={"flex-start"}
-              alignItems={"center"}
-              item
-              xs={12}
-            >
-              <Typography
-                paddingTop={"8px"}
-                style={TextFontSize30LineHeight38}
-              >
-                {props}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Card>
-      </Grid>
+    <ReusableCard
+      title={title}
+      props={props}
+      style={{
+        subtitle: Subtitle,
+        textFontSize30LineHeight38: TextFontSize30LineHeight38,
+      }}
+    />
   );
 };
 
-export default CardRendered
+export default CardRendered;
