ae510f42ba91645b997432382934bfe4f1425462
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-02-06 08:23:29 -0500

    refactor: use ReusableCardWithHeaderAndFooter in device setup
    Refactor CardRendered to use the ReusableCardWithHeaderAndFooter component
    to improve code reusability and maintainability. This change simplifies
    the layout logic and removes unused media query hooks and styles.

--- src/pages/events/quickGlance/components/CardRenderedDeviceSetup.jsx
+++ src/pages/events/quickGlance/components/CardRenderedDeviceSetup.jsx
@@ -1,26 +1,43 @@
 import { Grid, IconButton, Typography } from "@mui/material";
-import { useMediaQuery } from "@uidotdev/usehooks";
-import { Card, Switch, Tooltip } from "antd";
+// import { useMediaQuery } from "@uidotdev/usehooks";
+import { Switch, Tooltip } from "antd";
 import { lazy, Suspense, useState } from "react";
 import Loading from "../../../../components/animation/Loading";
+import ViewIcon from "../../../../components/icons/ViewIcon";
 import BlueButtonComponent from "../../../../components/UX/buttons/BlueButton";
 import CenteringGrid from "../../../../styles/global/CenteringGrid";
-import { Subtitle } from "../../../../styles/global/Subtitle";
+// import { Subtitle } from "../../../../styles/global/Subtitle";
 import { TextFontSize30LineHeight38 } from "../../../../styles/global/TextFontSize30LineHeight38";
 import ModalAllItemsBasedOnGroup from "./ModalAllItemsBasedOnGroup";
-import ViewIcon from "../../../../components/icons/ViewIcon";
-const ModalAddAndUpdateDeviceSetup = lazy(() =>
-  import("./ModalAddAndUpdateDeviceSetup")
+import ReusableCardWithHeaderAndFooter from "../../../../components/UX/cards/ReusableCardWithHeaderAndFooter";
+const ModalAddAndUpdateDeviceSetup = lazy(
+  () => import("./ModalAddAndUpdateDeviceSetup"),
 );
 const CardRendered = ({ props, title, onChange, loadingStatus, database }) => {
   const [openModalDeviceSetup, setOpenModalDeviceSetup] = useState(false);
   const [openModalItemList, setOpenModalItemList] = useState(false);
-  const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
-  const isMediumDevice = useMediaQuery(
-    "only screen and (min-width : 769px) and (max-width : 992px)"
-  );
+  // const isSmallDevice = useMediaQuery("only screen and (max-width : 768px)");
+  // const isMediumDevice = useMediaQuery(
+  //   "only screen and (min-width : 769px) and (max-width : 992px)"
+  // );
   const handleOpenModal = () => {
     return setOpenModalDeviceSetup(true);
+  };
+
+  const renderingAddInventoryButton = () => {
+    return (
+      <>
+        {props.startingNumber === null && props.endingNumber === null && (
+          <div style={{width:"-webkit-fit-content", padding:"0 24px"}}>
+            <BlueButtonComponent
+              key="allocate-device-type-button"
+              title={"Allocate device type to event"}
+              func={() => handleOpenModal()}
+            />
+          </div>
+        )}
+      </>
+    );
   };
   return (
     <Suspense
@@ -30,131 +47,79 @@ const CardRendered = ({ props, title, onChange, loadingStatus, database }) => {
         </div>
       }
     >
-      <Grid
-        padding={"0 0 10px"}
-        item
-        xs={12}
-        display={
-          props.isItSetAsContainerForEvent &&
-          props.startingNumber !== null &&
-          props.endingNumber !== null
-            ? "none"
-            : "flex"
-        }
+      <ReusableCardWithHeaderAndFooter
+        title={title}
+        actions={[renderingAddInventoryButton()]}
+        style={{ with: "100%" }}
       >
-        <Card
-          style={{
-            borderRadius: "12px",
-            border: "1px solid var(--gray-200, #EAECF0)",
-            background: "var(--base-white, #FFF)",
-            boxShadow:
-              "0px 1px 2px 0px rgba(16, 24, 40, 0.06), 0px 1px 3px 0px rgba(16, 24, 40, 0.10)",
-            cursor: "pointer",
-          }}
-        >
+        <Grid container>
           <Grid
             display={"flex"}
-            justifyContent={"space-around"}
+            justifyContent={"space-between"}
             alignItems={"center"}
-            container
+            item
+            xs={12}
+            sm={12}
+            md={12}
+            lg={12}
           >
-            <Grid
-              display={"flex"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-              item
-              xs={12}
-            >
-              <Typography
-                textAlign={`${(isSmallDevice || isMediumDevice) && "left"}`}
-                style={{ ...Subtitle, textWrap: "nowrap" }}
+            <Typography paddingTop={"8px"} style={TextFontSize30LineHeight38}>
+              {props.quantity}
+            </Typography>
+            <div style={{ display: "flex", gap: "5px" }}>
+              <Tooltip
+                title={`${
+                  props.consumerUses ? "For consumers." : "For internal use."
+                }`}
+                style={{ width: "100%" }}
               >
-                {title}&nbsp;
-              </Typography>
-            </Grid>
-          </Grid>
-          <Grid container>
-            <Grid
-              display={"flex"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-            >
-              <Typography paddingTop={"8px"} style={TextFontSize30LineHeight38}>
-                {props.quantity}
-              </Typography>
-              <div style={{ display: "flex", gap: "5px" }}>
-                <Tooltip
-                  title={`${
-                    props.consumerUses ? "For consumers." : "For internal use."
-                  }`}
-                  style={{ width: "100%" }}
-                >
-                  <div style={{ margin: "0 0 0 15px" }}>
-                    <Switch
-                      checked={props.consumerUses}
-                      loading={loadingStatus}
-                      onChange={onChange}
-                    />
-                  </div>
-                </Tooltip>
-                <Tooltip
-                  title={"View all serial numbers of this device type"}
-                  style={{ width: "100%" }}
-                >
-                  <div
-                    style={{
-                      margin: "0 0 0 15px",
-                      display:
-                        props.startingNumber !== null &&
-                        props.endingNumber !== null
-                          ? "flex"
-                          : "none",
-                    }}
-                  >
-                    <IconButton
-                      size="small"
-                      onClick={() => setOpenModalItemList(true)}
-                      sx={{ mr: 1 }}
-                    >
-                      <ViewIcon />
-                    </IconButton>
-                  </div>
-                </Tooltip>
-              </div>
-            </Grid>
-            <Grid
-              display={"flex"}
-              justifyContent={"space-between"}
-              alignItems={"center"}
-              item
-              xs={12}
-              sm={12}
-              md={12}
-              lg={12}
-            >
-              {props.startingNumber === null && props.endingNumber === null && (
-                <BlueButtonComponent
-                  title={"Allocate device type to event"}
-                  func={() => handleOpenModal()}
-                  styles={{
+                <div style={{ margin: "0 0 0 15px" }}>
+                  <Switch
+                    checked={props.consumerUses}
+                    loading={loadingStatus}
+                    onChange={onChange}
+                  />
+                </div>
+              </Tooltip>
+              <Tooltip
+                title={"View all serial numbers of this device type"}
+                style={{ width: "100%" }}
+              >
+                <div
+                  style={{
+                    margin: "0 0 0 15px",
                     display:
                       props.startingNumber !== null &&
                       props.endingNumber !== null
-                        ? "none"
-                        : "flex",
-                    width: "100%",
+                        ? "flex"
+                        : "none",
                   }}
-                />
-              )}
-            </Grid>
+                >
+                  <IconButton
+                    size="small"
+                    onClick={() => setOpenModalItemList(true)}
+                    sx={{ mr: 1 }}
+                  >
+                    <ViewIcon />
+                  </IconButton>
+                </div>
+              </Tooltip>
+            </div>
           </Grid>
-        </Card>
-      </Grid>
+          {props.startingNumber === null && props.endingNumber === null && (
+            <Grid
+              display={"flex"}
+              justifyContent={"space-between"}
+              alignItems={"center"}
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+            ></Grid>
+          )}
+        </Grid>
+      </ReusableCardWithHeaderAndFooter>
       {openModalDeviceSetup && (
         <ModalAddAndUpdateDeviceSetup
           openModalDeviceSetup={openModalDeviceSetup}
