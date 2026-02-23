e6728ffd7bcdbd0bd19beb67585aa0a9ae007800
Gustavo Santeliz <grodriguez@contextglobal.com>
2026-02-23 08:49:28 -0500

    Refactor: Dynamic Button Title in `ButtonsForm`
    This commit refactors the `ButtonsForm` component to dynamically set the title of the primary button based on the current route.
    
    - **Dynamic Title**: The `BlueButtonComponent` now uses the `location.pathname` to determine whether to display "Save new group of items" or "Update group of items". This makes the component more reusable and adaptable to different contexts.
    - **Improved Readability**: The code has been formatted for better readability and maintainability.

---
+++ src/pages/inventory/actions/edit/ux/ButtonFormSection.jsx
@@ -0,0 +1,39 @@
+import { Link, useLocation } from "react-router-dom";
+import { Divider } from "antd";
+import BlueButtonComponent from "../../../../../components/UX/buttons/BlueButton";
+import GrayButtonComponent from "../../../../../components/UX/buttons/GrayButton";
+
+const ButtonsForm = ({ stylingComponents, loadingStatus, moreInfoDisplay }) => {
+  const location = useLocation()
+  return (
+    <>
+      <Divider style={{ display: moreInfoDisplay ? "" : "none" }} />
+        <div
+          style={{
+            display: "grid",
+            gridAutoColumns: "minmax(1fr, 1fr 1fr)",
+            gap: "0.5rem",
+          }}
+        >
+          <BlueButtonComponent
+            title={location.pathname.includes("bulk") ? "Save new group of items":"Update group of items"}
+            loadingState={loadingStatus}
+            disabled={loadingStatus}
+            styles={stylingComponents({ loadingStatus }).buttonStyleLoading}
+            buttonType="submit"
+          />
+          <Link to="/inventory" style={{ width: "100%" }}>
+            <GrayButtonComponent
+              title={"Go back"}
+              func={() => null}
+              // icon={<WhiteCirclePlusIcon stroke="#344054" hoverStroke="#fff" />}
+              styles={{ width: "100%" }}
+              buttonType="reset"
+            />
+          </Link>
+        </div>
+    </>
+  );
+};
+
+export default ButtonsForm;
