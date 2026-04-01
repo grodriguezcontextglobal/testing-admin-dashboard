2aec38450069b079ab18099a771d082cb24cf929
Gustavo Rodriguez <grodriguez@contextglobal.com>
2026-04-01 16:30:42 -0400

    feat(UX): create notification icon dictionary
    This commit introduces a new utility file that exports `dicIconNotification`, a dictionary mapping notification types to their corresponding icon components.
    
    This utility provides a centralized and reusable way to display consistent icons in Ant Design notifications throughout the application. By mapping strings like "success", "error", and "warning" to their respective icon components, it simplifies the process of adding clear visual cues to user feedback, enhancing the overall user experience and ensuring visual consistency.

---
+++ src/utils/dicIconNotification.jsx
@@ -0,0 +1,11 @@
+import { ErrorIcon } from "../components/icons/ErrorIcon";
+import { InformationIcon } from "../components/icons/InformationIcon";
+import { SuccessIcon } from "../components/icons/SuccessIcon";
+import { WarningIcon } from "../components/icons/WarningIcon";
+
+export const dicIconNotification = {
+    success: <SuccessIcon />,
+    error: <ErrorIcon />,
+    info: <InformationIcon />,
+    warning: <WarningIcon />,
+};
