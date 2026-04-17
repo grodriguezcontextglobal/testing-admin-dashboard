678ffc901267dab191b49bbcbbc6cf19f7e836a2
Gustavo Rodriguez <grodriguez@contextglobal.com>
2026-04-17 13:35:03 -0400

    refactor(documents): Redesign DocumentUpload form with sectioned layout

--- src/components/documents/DocumentUpload.jsx
+++ src/components/documents/DocumentUpload.jsx
@@ -1,19 +1,14 @@
-import { Grid, Paper } from "@mui/material";
-import {
-  Button,
-  DatePicker,
-  Form,
-  Input,
-  message,
-  Select
-} from "antd";
+import { Divider, Grid } from "@mui/material";
+import { DatePicker, Form, Input, message, Select } from "antd";
 import { useState } from "react";
 import { useSelector } from "react-redux";
 import { devitrakApi } from "../../api/devitrakApi";
-import { OutlinedInputStyle } from "../../styles/global/OutlinedInputStyle";
 import { UploadIcon } from "../icons/UploadIcon";
+import SectionFooter from "./new_form_components/SectionFooter";
+import SectionHeader from "./new_form_components/SectionHeader";
+import SectionLabel from "./new_form_components/SectionLabel";
 
-const DocumentUpload = ({activeTab, refetch}) => {
+const DocumentUpload = ({ activeTab, refetch }) => {
   const [form] = Form.useForm();
   const [file, setFile] = useState(null);
   const { user } = useSelector((state) => state.admin);
@@ -48,11 +43,7 @@ const DocumentUpload = ({activeTab, refetch}) => {
         formDataToSend.append(key, JSON.stringify(values[key] || []));
       } else if (key === "expiration_date") {
         formDataToSend.append(key, values[key]?.toISOString());
-      } 
-      // else if (key === "requires_signature") {
-      //   formDataToSend.append(key, String(values[key]));
-      // } 
-      else {
+      } else {
         formDataToSend.append(key, values[key]);
       }
     });
@@ -63,12 +54,12 @@ const DocumentUpload = ({activeTab, refetch}) => {
         formDataToSend
       );
 
-      const data = await response.data
+      const data = await response.data;
       if (data.ok) {
         form.resetFields();
         setFile(null);
-        activeTab("1")
-        return refetch()
+        activeTab("1");
+        return refetch();
       } else {
         console.error("Upload failed:", data.msg);
         throw Error(data.msg);
@@ -80,105 +71,138 @@ const DocumentUpload = ({activeTab, refetch}) => {
   };
 
   return (
-    <Paper sx={{ p: 3 }}>
-      <Form form={form} onFinish={handleSubmit} layout="vertical">
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Form.Item label="Upload PDF Document">
-              <input
-                type="file"
-                onChange={handleFileChange}
-                style={{ ...OutlinedInputStyle }}
-                id="pdf-upload"
-                accept="application/pdf"
-                label={
-                  <Button component="span">
-                    <UploadIcon />
-                    Upload PDF
-                  </Button>
-                }
-              />
-            </Form.Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Form.Item
-              label="Title"
-              name="title"
-              rules={[{ required: true, message: "Please input the title!" }]}
-            >
-              <Input />
-            </Form.Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Form.Item
-              label="Document Type"
-              name="document_type"
-              rules={[
-                { required: true, message: "Please select document type!" },
-              ]}
-            >
-              <Select>
-                <Select.Option value="policy">Policy</Select.Option>
-                <Select.Option value="procedure">Procedure</Select.Option>
-                <Select.Option value="form">Form</Select.Option>
-              </Select>
-            </Form.Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Form.Item label="Uses for" name="trigger_action">
-              <Select>
-                <Select.Option value="onboarding">Staff</Select.Option>
-                {/* <Select.Option value="annual">Annual</Select.Option> */}
-                <Select.Option value="event">Event</Select.Option>
-              </Select>
-            </Form.Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Form.Item label="Language" name="language" initialValue="en">
-              <Select>
-                <Select.Option value="en">English</Select.Option>
-                <Select.Option value="es">Spanish</Select.Option>
-              </Select>
-            </Form.Item>
-          </Grid>
-
-          <Grid item xs={12}>
-            <Form.Item label="Description" name="description">
-              <Input.TextArea rows={4} />
-            </Form.Item>
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <Form.Item label="Expiration Date" name="expiration_date">
-              <DatePicker style={{ width: "100%" }} />
-            </Form.Item>
-          </Grid>
-
-          {/* <Grid item xs={12} sm={6}>
-            <Form.Item
-              label="Requires Signature"
-              name="requires_signature"
-              valuePropName="checked"
-              initialValue={false}
-            >
-              <Switch />
-            </Form.Item>
-          </Grid> */}
-
-          <Grid item xs={12}>
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Upload Document
-              </Button>
-            </Form.Item>
-          </Grid>
+    <Form form={form} onFinish={handleSubmit} layout="vertical" style={{margin:"1rem 0"}}>
+      <SectionHeader
+        title="Document details"
+        subtitle="Update your document details here."
+        cancelButton={() => form.resetFields()}
+        saveButton={() => form.submit()}
+      />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Document title"
+            description="This will be displayed on the document."
+          />
         </Grid>
-      </Form>
-    </Paper>
+        <Grid item xs={12} lg={9}>
+          <Form.Item
+            name="title"
+            rules={[{ required: true, message: "Please input the title!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Document type"
+            description="Select the type of document."
+          />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          <Form.Item
+            name="document_type"
+            rules={[
+              { required: true, message: "Please select document type!" },
+            ]}
+          >
+            <Select>
+              <Select.Option value="policy">Policy</Select.Option>
+              <Select.Option value="procedure">Procedure</Select.Option>
+              <Select.Option value="form">Form</Select.Option>
+            </Select>
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Uses for"
+            description="Select the purpose of the document."
+          />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          <Form.Item name="trigger_action">
+            <Select>
+              <Select.Option value="onboarding">Staff</Select.Option>
+              <Select.Option value="event">Event</Select.Option>
+            </Select>
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Language"
+            description="Select the language of the document."
+          />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          <Form.Item name="language" initialValue="en">
+            <Select>
+              <Select.Option value="en">English</Select.Option>
+              <Select.Option value="es">Spanish</Select.Option>
+            </Select>
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Description"
+            description="Write a short description of the document."
+          />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          <Form.Item name="description">
+            <Input.TextArea rows={4} />
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Expiration Date"
+            description="Set an expiration date for the document."
+          />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          <Form.Item name="expiration_date">
+            <DatePicker style={{ width: "100%" }} />
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <Divider />
+      <Grid container spacing={3} mt={2}>
+        <Grid item xs={12} lg={3}>
+          <SectionLabel
+            title="Upload PDF Document"
+            description="Upload the document in PDF format."
+          />
+        </Grid>
+        <Grid item xs={12} lg={9}>
+          <Form.Item>
+            <Input
+              type="file"
+              onChange={handleFileChange}
+              addonBefore={<UploadIcon />}
+              accept="application/pdf"
+            />
+          </Form.Item>
+        </Grid>
+      </Grid>
+      <SectionFooter
+        cancelButton={() => form.resetFields()}
+        saveButton={() => form.submit()}
+      />
+    </Form>
   );
 };
 
