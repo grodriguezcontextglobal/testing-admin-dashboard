78126ce8898b09d08ce1fec0e7f016d2a69d15c7
Gustavo Rodriguez <gar.santeliz@gmail.com>
2026-05-06 14:54:38 -0400

    feat(inventory): Add Ship Out Inventory action button
    This commit introduces a new "Ship out inventory" button to the inventory search bar, providing a direct entry point to the shipping workflow.
    
    - A LightBlueButtonComponent has been added to trigger the new ShippingInventoryModal .
    - For initial integration and testing, the button's visibility is conditionally rendered based on the user's email, allowing for a phased rollout of the feature.

--- src/pages/inventory/utils/ux/InventorySearchBar.jsx
+++ src/pages/inventory/utils/ux/InventorySearchBar.jsx
@@ -7,6 +7,7 @@ import BlueButtonComponent from "../../../../components/UX/buttons/BlueButton";
 import { useSelector } from "react-redux";
 import DangerButtonComponent from "../../../../components/UX/buttons/DangerButton";
 import GrayButtonComponent from "../../../../components/UX/buttons/GrayButton";
+import LightBlueButtonComponent from "../../../../components/UX/buttons/LigthBlueButton";
 const InventorySearchBar = ({
   companyHasInventoryQuery,
   handleSubmit,
@@ -21,8 +22,10 @@ const InventorySearchBar = ({
   setOpenAdvanceSearchModal,
   setOpenCheckInDevicesFromEvent,
   setOpenDeleteItemModal,
+  setOpenShippingModal,
 }) => {
   const { role, locations } = useSelector((state) => state.permission);
+  const { user } = useSelector(state => state.admin)
   const canRenderButton =
     role === "0" ||
     locations?.some(
@@ -79,6 +82,12 @@ const InventorySearchBar = ({
             <BlueButtonComponent
               title="Check in devices from events"
               func={() => setOpenCheckInDevicesFromEvent(true)}
+            />
+          )}
+          {user.email === "gars.software.dev@gmail.com" && (
+            <LightBlueButtonComponent
+              title="Ship out inventory"
+              func={() => setOpenShippingModal(true)}
             />
           )}
         </div>
