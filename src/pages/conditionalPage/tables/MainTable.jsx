250ce83e001d18a25b3b155a0312b86aa6f32b27
Gustavo Rodriguez <grodriguez@contextglobal.com>
2026-05-29 11:10:52 -0400

    feat(members): Improve address display and table navigation
    Refactors the members table to enhance usability and data presentation.
    
    This commit introduces two key improvements:
    
    - **Address Trimming**: The address column now trims whitespace from the beginning and end of the address string before checking its length. This ensures that addresses consisting only of whitespace are not displayed, leading to a cleaner and more professional appearance.
    
    - **Clickable Rows**: The entire row is now clickable, allowing users to navigate to the member details page by clicking anywhere on the row. This provides a more intuitive and efficient user experience, replacing the previous implementation where only the narrow-in-circle icon was clickable.

--- src/pages/conditionalPage/tables/MainTable.jsx
+++ src/pages/conditionalPage/tables/MainTable.jsx
@@ -7,7 +7,7 @@ import TableHeader from "../../../components/UX/TableHeader";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { devitrakApi } from "../../../api/devitrakApi";
 import Loading from "../../../components/animation/Loading";
 import BaseTable from "../../../components/UX/tables/BaseTable";
@@ -29,6 +29,7 @@ const MainTable = ({ state }) => {
   const tableStyle = {
     position: "absolute !important",
   };
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [membersData, setMembersData] = useState([]);
   const { user } = useSelector((state) => state.admin);
@@ -150,6 +151,7 @@ const MainTable = ({ state }) => {
         compare: (a, b) => ("" + a.address).localeCompare(b.address),
       },
       render: (address) => {
+        console.log(address, String(address).trim().length)
         return (
           <span
             key={address}
@@ -160,7 +162,9 @@ const MainTable = ({ state }) => {
               alignSelf: "flex-start",
             }}
           >
-            <Typography style={styleCellColumns}>{address}</Typography>
+            <Typography style={styleCellColumns}>
+              {String(address).trim().length < 5 ? "" : address}
+            </Typography>
           </span>
         );
       },
@@ -200,6 +204,14 @@ const MainTable = ({ state }) => {
           rowClassName="editable-row"
           enablePagination={true}
           pageSize={10}
+          rowKey={(record) => record.member_id}
+          onRow={(record) => {
+            return {
+              onClick: () => {
+                navigate(`/member/${record?.member_id}/main`)
+              },
+            };
+          }}
         />
       )}{" "}
     </Grid>
