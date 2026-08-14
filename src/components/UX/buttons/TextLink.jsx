6d4d64189ce5accb0403cc8885193dbcf6b636d4
Gustavo Rodriguez <grodriguez@contextglobal.com>
2026-08-14 10:46:14 -0400

    Remove(href behavior) It is not behaving as expected when href is not provided, so commenting it out for now. After searching within app, there is not usage of this component with href, so it should be safe to remove this logic.

--- src/components/UX/buttons/TextLink.jsx
+++ src/components/UX/buttons/TextLink.jsx
@@ -20,24 +20,25 @@ const TextLink = ({
     color === "gray"
       ? " customized__textLink--gray"
       : color === "error"
-      ? " customized__textLink--error"
-      : "";
+        ? " customized__textLink--error"
+        : "";
   const cls = `customized__textLink${colorClass} ${className}`.trim();
-
-  if (href) {
-    return (
-      <a
-        href={href}
-        target={target}
-        rel={rel ?? (target === "_blank" ? "noopener noreferrer" : undefined)}
-        className={cls}
-        {...rest}
-      >
-        {iconLeading}
-        {children}
-      </a>
-    );
-  }
+  //it is not behaving as expected when href is not provided, so commenting it out for now.
+  //after searching within app, there is not usage of this component with href, so it should be safe to remove this logic.
+  // if (href) {
+  //   return (
+  //     <a
+  //       href={href}
+  //       target={target}
+  //       rel={rel ?? (target === "_blank" ? "noopener noreferrer" : undefined)}
+  //       className={cls}
+  //       {...rest}
+  //     >
+  //       {iconLeading}
+  //       {children}
+  //     </a>
+  //   );
+  // }
 
   return (
     <button type="button" className={cls} {...rest}>
